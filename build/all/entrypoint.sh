6ed7b08718041cdf8e44075443455584447d0e06
fredkan <kjb0518@163.com>
2019-05-24 19:55:21 +0800

    fix oss deploy

--- build/all/entrypoint.sh
+++ build/all/entrypoint.sh
@@ -7,7 +7,7 @@ ossfsVer="1.80.3"
 mkdir -p /host/etc/csi-oss-tool/
 if [ ! `/nsenter --mount=/proc/1/ns/mnt which ossfs` ]; then
     cp /root/ossfs_${ossfsVer}_centos7.0_x86_64.rpm /host/etc/csi-oss-tool/
-    /nsenter --mount=/proc/1/ns/mnt yum localinstall -y /host/etc/csi-oss-tool/ossfs_${ossfsVer}_centos7.0_x86_64.rpm
+    /nsenter --mount=/proc/1/ns/mnt yum localinstall -y /etc/csi-oss-tool/ossfs_${ossfsVer}_centos7.0_x86_64.rpm
 # update OSSFS
 else
     oss_info=`/nsenter --mount=/proc/1/ns/mnt ossfs --version`
@@ -15,7 +15,7 @@ else
     if [ "$vers_conut" = "0" ]; then
         /nsenter --mount=/proc/1/ns/mnt yum remove -y ossfs
         cp /root/ossfs_${ossfsVer}_centos7.0_x86_64.rpm /host/etc/csi-oss-tool/
-        /nsenter --mount=/proc/1/ns/mnt yum localinstall -y /host/etc/csi-oss-tool/ossfs_${ossfsVer}_centos7.0_x86_64.rpm
+        /nsenter --mount=/proc/1/ns/mnt yum localinstall -y /etc/csi-oss-tool/ossfs_${ossfsVer}_centos7.0_x86_64.rpm
     fi
 fi
 
