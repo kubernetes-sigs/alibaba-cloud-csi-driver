2fc98912f1e3e27dfca0e78660952354633b001f
fredkan <kjb0518@163.com>
2019-10-11 20:48:31 +0800

    support lvm localdisk pv

--- pkg/lvm/nodeserver.go
+++ pkg/lvm/nodeserver.go
@@ -38,9 +38,16 @@ import (
 )
 
 const (
-	NSENTER_CMD = "/nsenter --mount=/proc/1/ns/mnt"
-	VG_NAME_TAG = "vgName"
-	DEFAULTFS   = "ext4"
+	NSENTER_CMD   = "/nsenter --mount=/proc/1/ns/mnt"
+	VG_NAME_TAG   = "vgName"
+	PV_TYPE_TAG   = "pvType"
+	FS_TYPE_TAG   = "fsType"
+	LVM_TYPE_TYPE = "lvmType"
+	LOCAL_DISK    = "localdisk"
+	CLOUD_DISK    = "clouddisk"
+	LINEAR_TYPE   = "linear"
+	STRIPING_TYPE = "striping"
+	DEFAULT_FS    = "ext4"
 )
 
 type nodeServer struct {
@@ -52,8 +59,9 @@ type nodeServer struct {
 }
 
 var (
-	masterURL  string
-	kubeconfig string
+	masterURL    string
+	kubeconfig   string
+	DEVICE_CHARS = []string{"b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
 )
 
 func NewNodeServer(d *csicommon.CSIDriver, nodeID string) csi.NodeServer {
@@ -81,11 +89,13 @@ func (ns *nodeServer) GetNodeID() string {
 }
 
 func (ns *nodeServer) NodePublishVolume(ctx context.Context, req *csi.NodePublishVolumeRequest) (*csi.NodePublishVolumeResponse, error) {
+	log.Infof("NodePublishVolume:: req, %v", req)
+
+	// parse request args.
 	targetPath := req.GetTargetPath()
 	if targetPath == "" {
 		return nil, status.Error(codes.Internal, "targetPath is empty")
 	}
-
 	vgName := ""
 	if _, ok := req.VolumeContext[VG_NAME_TAG]; ok {
 		vgName = req.VolumeContext[VG_NAME_TAG]
@@ -93,14 +103,26 @@ func (ns *nodeServer) NodePublishVolume(ctx context.Context, req *csi.NodePublis
 	if vgName == "" {
 		return nil, status.Error(codes.Internal, "error with input vgName is empty")
 	}
-	log.Infof("NodePublishVolume: Starting to mount lvm at: %s, with vg: %s, with volume: %s", targetPath, vgName, req.GetVolumeId())
+	pvType := CLOUD_DISK
+	if _, ok := req.VolumeContext[PV_TYPE_TAG]; ok {
+		pvType = req.VolumeContext[PV_TYPE_TAG]
+	}
+	lvmType := LINEAR_TYPE
+	if _, ok := req.VolumeContext[LVM_TYPE_TYPE]; ok {
+		lvmType = req.VolumeContext[LVM_TYPE_TYPE]
+	}
+	fsType := DEFAULT_FS
+	if _, ok := req.VolumeContext[FS_TYPE_TAG]; ok {
+		fsType = req.VolumeContext[FS_TYPE_TAG]
+	}
+	log.Infof("NodePublishVolume: Starting to mount lvm at: %s, with vg: %s, with volume: %s, PV type: %s, LVM type: %s", targetPath, vgName, req.GetVolumeId(), pvType, lvmType)
 
 	volumeNewCreated := false
 	volumeId := req.GetVolumeId()
 	devicePath := filepath.Join("/dev/", vgName, volumeId)
 	if _, err := os.Stat(devicePath); os.IsNotExist(err) {
 		volumeNewCreated = true
-		err := ns.createVolume(ctx, volumeId, vgName)
+		err := ns.createVolume(ctx, volumeId, vgName, pvType, lvmType)
 		if err != nil {
 			return nil, status.Error(codes.Internal, err.Error())
 		}
@@ -123,8 +145,8 @@ func (ns *nodeServer) NodePublishVolume(ctx context.Context, req *csi.NodePublis
 		return nil, status.Errorf(codes.Internal, "check fs type err: %v", err)
 	}
 	if exitFSType == "" {
-		log.Printf("The device %v has no filesystem, format %v", devicePath, exitFSType)
-		if err := formatDevice(devicePath, DEFAULTFS); err != nil {
+		log.Printf("The device %v has no filesystem, starting format: %v", devicePath, fsType)
+		if err := formatDevice(devicePath, fsType); err != nil {
 			return nil, status.Errorf(codes.Internal, "format fstype failed: err=%v", err)
 		}
 	}
@@ -139,7 +161,7 @@ func (ns *nodeServer) NodePublishVolume(ctx context.Context, req *csi.NodePublis
 		mountFlags := req.GetVolumeCapability().GetMount().GetMountFlags()
 		options = append(options, mountFlags...)
 
-		err = ns.mounter.Mount(devicePath, targetPath, DEFAULTFS, options...)
+		err = ns.mounter.Mount(devicePath, targetPath, fsType, options...)
 		if err != nil {
 			return nil, status.Error(codes.Internal, err.Error())
 		}
@@ -274,17 +296,42 @@ func (ns *nodeServer) getPvSize(volumeId string) int64 {
 	return pvSize
 }
 
-func (ns *nodeServer) createVolume(ctx context.Context, volumeId string, vgName string) error {
+// create lvm volume
+func (ns *nodeServer) createVolume(ctx context.Context, volumeId, vgName, pvType, lvmType string) error {
 	pvSize := ns.getPvSize(volumeId)
+
+	pvNumber := 0
+	var err error = nil
+	// Create VG if vg not exist,
+	if pvType == LOCAL_DISK {
+		if pvNumber, err = createVG(vgName); err != nil {
+			return err
+		}
+	}
+
+	// check vg exist
 	ckCmd := fmt.Sprintf("%s vgck %s", NSENTER_CMD, vgName)
-	_, err := utils.Run(ckCmd)
+	_, err = utils.Run(ckCmd)
 	if err != nil {
+		log.Errorf("createVolume:: VG is not exist: %s", vgName)
 		return err
 	}
-	cmd := fmt.Sprintf("%s lvcreate -n %s -L %dg %s", NSENTER_CMD, volumeId, pvSize, vgName)
-	_, err = utils.Run(cmd)
-	if err != nil {
-		return err
+
+	// Create lvm volume
+	if lvmType == STRIPING_TYPE {
+		cmd := fmt.Sprintf("%s lvcreate -i %d -n %s -L %dg %s", NSENTER_CMD, pvNumber, volumeId, pvSize, vgName)
+		_, err = utils.Run(cmd)
+		if err != nil {
+			return err
+		}
+		log.Infof("Successful Create Striping LVM volume: %s, Size: %d, vgName: %s, striped number: %d", volumeId, pvSize, vgName, pvNumber)
+	} else if lvmType == LINEAR_TYPE {
+		cmd := fmt.Sprintf("%s lvcreate -n %s -L %dg %s", NSENTER_CMD, volumeId, pvSize, vgName)
+		_, err = utils.Run(cmd)
+		if err != nil {
+			return err
+		}
+		log.Infof("Successful Create Linear LVM volume: %s, Size: %d, vgName: %s", volumeId, pvSize, vgName)
 	}
 	return nil
 }
--- pkg/lvm/utils.go
+++ pkg/lvm/utils.go
@@ -18,10 +18,19 @@ package lvm
 
 import (
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os/exec"
+	"path/filepath"
+	"strconv"
 	"strings"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/aliyun/alibaba-cloud-sdk-go/services/ecs"
+	"github.com/kubernetes-sigs/alibaba-cloud-csi-driver/pkg/utils"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 const (
@@ -82,4 +91,143 @@ func checkFSType(devicePath string) (string, error) {
 		}
 	}
 	return "", parseErr
+}
+
+func isVgExist(vgName string) (bool, error) {
+	vgCmd := fmt.Sprintf("%s vgdisplay %s | grep 'VG Name' | grep %s | grep -v grep | wc -l", NSENTER_CMD, vgName, vgName)
+	vgline, err := utils.Run(vgCmd)
+	if err != nil {
+		return false, err
+	}
+	if strings.TrimSpace(vgline) == "1" {
+		return true, nil
+	}
+	return false, nil
+}
+
+// Get Local Disk Number from ecs API
+// Requirements: The instance must have role which contains ecs::DescribeInstances, ecs::DescribeInstancesType.
+func getLocalDeviceNum() (int, error) {
+	instanceId := GetMetaData(INSTANCE_ID)
+	regionId := GetMetaData(REGIONID_TAG)
+	localDeviceNum := 0
+	akId, akSecret, token := "", "", ""
+	akId, akSecret = utils.GetLocalAK()
+	if akId == "" || akSecret == "" {
+		akId, akSecret, token = utils.GetSTSAK()
+	}
+	client := utils.NewEcsClient(akId, akSecret, token)
+
+	// Get Instance Type
+	request := ecs.CreateDescribeInstancesRequest()
+	request.RegionId = regionId
+	request.InstanceIds = "[\"" + instanceId + "\"]"
+	instanceResponse, err := client.DescribeInstances(request)
+	if err != nil {
+		log.Errorf("getLocalDeviceNum: Describe Instance: %s Error: %s", instanceId, err.Error())
+		return -1, err
+	}
+	if instanceResponse == nil || len(instanceResponse.Instances.Instance) == 0 {
+		log.Infof("getLocalDeviceNum: Describe Instance Error, with empty response: %s", instanceId)
+		return -1, err
+	}
+
+	// Get Instance LocalDisk Number
+	instanceTypeId := instanceResponse.Instances.Instance[0].InstanceType
+	instanceTypeFamily := instanceResponse.Instances.Instance[0].InstanceTypeFamily
+	instanceTypeRequest := ecs.CreateDescribeInstanceTypesRequest()
+	instanceTypeRequest.InstanceTypeFamily = instanceTypeFamily
+	response, err := client.DescribeInstanceTypes(instanceTypeRequest)
+	if err != nil {
+		log.Errorf("getLocalDeviceNum: Describe Instance: %s, Type: %s, Family: %s Error: %s", instanceId, instanceTypeId, instanceTypeFamily, err.Error())
+		return -1, err
+	}
+	for _, instance := range response.InstanceTypes.InstanceType {
+		if instance.InstanceTypeId == instanceTypeId {
+			localDeviceNum = instance.LocalStorageAmount
+			log.Infof("getLocalDeviceNum: Instance: %s, InstanceType: %s, InstanceLocalDiskNum: %d", instanceId, instanceTypeId, localDeviceNum)
+			break
+		}
+	}
+	return localDeviceNum, nil
+}
+
+// create vg if not exist
+func createVG(vgName string) (int, error) {
+	pvNum := 0
+
+	// step1: check vg is created or not
+	vgCmd := fmt.Sprintf("%s vgdisplay %s | grep 'VG Name' | grep %s | grep -v grep | wc -l", NSENTER_CMD, vgName, vgName)
+	vgline, err := utils.Run(vgCmd)
+	if err != nil {
+		return 0, err
+	}
+	if strings.TrimSpace(vgline) == "1" {
+		pvNumCmd := fmt.Sprintf("%s vgdisplay %s | grep 'Cur PV' | grep -v grep | awk '{print $3}'", NSENTER_CMD, vgName)
+		if pvNumStr, err := utils.Run(pvNumCmd); err != nil {
+			return 0, err
+		} else {
+			if pvNum, err = strconv.Atoi(strings.TrimSpace(pvNumStr)); err != nil {
+				return 0, err
+			}
+		}
+		return pvNum, nil
+	}
+
+	// Step 2: Get LocalDisk Number
+	localDeviceList := []string{}
+	localDeviceNum, err := getLocalDeviceNum()
+	if err != nil {
+		log.Errorf("LocalDiskMount:: Get Local Disk Number Error, Error: %s", err.Error())
+		return 0, status.Error(codes.Internal, "Get Local Disk Number Error")
+	}
+	if localDeviceNum < 1 {
+		log.Errorf("VG not exist and also not local disk exist, vgName: %s", vgName)
+		return 0, status.Error(codes.Internal, "VG not exist, and also not local disk exist")
+	}
+
+	// Step 3: Get LocalDisk device
+	deviceStartWith := "vdb"
+	deviceNamePrefix := "vd"
+	deviceStartChar := "b"
+	deviceStartIndex := 0
+	deviceNameLen := len(deviceStartWith)
+	if deviceNameLen > 1 {
+		deviceStartChar = deviceStartWith[deviceNameLen-1 : deviceNameLen]
+		for index := 0; index < 15; index++ {
+			if deviceStartChar == DEVICE_CHARS[index] {
+				deviceStartIndex = index
+			}
+		}
+		deviceNamePrefix = deviceStartWith[0 : deviceNameLen-1]
+	}
+	for i := deviceStartIndex; i < localDeviceNum; i++ {
+		deviceName := deviceNamePrefix + DEVICE_CHARS[i]
+		devicePath := filepath.Join("/dev", deviceName)
+		localDeviceList = append(localDeviceList, devicePath)
+	}
+	log.Infof("doLocalVolumeMounts, Starting LocalDisk Mount: vgName: %s, LocalDisk Number: %d, LocalDisk: %v", vgName, localDeviceNum, localDeviceList)
+
+	for _, devicePath := range localDeviceList {
+		if !utils.IsFileExisting(devicePath) {
+			log.Errorf("PV (%s) is not exist", devicePath)
+			return 0, status.Error(codes.Internal, "PV is Not exit: "+devicePath)
+		}
+		pvCmd := fmt.Sprintf("%s pvdisplay %s | grep 'VG Name' | grep -v grep | awk '{print $3}'", NSENTER_CMD, devicePath)
+		existVgName, err := utils.Run(pvCmd)
+		if err != nil {
+			log.Errorf("PV (%s) is Already in VG: %s", devicePath, strings.TrimSpace(existVgName))
+			return 0, err
+		}
+	}
+	localDeviceStr := strings.Join(localDeviceList, " ")
+	vgAddCmd := fmt.Sprintf("%s vgcreate %s %s", NSENTER_CMD, vgName, localDeviceStr)
+	_, err = utils.Run(vgAddCmd)
+	if err != nil {
+		log.Errorf("Add PV (%s) to VG: %s error: %s", localDeviceStr, strings.TrimSpace(vgName), err.Error())
+		return 0, err
+	}
+
+	log.Infof("Successful add Local Disks to VG (%s): %v", vgName, localDeviceList)
+	return localDeviceNum, nil
 }
