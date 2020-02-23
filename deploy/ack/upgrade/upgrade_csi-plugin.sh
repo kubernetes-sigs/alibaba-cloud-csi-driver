e718663d6ccaf5964483c4c252d1056df73dfd84
fredkan <kjb0518@163.com>
2020-02-23 18:25:00 +0800

    ack upgrade scripts

---
+++ deploy/ack/upgrade/upgrade_csi-plugin.sh
@@ -0,0 +1,297 @@
+#!/bin/bash
+
+## Usage
+## Append image tag which is expect.
+## sh upgrade_csi-plugin.sh v1.14.8.36-93f2b131-aliyun
+
+
+if [ "$1" = "" ]; then
+  echo "Please input the expect csi plugin version... "
+fi
+imageVersion=$1
+echo `date`" Start to Upgrade CSI Provisioner to $imageVersion ..."
+
+# new deploy template file
+# if any changes, just update here and replace image value
+cat > .aliyun-csi-plugin.yaml << EOF
+apiVersion: storage.k8s.io/v1beta1
+kind: CSIDriver
+metadata:
+  name: diskplugin.csi.alibabacloud.com
+spec:
+  attachRequired: true
+  podInfoOnMount: true
+---
+apiVersion: storage.k8s.io/v1beta1
+kind: CSIDriver
+metadata:
+  name: nasplugin.csi.alibabacloud.com
+spec:
+  attachRequired: false
+  podInfoOnMount: true
+---
+apiVersion: storage.k8s.io/v1beta1
+kind: CSIDriver
+metadata:
+  name: ossplugin.csi.alibabacloud.com
+spec:
+  attachRequired: false
+  podInfoOnMount: true
+---
+kind: DaemonSet
+apiVersion: apps/v1
+metadata:
+  name: csi-plugin
+  namespace: kube-system
+spec:
+  selector:
+    matchLabels:
+      app: csi-plugin
+  template:
+    metadata:
+      labels:
+        app: csi-plugin
+    spec:
+      tolerations:
+        - operator: "Exists"
+      priorityClassName: system-node-critical
+      serviceAccount: admin
+      hostNetwork: true
+      hostPID: true
+      containers:
+        - name: disk-driver-registrar
+          image: csi-image-prefix/acs/csi-node-driver-registrar:v1.2.0
+          imagePullPolicy: Always
+          args:
+            - "--v=5"
+            - "--csi-address=/var/lib/kubelet/csi-plugins/diskplugin.csi.alibabacloud.com/csi.sock"
+            - "--kubelet-registration-path=/var/lib/kubelet/csi-plugins/diskplugin.csi.alibabacloud.com/csi.sock"
+          volumeMounts:
+            - name: kubelet-dir
+              mountPath: /var/lib/kubelet
+            - name: registration-dir
+              mountPath: /registration
+        - name: csi-diskplugin
+          securityContext:
+            privileged: true
+            capabilities:
+              add: ["SYS_ADMIN"]
+            allowPrivilegeEscalation: true
+          image: csi-image-prefix/acs/csi-plugin:csi-image-version
+          imagePullPolicy: "Always"
+          args:
+            - "--endpoint=\$(CSI_ENDPOINT)"
+            - "--v=2"
+            - "--driver=diskplugin.csi.alibabacloud.com"
+          env:
+            - name: CSI_ENDPOINT
+              value: unix://var/lib/kubelet/csi-plugins/diskplugin.csi.alibabacloud.com/csi.sock
+            - name: ACCESS_KEY_ID
+              value: ""
+            - name: ACCESS_KEY_SECRET
+              value: ""
+            - name: MAX_VOLUMES_PERNODE
+              value: "15"
+            - name: DISK_TAGED_BY_PLUGIN
+              value: "true"
+          livenessProbe:
+            httpGet:
+              path: /healthz
+              port: healthz
+              scheme: HTTP
+            initialDelaySeconds: 10
+            periodSeconds: 10
+            timeoutSeconds: 3
+            failureThreshold: 5
+          ports:
+          - containerPort: 9810
+            name: healthz
+            protocol: TCP
+          volumeMounts:
+            - name: kubelet-dir
+              mountPath: /var/lib/kubelet
+              mountPropagation: "Bidirectional"
+            - name: container-dir
+              mountPath: /var/lib/container
+              mountPropagation: "Bidirectional"
+            - mountPath: /dev
+              mountPropagation: "HostToContainer"
+              name: host-dev
+            - mountPath: /var/log/
+              name: host-log
+            - name: etc
+              mountPath: /host/etc
+        - name: disk-liveness-probe
+          image: csi-image-prefix/acs/csi-livenessprobe:v2.0.0
+          args:
+            - --csi-address=/var/lib/kubelet/csi-plugins/diskplugin.csi.alibabacloud.com/csi.sock
+            - --health-port=9810
+          volumeMounts:
+            - name: kubelet-dir
+              mountPath: /var/lib/kubelet
+
+        - name: nas-driver-registrar
+          image: csi-image-prefix/acs/csi-node-driver-registrar:v1.2.0
+          imagePullPolicy: Always
+          args:
+          - "--v=5"
+          - "--csi-address=/var/lib/kubelet/csi-plugins/nasplugin.csi.alibabacloud.com/csi.sock"
+          - "--kubelet-registration-path=/var/lib/kubelet/csi-plugins/nasplugin.csi.alibabacloud.com/csi.sock"
+          volumeMounts:
+          - name: kubelet-dir
+            mountPath: /var/lib/kubelet/
+          - name: registration-dir
+            mountPath: /registration
+
+        - name: csi-nasplugin
+          securityContext:
+            privileged: true
+            capabilities:
+              add: ["SYS_ADMIN"]
+            allowPrivilegeEscalation: true
+          image: csi-image-prefix/acs/csi-plugin:csi-image-version
+          imagePullPolicy: "Always"
+          args:
+          - "--endpoint=\$(CSI_ENDPOINT)"
+          - "--v=2"
+          - "--driver=nasplugin.csi.alibabacloud.com"
+          env:
+          - name: CSI_ENDPOINT
+            value: unix://var/lib/kubelet/csi-plugins/nasplugin.csi.alibabacloud.com/csi.sock
+          livenessProbe:
+            httpGet:
+              path: /healthz
+              port: healthz
+              scheme: HTTP
+            initialDelaySeconds: 10
+            periodSeconds: 10
+            timeoutSeconds: 3
+            failureThreshold: 5
+          ports:
+          - containerPort: 9811
+            name: healthz
+            protocol: TCP
+          volumeMounts:
+          - name: kubelet-dir
+            mountPath: /var/lib/kubelet/
+            mountPropagation: "Bidirectional"
+          - mountPath: /var/log/
+            name: host-log
+        - name: nas-liveness-probe
+          image: csi-image-prefix/acs/csi-livenessprobe:v2.0.0
+          args:
+            - --csi-address=/var/lib/kubelet/csi-plugins/nasplugin.csi.alibabacloud.com/csi.sock
+            - --health-port=9811
+          volumeMounts:
+            - name: kubelet-dir
+              mountPath: /var/lib/kubelet
+
+        - name: oss-driver-registrar
+          image: csi-image-prefix/acs/csi-node-driver-registrar:v1.2.0
+          imagePullPolicy: Always
+          args:
+          - "--v=5"
+          - "--csi-address=/var/lib/kubelet/csi-plugins/ossplugin.csi.alibabacloud.com/csi.sock"
+          - "--kubelet-registration-path=/var/lib/kubelet/csi-plugins/ossplugin.csi.alibabacloud.com/csi.sock"
+          volumeMounts:
+          - name: kubelet-dir
+            mountPath: /var/lib/kubelet/
+          - name: registration-dir
+            mountPath: /registration
+
+        - name: csi-ossplugin
+          securityContext:
+            privileged: true
+            capabilities:
+              add: ["SYS_ADMIN"]
+            allowPrivilegeEscalation: true
+          image: csi-image-prefix/acs/csi-plugin:csi-image-version
+          imagePullPolicy: "Always"
+          args:
+          - "--endpoint=\$(CSI_ENDPOINT)"
+          - "--v=2"
+          - "--driver=ossplugin.csi.alibabacloud.com"
+          env:
+          - name: CSI_ENDPOINT
+            value: unix://var/lib/kubelet/csi-plugins/ossplugin.csi.alibabacloud.com/csi.sock
+          livenessProbe:
+            httpGet:
+              path: /healthz
+              port: healthz
+              scheme: HTTP
+            initialDelaySeconds: 10
+            periodSeconds: 10
+            timeoutSeconds: 3
+            failureThreshold: 5
+          ports:
+          - containerPort: 9812
+            name: healthz
+            protocol: TCP
+          volumeMounts:
+          - name: kubelet-dir
+            mountPath: /var/lib/kubelet/
+            mountPropagation: "Bidirectional"
+          - name: etc
+            mountPath: /host/etc
+          - mountPath: /var/log/
+            name: host-log
+          - mountPath: /host/usr/
+            name: ossconnectordir
+        - name: oss-liveness-probe
+          image: csi-image-prefix/acs/csi-livenessprobe:v2.0.0
+          args:
+            - --csi-address=/var/lib/kubelet/csi-plugins/ossplugin.csi.alibabacloud.com/csi.sock
+            - --health-port=9812
+          volumeMounts:
+            - name: kubelet-dir
+              mountPath: /var/lib/kubelet
+      volumes:
+        - name: registration-dir
+          hostPath:
+            path: /var/lib/kubelet/plugins_registry
+            type: DirectoryOrCreate
+        - name: container-dir
+          hostPath:
+            path: /var/lib/container
+            type: DirectoryOrCreate
+        - name: kubelet-dir
+          hostPath:
+            path: /var/lib/kubelet
+            type: Directory
+        - name: host-dev
+          hostPath:
+            path: /dev
+        - name: host-log
+          hostPath:
+            path: /var/log/
+        - name: etc
+          hostPath:
+            path: /etc
+        - name: ossconnectordir
+          hostPath:
+            path: /usr/
+  updateStrategy:
+    rollingUpdate:
+      maxUnavailable: 10%
+    type: RollingUpdate
+EOF
+
+## Set target csi-plugin version
+imageBefore=`kubectl describe ds csi-plugin -nkube-system | grep Image: | awk 'NR==2 {print $2}'`
+imagePrefix=`kubectl describe ds csi-plugin -nkube-system | grep Image: | awk 'NR==2 {print $2}' | awk -F: '{print $1}' | awk -F/ '{print $1}'`
+imageName=${imagePrefix}/acs/csi-plugin:$imageVersion
+
+## Check current version
+if [[ $imagePrefix != registry* ]] || [[ $imagePrefix != *aliyuncs.com ]]; then
+    echo "Current image is not expect: "$imagePrefix
+    exit 0
+fi
+
+## Delete old plugin
+kubectl delete csidriver diskplugin.csi.alibabacloud.com nasplugin.csi.alibabacloud.com ossplugin.csi.alibabacloud.com
+kubectl delete ds csi-plugin -nkube-system
+
+## do csi-plugin upgrade
+cat .aliyun-csi-plugin.yaml | sed "s/csi-image-prefix/$imagePrefix/" | sed "s/csi-image-version/$imageVersion/" | kubectl apply -f -
+
+echo "Upgrade csi From "$imageBefore" to "$imageName
---
+++ deploy/ack/upgrade/upgrade_csi-provisioner.sh
@@ -0,0 +1,306 @@
+#!/bin/bash
+
+## Usage
+## Append image tag which is expect.
+## sh upgrade_csi-provisioner.sh v1.14.8.36-93f2b131-aliyun
+
+
+if [ "$1" = "" ]; then
+  echo "Please input the expect csi provisioner version... "
+fi
+imageVersion=$1
+
+echo `date`" Start to Upgrade CSI Provisioner to $imageVersion ..."
+
+# new deploy template file
+# if any changes, just update here and replace image value
+cat > .aliyun-csi-provisioner.yaml << EOF
+apiVersion: storage.k8s.io/v1
+kind: StorageClass
+metadata:
+   name: alicloud-disk-available
+provisioner: diskplugin.csi.alibabacloud.com
+parameters:
+    type: available
+reclaimPolicy: Delete
+allowVolumeExpansion: true
+---
+apiVersion: storage.k8s.io/v1
+kind: StorageClass
+metadata:
+   name: alicloud-disk-essd
+provisioner: diskplugin.csi.alibabacloud.com
+parameters:
+    type: cloud_essd
+reclaimPolicy: Delete
+allowVolumeExpansion: true
+---
+apiVersion: storage.k8s.io/v1
+kind: StorageClass
+metadata:
+   name: alicloud-disk-ssd
+provisioner: diskplugin.csi.alibabacloud.com
+parameters:
+    type: cloud_ssd
+reclaimPolicy: Delete
+allowVolumeExpansion: true
+---
+apiVersion: storage.k8s.io/v1
+kind: StorageClass
+metadata:
+   name: alicloud-disk-efficiency
+provisioner: diskplugin.csi.alibabacloud.com
+parameters:
+    type: cloud_efficiency
+reclaimPolicy: Delete
+allowVolumeExpansion: true
+---
+apiVersion: storage.k8s.io/v1
+kind: StorageClass
+metadata:
+   name: alicloud-disk-topology
+provisioner: diskplugin.csi.alibabacloud.com
+parameters:
+    type: available
+reclaimPolicy: Delete
+volumeBindingMode: WaitForFirstConsumer
+allowVolumeExpansion: true
+---
+kind: Deployment
+apiVersion: apps/v1
+metadata:
+  name: csi-provisioner
+  namespace: kube-system
+spec:
+  selector:
+    matchLabels:
+      app: csi-provisioner
+  replicas: 2
+  template:
+    metadata:
+      labels:
+        app: csi-provisioner
+    spec:
+      tolerations:
+      - operator: "Exists"
+      affinity:
+        nodeAffinity:
+          preferredDuringSchedulingIgnoredDuringExecution:
+          - weight: 1
+            preference:
+              matchExpressions:
+              - key: node-role.kubernetes.io/master
+                operator: Exists
+      priorityClassName: system-node-critical
+      serviceAccount: admin
+      hostNetwork: true
+      containers:
+        - name: external-disk-provisioner
+          image: csi-image-prefix/acs/csi-provisioner:v1.4.0-aliyun
+          args:
+            - "--provisioner=diskplugin.csi.alibabacloud.com"
+            - "--csi-address=\$(ADDRESS)"
+            - "--feature-gates=Topology=True"
+            - "--volume-name-prefix=disk"
+            - "--strict-topology=true"
+            - "--timeout=150s"
+            - "--enable-leader-election=true"
+            - "--leader-election-type=leases"
+            - "--retry-interval-start=500ms"
+            - "--v=5"
+          env:
+            - name: ADDRESS
+              value: /socketDir/csi.sock
+          imagePullPolicy: "Always"
+          volumeMounts:
+            - name: disk-provisioner-dir
+              mountPath: /socketDir
+        - name: external-disk-attacher
+          image: csi-image-prefix/acs/csi-attacher:v2.1.0
+          args:
+            - "--v=5"
+            - "--csi-address=\$(ADDRESS)"
+            - "--leader-election=true"
+          env:
+            - name: ADDRESS
+              value: /csi/csi.sock
+          imagePullPolicy: "Always"
+          volumeMounts:
+            - name: disk-provisioner-dir
+              mountPath: /csi
+        - name: external-disk-resizer
+          image: csi-image-prefix/acs/csi-resizer:v0.3.0
+          args:
+            - "--v=5"
+            - "--csi-address=\$(ADDRESS)"
+            - "--leader-election"
+          env:
+            - name: ADDRESS
+              value: /socketDir/csi.sock
+          imagePullPolicy: "Always"
+          volumeMounts:
+            - name: disk-provisioner-dir
+              mountPath: /socketDir/
+        - name: csi-diskprovisioner
+          securityContext:
+            privileged: true
+            capabilities:
+              add: ["SYS_ADMIN"]
+            allowPrivilegeEscalation: true
+          image: csi-image-prefix/acs/csi-plugin:csi-image-version
+          imagePullPolicy: "Always"
+          args:
+            - "--endpoint=\$(CSI_ENDPOINT)"
+            - "--v=2"
+            - "--driver=diskplugin.csi.alibabacloud.com"
+          env:
+            - name: CSI_ENDPOINT
+              value: unix://socketDir/csi.sock
+            - name: ACCESS_KEY_ID
+              value: ""
+            - name: ACCESS_KEY_SECRET
+              value: ""
+            - name: MAX_VOLUMES_PERNODE
+              value: "15"
+          livenessProbe:
+            httpGet:
+              path: /healthz
+              port: healthz
+              scheme: HTTP
+            initialDelaySeconds: 10
+            periodSeconds: 30
+            timeoutSeconds: 3
+            failureThreshold: 5
+          ports:
+          - containerPort: 9808
+            name: healthz
+            protocol: TCP
+          volumeMounts:
+            - mountPath: /dev
+              mountPropagation: "HostToContainer"
+              name: host-dev
+            - mountPath: /var/log/
+              name: host-log
+            - mountPath: /socketDir/
+              name: disk-provisioner-dir
+            - name: etc
+              mountPath: /host/etc
+          resources:
+            limits:
+              cpu: 100m
+              memory: 100Mi
+            requests:
+              cpu: 100m
+              memory: 100Mi
+        - name: dusk-liveness-probe
+          image: csi-image-prefix/acs/csi-livenessprobe:v2.0.0
+          args:
+            - --csi-address=/csi/csi.sock
+            - --health-port=9808
+          volumeMounts:
+            - name: disk-provisioner-dir
+              mountPath: /csi
+        - name: external-nas-provisioner
+          image: csi-image-prefix/acs/csi-provisioner:v1.4.0-aliyun
+          args:
+            - "--provisioner=nasplugin.csi.alibabacloud.com"
+            - "--csi-address=\$(ADDRESS)"
+            - "--volume-name-prefix=nas"
+            - "--v=5"
+          env:
+            - name: ADDRESS
+              value: /socketDir/csi.sock
+          imagePullPolicy: "Always"
+          volumeMounts:
+            - name: nas-provisioner-dir
+              mountPath: /socketDir
+        - name: csi-nasprovisioner
+          securityContext:
+            privileged: true
+            capabilities:
+              add: ["SYS_ADMIN"]
+            allowPrivilegeEscalation: true
+          image: csi-image-prefix/acs/csi-plugin:csi-image-version
+          imagePullPolicy: "Always"
+          args:
+            - "--endpoint=\$(CSI_ENDPOINT)"
+            - "--v=2"
+            - "--driver=nasplugin.csi.alibabacloud.com"
+          env:
+            - name: CSI_ENDPOINT
+              value: unix://socketDir/csi.sock
+          livenessProbe:
+            httpGet:
+              path: /healthz
+              port: healthz
+              scheme: HTTP
+            initialDelaySeconds: 10
+            periodSeconds: 30
+            timeoutSeconds: 3
+            failureThreshold: 5
+          ports:
+          - containerPort: 9809
+            name: healthz
+            protocol: TCP
+          volumeMounts:
+            - mountPath: /var/log/
+              name: host-log
+            - mountPath: /socketDir/
+              name: nas-provisioner-dir
+            - name: etc
+              mountPath: /host/etc
+          resources:
+            limits:
+              cpu: 100m
+              memory: 100Mi
+            requests:
+              cpu: 100m
+              memory: 100Mi
+        - name: nas-liveness-probe
+          image: csi-image-prefix/acs/csi-livenessprobe:v2.0.0
+          args:
+            - --csi-address=/csi/csi.sock
+            - --health-port=9809
+          volumeMounts:
+            - name: nas-provisioner-dir
+              mountPath: /csi
+      volumes:
+        - name: disk-provisioner-dir
+          emptyDir: {}
+        - name: nas-provisioner-dir
+          emptyDir: {}
+        - name: host-log
+          hostPath:
+            path: /var/log/
+        - name: host-dev
+          hostPath:
+            path: /dev
+        - name: etc
+          hostPath:
+            path: /etc
+EOF
+
+# get registry address
+imagePrefix=`kubectl describe ds csi-plugin -nkube-system | grep Image: | awk 'NR==2 {print $2}' | awk -F: '{print $1}' | awk -F/ '{print $1}'`
+
+# New image name
+imageName=${imagePrefix}/acs/csi-plugin:$imageVersion
+
+## Check current version
+if [[ $imagePrefix != registry* ]] || [[ $imagePrefix != *aliyuncs.com ]]; then
+    echo "Setting image is not expect: "$imagePrefix
+    exit 0
+fi
+
+if [[ $imageVersion != *aliyun ]]; then
+    echo "current image version is not expect: "$imageVersion
+    exit 0
+fi
+
+## Delete old plugin
+kubectl delete sts csi-provisioner -nkube-system
+
+## do csi-provisioner upgrade
+cat .aliyun-csi-provisioner.yaml | sed "s/csi-image-prefix/$imagePrefix/" | sed "s/csi-image-version/$imageVersion/" | kubectl apply -f -
+
+echo "Upgrade csi provisioner to "$imageName
