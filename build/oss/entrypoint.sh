a1c6d486fa74441e31ba1155740fb9559124c70c
Etby Gao <etby.org@gmail.com>
2020-04-27 19:24:15 +0800

    update oss support ubuntu 18.04

--- build/oss/entrypoint.sh
+++ build/oss/entrypoint.sh
@@ -1,26 +1,96 @@
 #!/bin/sh
 
+echo "Preparing deploy oss csi-plugin...."
+
+HOST_CMD="/nsenter --mount=/proc/1/ns/mnt"
+
+host_os="centos-7"
+$HOST_CMD which lsb_release
+lsb_release_exist=$?
+
+if [ "$lsb_release_exist" != "0" ]; then
+  $HOST_CMD ls /etc/os-release
+  os_release_exist=$?
+fi
+
+if [ "$lsb_release_exist" = "0" ]; then
+    os_info=`${HOST_CMD} lsb_release -a`
+    if [ `echo $os_info | grep CentOS | grep 7. | wc -l` != "0" ]; then
+        host_os="centos-7"
+    elif [ `echo $os_info | grep 14.04 | wc -l` != "0" ]; then
+        : # host_os="ubuntu-1404"
+    elif [ `echo $os_info | grep 16.04 | wc -l` != "0" ]; then
+        : # host_os="ubuntu-1604"
+    elif [ `echo $os_info | grep 18.04 | wc -l` != "0" ]; then
+        host_os="ubuntu-1804"
+    elif [ `echo $os_info | grep Aliyun | wc -l` != "0" ]; then
+        : # host_os="alios"
+    elif [ `echo $os_info | grep Alibaba | wc -l` != "0" ]; then
+        : # host_os="alios"
+    fi
+
+elif [ "$os_release_exist" = "0" ]; then
+    osId=`$HOST_CMD cat /etc/os-release | grep "ID="`
+    osVersion=`$HOST_CMD cat /etc/os-release | grep "VERSION_ID="`
+
+    if [ `echo $osId | grep "centos" | wc -l` != "0" ]; then
+        if [ `echo $osVersion | grep "7" | wc -l` = "1" ]; then
+            host_os="centos-7"
+        fi
+    elif [ `echo $osId | grep "alios" | wc -l` != "0" ];then
+       if [ `echo $osVersion | grep "7" | wc -l` = "1" ]; then
+            host_os="centos-7"
+       fi
+    elif [ `echo $osId | grep "ubuntu" | wc -l` != "0" ]; then
+        if [ `echo $osVersion | grep "14.04" | wc -l` != "0" ]; then
+            : # host_os="ubuntu-1404"
+        elif [ `echo $osVersion | grep "16.04" | wc -l` != "0" ]; then
+            : # host_os="ubuntu-1604"
+        elif [ `echo $osVersion | grep "16.04" | wc -l` != "0" ]; then
+            host_os="ubuntu-1804"
+        fi
+    fi
+fi
+
+
+case $host_os in
+
+    ubuntu-1804)
+        ossfsVer="1.80.6"
+        ossfsPackage=ossfs_${ossfsVer}_ubuntu18.04_amd64.deb
+        cmd_localinstall="$HOST_CMD apt-get install -y"
+        cmd_localremove="$HOST_CMD apt-get remove -y" 
+    ;;
+
+    *)
+        ossfsVer="1.80.6.ack.1"
+        ossfsPackage=ossfs_${ossfsVer}_centos7.0_x86_64.rpm
+        cmd_localinstall="$HOST_CMD yum localinstall -y"
+        cmd_localremove="$HOST_CMD yum remove -y"
+    ;;
+
+esac
+
 
 echo "Starting deploy oss csi-plugin...."
 
-ossfsVer="1.80.6.ack.1"
 # install OSSFS
 mkdir -p /host/etc/csi-tool/
-if [ ! `/nsenter --mount=/proc/1/ns/mnt which ossfs` ]; then
+if [ ! `$HOST_CMD which ossfs` ]; then
     echo "First install ossfs...."
-    cp /root/ossfs_${ossfsVer}_centos7.0_x86_64.rpm /host/etc/csi-tool/
-    /nsenter --mount=/proc/1/ns/mnt yum localinstall -y /etc/csi-tool/ossfs_${ossfsVer}_centos7.0_x86_64.rpm
+    cp /root/$ossfsPackage /host/etc/csi-tool/
+    $cmd_localinstall /etc/csi-tool/$ossfsPackage
 # update OSSFS
 else
     echo "Check ossfs Version...."
-    oss_info=`/nsenter --mount=/proc/1/ns/mnt ossfs --version | grep -E -o "V[0-9.a-z]+" | cut -d"V" -f2`
+    oss_info=`$HOST_CMD ossfs --version | grep -E -o "V[0-9.a-z]+" | cut -d"V" -f2`
     #vers_conut=`echo $oss_info | grep ${ossfsVer} | wc -l`
     #if [ "$vers_conut" = "0" ]; then
     if [ "$oss_info" != "$ossfsVer" ]; then
         echo "Upgrade ossfs...."
-        /nsenter --mount=/proc/1/ns/mnt yum remove -y ossfs
-        cp /root/ossfs_${ossfsVer}_centos7.0_x86_64.rpm /host/etc/csi-tool/
-        /nsenter --mount=/proc/1/ns/mnt yum localinstall -y /etc/csi-tool/ossfs_${ossfsVer}_centos7.0_x86_64.rpm
+        $cmd_localremove ossfs
+        cp /root/$ossfsPackage /host/etc/csi-tool/
+        $cmd_localinstall /etc/csi-tool/$ossfsPackage
     fi
 fi
 
@@ -64,13 +134,14 @@ fi
 
 if [ "$updateConnectorService" = "true" ]; then
     echo "install csiplugin connector service...."
+    mkdir -p /host/usr/lib/systemd/system
     cp /bin/csiplugin-connector.service /host/usr/lib/systemd/system/csiplugin-connector.service
-    /nsenter --mount=/proc/1/ns/mnt systemctl daemon-reload
+    $HOST_CMD systemctl daemon-reload
 fi
 
 rm -rf /var/log/alicloud/connector.pid
-/nsenter --mount=/proc/1/ns/mnt systemctl enable csiplugin-connector.service
-/nsenter --mount=/proc/1/ns/mnt systemctl restart csiplugin-connector.service
+$HOST_CMD systemctl enable csiplugin-connector.service
+$HOST_CMD systemctl restart csiplugin-connector.service
 
 # start daemon
 /bin/plugin.csi.alibabacloud.com $@
