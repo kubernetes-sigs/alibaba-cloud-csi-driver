68d035000f847269a6c3cef1e01a592d62950297
fredkan <kjb0518@163.com>
2020-07-10 21:27:52 +0800

    update device reference issue

--- pkg/om/blockvolume.go
+++ pkg/om/blockvolume.go
@@ -3,10 +3,17 @@ package om
 import (
 	log "github.com/sirupsen/logrus"
 	"os"
+	"os/exec"
+	"path/filepath"
 	"strings"
 )
 
+const (
+	MountInfo = "/proc/mounts"
+)
+
 // remove reference links
+// error message: The device %q is still referenced from other Pods;
 func FixReferenceMountIssue(line string) bool {
 	linkFiles := parseReferenceMount(line)
 	if len(linkFiles) == 0 {
@@ -17,10 +24,15 @@ func FixReferenceMountIssue(line string) bool {
 	// loop files
 	for _, file := range linkFiles {
 		if IsFileExisting(file) {
-			if !checkFileIsBlockLink(file) {
+			if !isFileBlockLink(file) {
 				log.Infof("Reference File %s not expect", file)
 				continue
 			}
+			if removable, podInfo := isFileRemovable(file); !removable {
+				log.Infof("Reference File %s cannot be removed as pod(%s) is running", file, podInfo)
+				continue
+			}
+
 			err := os.Remove(file)
 			if err != nil {
 				log.Errorf("Remove Reference File %s with error %s", file, err.Error())
@@ -28,14 +40,30 @@ func FixReferenceMountIssue(line string) bool {
 				log.Infof("Remove Reference File %s Successful", file)
 			}
 		} else {
-			log.Infof("Reference File %s not exist, skipping", file)
+			log.Debug("Reference File %s not exist, skipping", file)
 		}
 	}
 	return true
 }
 
 // /var/lib/kubelet/plugins/kubernetes.io/csi/volumeDevices/pvc-54574338-42dc-4c7e-8337-e015d4e6dbc1/dev/abc
-func checkFileIsBlockLink(file string) bool {
+// If podUid is in mount list, it means the pod related pod is running, not remove it.
+func isFileRemovable(file string) (bool, string) {
+	fileName := filepath.Base(file)
+	findmntCmd := "grep"
+	findmntArgs := []string{fileName, "/proc/mounts"}
+	out, err := exec.Command(findmntCmd, findmntArgs...).CombinedOutput()
+	if err == nil {
+		outStr := strings.TrimSpace(string(out))
+		if outStr != "" {
+			return false, outStr
+		}
+	}
+	return true, ""
+}
+
+// /var/lib/kubelet/plugins/kubernetes.io/csi/volumeDevices/pvc-54574338-42dc-4c7e-8337-e015d4e6dbc1/dev/abc
+func isFileBlockLink(file string) bool {
 	if !strings.HasPrefix(file, "/var/lib/kubelet/plugins/kubernetes.io/csi/volumeDevices/") {
 		return false
 	}
