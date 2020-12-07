209e6ca1192f3e784c29d2b6400aeefd64983649
fredkan <kjb0518@163.com>
2020-12-07 12:22:12 +0800

    csi log restruct oss

--- pkg/log/codes.go
+++ pkg/log/codes.go
@@ -1,18 +1,5 @@
 package log
 
-// const variables for Storage Type
-const (
-	TypeOSS   = "oss"
-	TypeDisk  = "disk"
-	TypeNAS   = "nas"
-	TypeDBFS  = "dbfs"
-	TypeCPFS  = "cpfs"
-	TypeLVM   = "lvm"
-	TypePMEM  = "pmem"
-	TypeMEM   = "memory"
-	TypeQuota = "quota"
-)
-
 // const variables for Error Code
 const (
 	StatusOK                  = "Ok"
---
+++ pkg/log/type.go
@@ -0,0 +1,14 @@
+package log
+
+// const variables for Storage Type
+const (
+	TypeOSS   = "oss"
+	TypeDisk  = "disk"
+	TypeNAS   = "nas"
+	TypeDBFS  = "dbfs"
+	TypeCPFS  = "cpfs"
+	TypeLVM   = "lvm"
+	TypePMEM  = "pmem"
+	TypeMEM   = "memory"
+	TypeQuota = "quota"
+)
