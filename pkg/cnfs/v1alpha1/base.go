5b2f64f7ae537252000367f6c92dda91d978be13
JiaoDean <devjiaodean@gmail.com>
2021-06-02 15:54:21 +0800

    format

--- pkg/cnfs/v1alpha1/base.go
+++ pkg/cnfs/v1alpha1/base.go
@@ -4,35 +4,37 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/json"
 	"k8s.io/client-go/dynamic"
 )
 
+//getContainerNetworkFileSystem get cnfs crd
 func getContainerNetworkFileSystem(client dynamic.Interface, name string) (*ContainerNetworkFileSystem, error) {
 	utd, err := client.Resource(GVR).Get(context.TODO(), name, metav1.GetOptions{})
 	if err != nil {
-		logrus.Errorf("Dynamic get ContainerNetworkFileSystem %s is failed, err:%s",name,err)
+		log.Errorf("Dynamic get cnfs %s is failed, err:%s", name, err)
 		return nil, err
 	}
 	data, err := utd.MarshalJSON()
 	if err != nil {
-		logrus.Errorf("MarshalJSON ContainerNetworkFileSystem %s is failed, err:%s",name,err)
+		log.Errorf("MarshalJSON cnfs %s is failed, err:%s", name, err)
 		return nil, err
 	}
 	var cnfs ContainerNetworkFileSystem
 	if err := json.Unmarshal(data, &cnfs); err != nil {
-		logrus.Errorf("Unmarshal ContainerNetworkFileSystem %s is failed, err:%s",name,err)
+		log.Errorf("Unmarshal cnfs %s is failed, err:%s", name, err)
 		return nil, err
 	}
 	return &cnfs, nil
 }
 
-
+//GetContainerNetworkFileSystemServer get cnfs's server field
 func GetContainerNetworkFileSystemServer(client dynamic.Interface, name string) (string, error) {
 	cnfsObj, err := getContainerNetworkFileSystem(client, name)
 	if err != nil {
+		log.Errorf("Get cnfs %s is failed, err:%s", name, err)
 		return "", err
 	}
 	if cnfsObj.Status.Status != "Available" {
