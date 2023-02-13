70428982b7052d1044088b12b4de633eca061d44
Jan Safranek <jsafrane@redhat.com>
2023-02-13 20:56:01 +0100

    Update go to 1.19

--- vendor/golang.org/x/crypto/ssh/terminal/util_bsd.go
+++ vendor/golang.org/x/crypto/ssh/terminal/util_bsd.go
@@ -2,10 +2,9 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build darwin || dragonfly || freebsd || netbsd || openbsd
 // +build darwin dragonfly freebsd netbsd openbsd
 
-package term
+package terminal
 
 import "golang.org/x/sys/unix"
 
