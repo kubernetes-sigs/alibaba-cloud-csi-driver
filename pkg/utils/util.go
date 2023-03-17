d8e0671b42da95bfb92dd5fd203629b4e1ffd7d8
JiaoDean <devjiaodean@gmail.com>
2023-03-17 17:06:00 +0800

    support cpfs metrics

--- pkg/utils/util.go
+++ pkg/utils/util.go
@@ -108,7 +108,7 @@ const (
 	fsckErrorsUncorrected = 4
 
 	// NsenterCmd is the nsenter command
-	NsenterCmd = "nsenter --mount=/proc/1/ns/mnt --ipc=/proc/1/ns/ipc --net=/proc/1/ns/net --uts=/proc/1/ns/uts "
+	NsenterCmd = "nsenter --mount=/proc/1/ns/mnt --ipc=/proc/1/ns/ipc --net=/proc/1/ns/net --uts=/proc/1/ns/uts"
 
 	// socketPath is path of connector sock
 	socketPath = "/host/etc/csi-tool/connector.sock"
