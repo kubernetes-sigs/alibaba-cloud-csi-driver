f47785c4fdd8626c58cc34c3a149362adac4d90d
tydra-wang <wjunjie96@outlook.com>
2023-08-10 10:12:58 +0800

    add ContainerizedFuseMounter implementation

---
+++ pkg/mounter/fuse_containerized_mounter.go
@@ -0,0 +1,288 @@
+package mounter
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"hash/fnv"
+	"strings"
+	"time"
+
+	"github.com/sirupsen/logrus"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/rand"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
+	watchtools "k8s.io/client-go/tools/watch"
+	mountutils "k8s.io/mount-utils"
+)
+
+const fuseMountTimeout = time.Minute
+
+const (
+	FuseTypeLabelKey          = "csi.alibabacloud.com/fuse-type"
+	FuseVolumeIdLabelKey      = "csi.alibabacloud.com/volume-id"
+	FuseMountPathHashLabelKey = "csi.alibabacloud.com/mount-path-hash"
+)
+
+type FuseMounterType interface {
+	name() string
+	buildPodSpec(source, target, fstype string, options, mountFlags []string, nodeName string, config FuseContainerConfig) corev1.PodSpec
+}
+
+type FuseContainerConfig struct {
+	Resources corev1.ResourceRequirements
+	Image     string
+	Extra     map[string]string
+}
+
+func extractFuseContainerConfig(configmap *corev1.ConfigMap, fuseType FuseMounterType) (config FuseContainerConfig) {
+	config.Resources.Requests = make(corev1.ResourceList)
+	config.Resources.Limits = make(corev1.ResourceList)
+	content := configmap.Data["fuse-"+fuseType.name()]
+	for _, line := range strings.Split(content, "\n") {
+		line = strings.TrimSpace(line)
+		if line == "" {
+			continue
+		}
+		elements := strings.SplitN(line, "=", 2)
+		invalid := false
+		if len(elements) == 2 {
+			switch elements[0] {
+			case "cpu-request", "cpu-limit", "memory-request", "memory-limit":
+				quantity, err := resource.ParseQuantity(elements[1])
+				if err != nil {
+					invalid = true
+					break
+				}
+				s := strings.Split(elements[0], "-")
+				resourceName := corev1.ResourceName(s[0])
+				switch s[1] {
+				case "request":
+					config.Resources.Requests[resourceName] = quantity
+				case "limit":
+					config.Resources.Limits[resourceName] = quantity
+				}
+			case "image":
+				config.Image = elements[1]
+			default:
+				if config.Extra == nil {
+					config.Extra = make(map[string]string)
+				}
+				config.Extra[elements[0]] = elements[1]
+			}
+		} else {
+			invalid = true
+		}
+		if invalid {
+			logrus.Warnf("invalid configuration line: %q", line)
+		}
+	}
+	return config
+}
+
+type ContainerizedFuseMounterFactory struct {
+	fuseType            FuseMounterType
+	config              FuseContainerConfig
+	nodeName, namespace string
+	client              kubernetes.Interface
+}
+
+func NewContainerizedFuseMounterFactory(
+	fuseType FuseMounterType,
+	configmap *corev1.ConfigMap,
+	client kubernetes.Interface,
+	nodeName string,
+) *ContainerizedFuseMounterFactory {
+	return &ContainerizedFuseMounterFactory{
+		fuseType:  fuseType,
+		nodeName:  nodeName,
+		config:    extractFuseContainerConfig(configmap, fuseType),
+		namespace: "kube-system",
+		client:    client,
+	}
+}
+
+func (fac *ContainerizedFuseMounterFactory) NewFuseMounter(volumeId string) *ContainerizedFuseMounter {
+	return &ContainerizedFuseMounter{
+		volumeId:  volumeId,
+		nodeName:  fac.nodeName,
+		namespace: fac.namespace,
+		client:    fac.client,
+		log: logrus.WithFields(logrus.Fields{
+			"fuse-type": fac.fuseType.name(),
+			"volume-id": volumeId,
+		}),
+		FuseMounterType: fac.fuseType,
+		fuseConfig:      fac.config,
+		Interface:       mountutils.New(""),
+	}
+}
+
+type ContainerizedFuseMounter struct {
+	volumeId   string
+	nodeName   string
+	namespace  string
+	client     kubernetes.Interface
+	log        *logrus.Entry
+	fuseConfig FuseContainerConfig
+	FuseMounterType
+	mountutils.Interface
+}
+
+func (mounter *ContainerizedFuseMounter) Mount(source string, target string, fstype string, options []string) error {
+	for _, option := range options {
+		if option == "bind" {
+			return mounter.Interface.Mount(source, target, fstype, options)
+		}
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), fuseMountTimeout)
+	defer cancel()
+	err := mounter.launchFusePod(ctx, source, target, fstype, options, nil)
+	if err != nil {
+		return err
+	}
+
+	// check mountpoint
+	notMounted, err := mounter.IsLikelyNotMountPoint(target)
+	if err != nil {
+		return err
+	}
+	if notMounted {
+		return errors.New("fuse pod launched but target still not mounted")
+	}
+	return nil
+}
+
+func (mounter *ContainerizedFuseMounter) Unmount(target string) error {
+	unmountErr := mounter.Interface.Unmount(target)
+	if unmountErr != nil {
+		mounter.log.Errorf("umount %s: %v", target, unmountErr)
+	} else {
+		mounter.log.Infof("successfully unmount %s", target)
+	}
+
+	_, listOptions := mounter.labelsAndListOptionsFor(target)
+	deletePodErr := mounter.client.CoreV1().Pods(mounter.namespace).DeleteCollection(context.TODO(), metav1.DeleteOptions{}, listOptions)
+	if deletePodErr != nil {
+		mounter.log.Errorf("failed to delete fuse pod: %v", unmountErr)
+	} else {
+		mounter.log.Infof("deleted fuse pod for %s", target)
+	}
+
+	// Only return nil when unmounted successfully and fuse pods are deleted
+	if unmountErr != nil {
+		return unmountErr
+	}
+	if deletePodErr != nil {
+		return deletePodErr
+	}
+	return nil
+}
+
+func (mounter *ContainerizedFuseMounter) labelsAndListOptionsFor(target string) (map[string]string, metav1.ListOptions) {
+	labels := map[string]string{
+		FuseTypeLabelKey:          mounter.name(),
+		FuseVolumeIdLabelKey:      mounter.volumeId,
+		FuseMountPathHashLabelKey: computeMountPathHash(target),
+	}
+	listOptions := metav1.ListOptions{
+		FieldSelector: fields.OneTermEqualSelector("spec.nodeName", mounter.nodeName).String(),
+		LabelSelector: metav1.FormatLabelSelector(&metav1.LabelSelector{MatchLabels: labels}),
+	}
+	return labels, listOptions
+}
+
+func (mounter *ContainerizedFuseMounter) launchFusePod(ctx context.Context, source, target, fstype string, options, mountFlags []string) error {
+	podClient := mounter.client.CoreV1().Pods(mounter.namespace)
+	labels, listOptions := mounter.labelsAndListOptionsFor(target)
+	podList, err := podClient.List(ctx, listOptions)
+	if err != nil {
+		return err
+	}
+	var pods []corev1.Pod
+	for _, pod := range podList.Items {
+		if pod.DeletionTimestamp.IsZero() {
+			if isFusePodReady(&pod) {
+				mounter.log.Infof("%s is already mounted by pod %s", target, pod.Name)
+				return nil
+			}
+			pods = append(pods, pod)
+		}
+	}
+
+	var fusePod *corev1.Pod
+	if len(pods) == 0 {
+		// create fuse pod for target
+		mounter.log.Infof("creating fuse pod for %s", target)
+		var rawPod corev1.Pod
+		rawPod.GenerateName = fmt.Sprintf("csi-%s-%s-", mounter.name(), mounter.volumeId)
+		rawPod.Labels = labels
+		rawPod.Spec = mounter.buildPodSpec(source, target, fstype, options, mountFlags, mounter.nodeName, mounter.fuseConfig)
+		createdPod, err := podClient.Create(ctx, &rawPod, metav1.CreateOptions{})
+		if err != nil {
+			return err
+		}
+		mounter.log.Infof("created fuse pod %s for %s", createdPod.Name, target)
+		fusePod = createdPod
+	} else {
+		if len(pods) > 1 {
+			mounter.log.Warnf("%d duplicated fuse pods for %s", len(pods), target)
+			// TODO: clean duplicated fuse pods
+		}
+		mounter.log.Infof("found existed fuse pod %s for %s", pods[0].Name, target)
+		fusePod = &pods[0]
+	}
+
+	mounter.log.Infof("wait util pod %s is ready", fusePod.Name)
+	fieldSelector := fields.OneTermEqualSelector("metadata.name", fusePod.Name).String()
+	lw := &cache.ListWatch{
+		ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
+			options.FieldSelector = fieldSelector
+			return podClient.List(ctx, options)
+		},
+		WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
+			options.FieldSelector = fieldSelector
+			return podClient.Watch(ctx, options)
+		},
+	}
+	event, err := watchtools.Until(ctx, fusePod.ResourceVersion, lw, func(event watch.Event) (bool, error) {
+		if event.Type == watch.Deleted {
+			return true, fmt.Errorf("fuse pod %s was deleted", fusePod.Name)
+		}
+		pod, ok := event.Object.(*corev1.Pod)
+		return ok && isFusePodReady(pod), nil
+	})
+	if err != nil {
+		return err
+	}
+	if pod, ok := event.Object.(*corev1.Pod); !(ok && isFusePodReady(pod)) {
+		return fmt.Errorf("failed to wait for pod %s to be ready", fusePod.Name)
+	}
+	mounter.log.Infof("fuse pod %s is ready", fusePod.Name)
+
+	return nil
+}
+
+func isFusePodReady(pod *corev1.Pod) bool {
+	if pod.Status.Phase != corev1.PodRunning {
+		return false
+	}
+	for _, cond := range pod.Status.Conditions {
+		if cond.Type == corev1.PodReady && cond.Status == corev1.ConditionTrue {
+			return true
+		}
+	}
+	return false
+}
+
+func computeMountPathHash(target string) string {
+	hasher := fnv.New32a()
+	hasher.Write([]byte(target))
+	return rand.SafeEncodeString(fmt.Sprint(hasher.Sum32()))
+}
---
+++ pkg/mounter/ossfs.go
@@ -0,0 +1,100 @@
+package mounter
+
+import (
+	"strings"
+
+	corev1 "k8s.io/api/core/v1"
+	mountutils "k8s.io/mount-utils"
+	"k8s.io/utils/pointer"
+
+	"github.com/kubernetes-sigs/alibaba-cloud-csi-driver/pkg/utils"
+	"github.com/sirupsen/logrus"
+)
+
+var FuseOssfs FuseMounterType = &fuseOssfs{}
+
+type fuseOssfs struct{}
+
+func (f *fuseOssfs) name() string {
+	return "ossfs"
+}
+
+func (f *fuseOssfs) buildPodSpec(source, target, fstype string, options, mountFlags []string, nodeName string, config FuseContainerConfig) (spec corev1.PodSpec) {
+	hostPathDirectoryType := corev1.HostPathDirectory
+	hostPathFileOrCreate := corev1.HostPathFileOrCreate
+	kubeletDirVolume := corev1.Volume{
+		Name: "kubelet-dir",
+		VolumeSource: corev1.VolumeSource{
+			HostPath: &corev1.HostPathVolumeSource{
+				Path: utils.KubeletRootDir,
+				Type: &hostPathDirectoryType,
+			},
+		},
+	}
+	metricsDirVolume := corev1.Volume{
+		Name: "metrics-dir",
+		VolumeSource: corev1.VolumeSource{
+			HostPath: &corev1.HostPathVolumeSource{
+				Path: "/var/run/ossfs",
+				Type: &hostPathDirectoryType,
+			},
+		},
+	}
+	passwdFileVolume := corev1.Volume{
+		Name: "passwd-ossfs",
+		VolumeSource: corev1.VolumeSource{
+			HostPath: &corev1.HostPathVolumeSource{
+				Path: "/etc/passwd-ossfs",
+				Type: &hostPathFileOrCreate,
+			},
+		},
+	}
+	spec.Volumes = []corev1.Volume{kubeletDirVolume, metricsDirVolume, passwdFileVolume}
+
+	switch dbglevel := config.Extra["dbglevel"]; dbglevel {
+	case "info", "warn", "err", "crit":
+		alreadySet := false
+		for _, option := range options {
+			if strings.Contains(option, "dbglevel") {
+				alreadySet = true
+				break
+			}
+		}
+		if !alreadySet {
+			options = append(options, "dbglevel="+dbglevel)
+		}
+	default:
+		logrus.Warnf("unknown ossfs dbglevel: %s", dbglevel)
+	}
+	args := mountutils.MakeMountArgs(source, target, "", options)
+	args = append(args, mountFlags...)
+	propagationContainerToHost := corev1.MountPropagationHostToContainer
+	container := corev1.Container{
+		Name:  "fuse-mounter",
+		Image: config.Image,
+		Args:  args,
+		VolumeMounts: []corev1.VolumeMount{
+			{
+				Name:             kubeletDirVolume.Name,
+				MountPath:        utils.KubeletRootDir,
+				MountPropagation: &propagationContainerToHost,
+			}, {
+				Name:      metricsDirVolume.Name,
+				MountPath: metricsDirVolume.HostPath.Path,
+			}, {
+				Name:      passwdFileVolume.Name,
+				MountPath: passwdFileVolume.HostPath.Path,
+			},
+		},
+		Resources: config.Resources,
+		SecurityContext: &corev1.SecurityContext{
+			Privileged: pointer.BoolPtr(true),
+		},
+	}
+	spec.Containers = []corev1.Container{container}
+	spec.RestartPolicy = corev1.RestartPolicyOnFailure
+	spec.NodeName = nodeName
+	spec.HostNetwork = true
+	spec.PriorityClassName = "system-node-critical"
+	return
+}
