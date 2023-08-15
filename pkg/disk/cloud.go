bd1b2990831a3c357bd2857cde123cc98c874b01
huweiwen <huweiwen.hww@alibaba-inc.com>
2023-08-15 15:52:49 +0800

    disk: call DescribeDisks for allocatable count

--- pkg/disk/cloud.go
+++ pkg/disk/cloud.go
@@ -1042,3 +1042,12 @@ func getDefaultDiskTags(diskVol *diskVolumeArgs) []ecs.CreateDiskTag {
 	}
 	return diskTags
 }
+
+func IsDiskCreatedByCsi(disk ecs.Disk) bool {
+	for _, tag := range disk.Tags.Tag {
+		if tag.TagKey == DISKTAGKEY2 {
+			return tag.TagValue == DISKTAGVALUE2
+		}
+	}
+	return false
+}
