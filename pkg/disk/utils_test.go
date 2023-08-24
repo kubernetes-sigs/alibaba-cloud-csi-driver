4cff81beb1a2b4dbcc669a26cf063d7d11744e3b
huweiwen <huweiwen.hww@alibaba-inc.com>
2023-08-24 13:00:59 +0800

    cleanup

--- pkg/disk/utils_test.go
+++ pkg/disk/utils_test.go
@@ -17,11 +17,10 @@ package disk
 
 import (
 	"fmt"
-	"gopkg.in/h2non/gock.v1"
 	"os"
-	"os/exec"
-	"path/filepath"
 	"testing"
+
+	"gopkg.in/h2non/gock.v1"
 
 	fakesnapshotv1 "github.com/kubernetes-csi/external-snapshotter/client/v4/clientset/versioned/fake"
 	"github.com/stretchr/testify/assert"
@@ -32,52 +31,13 @@ func TestIsFileExisting(t *testing.T) {
 	existsFilePath := ".tmp/test.data"
 	notExistsFilePath := ".tmp/notexists.data"
 
-	err := EnsureDir(".tmp/")
+	err := os.MkdirAll(".tmp", 0o777)
 	assert.Nil(t, err)
 	err = createDest(existsFilePath)
 	assert.Nil(t, err)
 	assert.True(t, IsFileExisting(existsFilePath))
 	assert.False(t, IsFileExisting(notExistsFilePath))
-	RemoveContents(".tmp/")
-}
-
-func EnsureDir(target string) error {
-	mdkirCmd := "mkdir"
-	_, err := exec.LookPath(mdkirCmd)
-	if err != nil {
-		if err == exec.ErrNotFound {
-			return fmt.Errorf("%q executable not found in $PATH", mdkirCmd)
-		}
-		return err
-	}
-
-	mkdirArgs := []string{"-p", target}
-	//log.Infof("mkdir for folder, the command is %s %v", mdkirCmd, mkdirArgs)
-	_, err = exec.Command(mdkirCmd, mkdirArgs...).CombinedOutput()
-	if err != nil {
-		return fmt.Errorf("mkdir for folder error: %v", err)
-	}
-	return nil
-}
-
-func RemoveContents(dir string) error {
-	d, err := os.Open(dir)
-	if err != nil {
-		return err
-	}
-	defer os.Remove(dir)
-	defer d.Close()
-	names, err := d.Readdirnames(-1)
-	if err != nil {
-		return err
-	}
-	for _, name := range names {
-		err = os.RemoveAll(filepath.Join(dir, name))
-		if err != nil {
-			return err
-		}
-	}
-	return nil
+	os.RemoveAll(".tmp")
 }
 
 func TestValidateDiskType(t *testing.T) {
@@ -163,7 +123,7 @@ func TestRetryGetInstanceDoc(t *testing.T) {
 		expectErr    bool
 	}{
 		{
-			reString:     "{\"region-id\": \"cn-hangzhou\", \"instance-id\": \"i-xxxxx\", \"zone-id\": \"cn-hangzhou-d\"}",
+			reString:     `{"region-id": "cn-hangzhou", "instance-id": "i-xxxxx", "zone-id": "cn-hangzhou-d"}`,
 			expectZoneId: "cn-hangzhou-d",
 			expectErr:    false,
 		},
