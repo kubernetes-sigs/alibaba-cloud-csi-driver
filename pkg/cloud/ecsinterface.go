ead1d9fc69157bb62698a668f8bec60332037732
huweiwen <huweiwen.hww@alibaba-inc.com>
2023-08-30 11:19:25 +0800

    add mock for ECS DescribeRegions

--- pkg/cloud/ecsinterface.go
+++ pkg/cloud/ecsinterface.go
@@ -5,4 +5,5 @@ import "github.com/aliyun/alibaba-cloud-sdk-go/services/ecs"
 type ECSInterface interface {
 	DescribeAvailableResource(request *ecs.DescribeAvailableResourceRequest) (response *ecs.DescribeAvailableResourceResponse, err error)
 	DeleteDisk(request *ecs.DeleteDiskRequest) (response *ecs.DeleteDiskResponse, err error)
+	DescribeRegions(request *ecs.DescribeRegionsRequest) (response *ecs.DescribeRegionsResponse, err error)
 }
