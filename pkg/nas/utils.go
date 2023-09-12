599583e0a30fcb19047445cbbb9bd79b51ffa711
tydra-wang <wjunjie96@outlook.com>
2023-09-12 18:58:11 +0800

    perf(nas): acquire a lock when NodePublishVolume/NodeUnpublishVolume

--- pkg/nas/utils.go
+++ pkg/nas/utils.go
@@ -526,17 +526,16 @@ func unmountLosetupPv(mounter mountutils.Interface, mountPoint string) error {
 	imgFile := filepath.Join(nfsPath, LoopImgFile)
 	lockFile := filepath.Join(nfsPath, LoopLockFile)
 	if utils.IsFileExisting(imgFile) {
+		log.Infof("cleanup the losetup mountpoint due to the existence of image file %s", imgFile)
 		if utils.IsFileExisting(lockFile) {
 			if err := os.Remove(lockFile); err != nil {
 				return fmt.Errorf("checkLosetupUnmount: remove lock file error %v", err)
 			}
 		}
-		if err := mounter.Unmount(nfsPath); err != nil {
+		if err := cleanupMountpoint(mounter, nfsPath); err != nil {
 			return fmt.Errorf("checkLosetupUnmount: umount nfs path error %v", err)
 		}
 		log.Infof("Losetup Unmount successful %s", mountPoint)
-	} else {
-		log.Infof("Losetup Unmount, image file not exist, skipping %s", mountPoint)
 	}
 	return nil
 }
@@ -613,4 +612,14 @@ func saveVolumeData(opt *Options, mountPath string) {
 			log.Warnf("NodePublishVolume: append nas volume spec to %s with error: %s", fileName, err.Error())
 		}
 	}
+}
+
+func cleanupMountpoint(mounter mountutils.Interface, mountPath string) (err error) {
+	forceUnmounter, ok := mounter.(mountutils.MounterForceUnmounter)
+	if ok {
+		err = mountutils.CleanupMountWithForce(mountPath, forceUnmounter, false, time.Second*30)
+	} else {
+		err = mountutils.CleanupMountPoint(mountPath, mounter, false)
+	}
+	return
 }
