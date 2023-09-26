10e1eb6236551f1e3c2bedd010d98be10cb8e2da
AlbeeSo <suyashi1321@163.com>
2023-09-26 11:47:25 +0800

    feat: upgrate ossfs version to 1.88.2

--- build/fuse-clients/ossfs/install-ossfs.sh
+++ build/fuse-clients/ossfs/install-ossfs.sh
@@ -4,7 +4,7 @@ echo "TARGETPLATFORM: $TARGETPLATFORM"
 echo "installing ossfs"
 case $TARGETPLATFORM in
     linux/amd64)
-        yum install -y http://ack-csiplugin.oss-cn-hangzhou.aliyuncs.com/pre/ossfs/ossfs_1.88.1_centos8_x86_64.rpm
+        yum install -y http://ack-csiplugin.oss-cn-hangzhou.aliyuncs.com/pre/ossfs/ossfs_1.88.2_centos8.0_x86_64.rpm
         ;;
     linux/arm64)
         yum install -y \
