ce8db61ca908ccbddbbb264f056560c0a756b2da
huweiwen <huweiwen.hww@alibaba-inc.com>
2023-09-28 01:30:43 +0800

    credentials: add support for ACCESS_KEY_ID env
    align with old utils.GetAccessControl. However, mark them as deprecated

--- pkg/credentials/credential.go
+++ pkg/credentials/credential.go
@@ -6,10 +6,29 @@ import (
 
 	"github.com/alibabacloud-go/tea/tea"
 	alicred "github.com/aliyun/credentials-go/credentials"
+	"github.com/sirupsen/logrus"
 	"k8s.io/utils/clock"
 )
 
+func MigrateEnv() {
+	accessKeyID := os.Getenv("ACCESS_KEY_ID")
+	accessSecret := os.Getenv("ACCESS_KEY_SECRET")
+	if accessKeyID != "" || accessSecret != "" {
+		logrus.Warnf("enviroment variable ACCESS_KEY_ID and ACCESS_KEY_SECRET is deprecated, please use %s and %s instead.", alicred.EnvVarAccessKeyIdNew, alicred.EnvVarAccessKeySecret)
+
+		// if ALIBABA_CLOUD_* not set, use ACCESS_KEY_ID and ACCESS_KEY_SECRET
+		if os.Getenv(alicred.EnvVarAccessKeyIdNew) == "" && accessKeyID != "" {
+			os.Setenv(alicred.EnvVarAccessKeyIdNew, accessKeyID)
+		}
+		if os.Getenv(alicred.EnvVarAccessKeySecret) == "" && accessSecret != "" {
+			os.Setenv(alicred.EnvVarAccessKeySecret, accessSecret)
+		}
+	}
+}
+
 func NewCredential() (alicred.Credential, error) {
+	MigrateEnv()
+
 	credential, err := alicred.NewCredential(nil)
 	if err == nil {
 		return credential, nil
