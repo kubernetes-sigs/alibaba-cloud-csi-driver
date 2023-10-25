kubernetes-sigs/alibaba-cloud-csi-driver

commit 18f98b1c2b85ff2ec2072cf291a3e88c036fc4ea

utils: fix incorrect blkio cgroup path
for non-BestEffort QoS pods

pkg/utils/io.go
@@ -3,12 +3,13 @@ package utils
 import (
 	"errors"
 	"fmt"
-	"github.com/container-storage-interface/spec/lib/go/csi"
-	log "github.com/sirupsen/logrus"
 	"os/exec"
 	"path/filepath"
 	"strconv"
 	"strings"
+
+	"github.com/container-storage-interface/spec/lib/go/csi"
+	log "github.com/sirupsen/logrus"
 )
 
 // SetVolumeIOLimit config io limit for device
@@ -70,11 +71,15 @@ func SetVolumeIOLimit(devicePath string, req *csi.NodePublishVolumeRequest) erro
 	}
 	// /sys/fs/cgroup/blkio/kubepods.slice/kubepods-besteffort.slice/kubepods-besteffort-podaadcc749_6776_4933_990d_d50f260f5d46.slice/blkio.throttle.write_bps_device
 	podUID = strings.ReplaceAll(podUID, "-", "_")
-	podBlkIOPath := filepath.Join("/sys/fs/cgroup/blkio/kubepods.slice/kubepods-besteffort.slice", "kubepods-besteffort-pod"+podUID+".slice")
-	if !IsHostFileExist(podBlkIOPath) {
-		podBlkIOPath = filepath.Join("/sys/fs/cgroup/blkio/kubepods.slice/kubepods-burstable.slice", "kubepods-besteffort-pod"+podUID+".slice")
+	podBlkIOPath := ""
+	for _, qosClass := range []string{"besteffort", "burstable", "guaranteed"} {
+		p := fmt.Sprintf("/sys/fs/cgroup/blkio/kubepods.slice/kubepods-%s.slice/kubepods-%s-pod%s.slice", qosClass, qosClass, podUID)
+		if IsHostFileExist(p) {
+			podBlkIOPath = p
+			break
+		}
 	}
-	if !IsHostFileExist(podBlkIOPath) {
+	if podBlkIOPath == "" {
 		log.Errorf("Volume(%s), Cannot get pod blkio/cgroup path: %s", req.VolumeId, podBlkIOPath)
 		return errors.New("Cannot get pod blkio/cgroup path: " + podBlkIOPath)
 	}

commit 63057cebee3ee66637629e3b65f1045146c5676b

utils: fix guaranteed QoS blkio cgroup path
In 18f98b1, we fixed burstable path, but guaranteed QoS does not follow this pattern, and is still wrong.

Fixes: 18f98b1c2b85ff2ec2072cf291a3e88c036fc4ea

pkg/utils/io.go
@@ -72,16 +72,19 @@ func SetVolumeIOLimit(devicePath string, req *csi.NodePublishVolumeRequest) erro
 	// /sys/fs/cgroup/blkio/kubepods.slice/kubepods-besteffort.slice/kubepods-besteffort-podaadcc749_6776_4933_990d_d50f260f5d46.slice/blkio.throttle.write_bps_device
 	podUID = strings.ReplaceAll(podUID, "-", "_")
 	podBlkIOPath := ""
-	for _, qosClass := range []string{"besteffort", "burstable", "guaranteed"} {
-		p := fmt.Sprintf("/sys/fs/cgroup/blkio/kubepods.slice/kubepods-%s.slice/kubepods-%s-pod%s.slice", qosClass, qosClass, podUID)
+	for _, p := range []string{
+		fmt.Sprintf("/sys/fs/cgroup/blkio/kubepods.slice/kubepods-besteffort.slice/kubepods-besteffort-pod%s.slice", podUID),
+		fmt.Sprintf("/sys/fs/cgroup/blkio/kubepods.slice/kubepods-burstable.slice/kubepods-burstable-pod%s.slice", podUID),
+		fmt.Sprintf("/sys/fs/cgroup/blkio/kubepods.slice/kubepods-pod%s.slice", podUID),
+	} {
 		if IsHostFileExist(p) {
 			podBlkIOPath = p
 			break
 		}
 	}
 	if podBlkIOPath == "" {
-		log.Errorf("Volume(%s), Cannot get pod blkio/cgroup path: %s", req.VolumeId, podBlkIOPath)
-		return errors.New("Cannot get pod blkio/cgroup path: " + podBlkIOPath)
+		log.Errorf("Volume(%s), pod blkio/cgroup path not found", req.VolumeId)
+		return errors.New("pod blkio/cgroup path not found")
 	}
 
 	// io limit set to blkio limit files