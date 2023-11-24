kubernetes-sigs/alibaba-cloud-csi-driver

commit 218de93c29738db94340d6bad324fc4af5f9abf5

introduce centralized node metadata
We are querying for node metadata everywhere in the code, from multiple
sources. But existing implementation is inconsistent, not robust to unavailable
sources. And there are many duplicated codes in different drivers.

The new implemetation fetches node metadata lazily from env variables, ECS
metadata server or Kunernetes Node resource, cache the result/error, and
fallback automatically to the next available source.

The core of this framework is MetadataProvider interface. Each metadata source
implements this interface.  The core Metadata struct stores a list of other
providers, and implement MetadataProvider interface itself.  It returns the
first non-error result from underlying providers.

Some sources fetch data from remote system, which may fail.  For convinience,
the framework also provide an MetadataFetcher interface.  Implemtations can
return a simpler provider that just parse from memory, or ErrUnknownMetadataKey
indicating it does not know this metadata, or any other error indicating the
source is not working.  lazyInitProvider implements MetadataProvider and
accepts a fetcher.  It invokes the fetcher when needed, and cache the resulting
provider or error.

Also add httpmock to test ECS metadata behavior

pkg/cloud/metadata/metadata.go
@@ -1,0 +1,132 @@
+package metadata
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"sync"
+
+	"github.com/sirupsen/logrus"
+	utilerrors "k8s.io/apimachinery/pkg/util/errors"
+	corev1 "k8s.io/client-go/kubernetes/typed/core/v1"
+)
+
+type MetadataKey int
+
+const (
+	RegionID     MetadataKey = iota
+	ZoneID       MetadataKey = iota
+	InstanceID   MetadataKey = iota
+	InstanceType MetadataKey = iota
+)
+
+var ErrUnknownMetadataKey = errors.New("unknown metadata key")
+
+const DISABLE_ECS_ENV = "ALIBABA_CLOUD_NO_ECS_METADATA"
+const KUBE_NODE_NAME_ENV = "KUBE_NODE_NAME"
+
+type MetadataProvider interface {
+	Get(key MetadataKey) (string, error)
+}
+
+type Metadata struct {
+	providers []MetadataProvider
+}
+
+type MetadataFetcher interface {
+	FetchFor(key MetadataKey) (MetadataProvider, error)
+}
+
+type lazyInitProvider struct {
+	provider MetadataProvider
+	err      error
+	initMu   sync.Mutex
+	fetcher  MetadataFetcher
+}
+
+func (p *lazyInitProvider) Get(key MetadataKey) (string, error) {
+	if p.provider == nil && p.err == nil {
+		p.initMu.Lock()
+		if p.provider == nil && p.err == nil {
+			provider, err := p.fetcher.FetchFor(key)
+			if err == ErrUnknownMetadataKey {
+				p.initMu.Unlock()
+				return "", err
+			}
+			if err != nil {
+				err = fmt.Errorf("%T failed: %w", p.fetcher, err)
+				// print a warning if we failed to get a value,
+				// because the error is hide if other providers succeed
+				logrus.Warn(err)
+			}
+			p.fetcher = nil
+			p.provider = provider
+			p.err = err
+		}
+		p.initMu.Unlock()
+	}
+	if p.err != nil {
+		return "", p.err
+	}
+	return p.provider.Get(key)
+}
+
+func NewMetadata() *Metadata {
+	return &Metadata{
+		providers: []MetadataProvider{
+			&ENVMetadata{},
+		},
+	}
+}
+
+func (m *Metadata) EnableEcs(httpRT http.RoundTripper) {
+	if os.Getenv(DISABLE_ECS_ENV) != "" {
+		logrus.Infof("ECS metadata is disabled by environment variable %s", DISABLE_ECS_ENV)
+		return
+	}
+	m.providers = append(m.providers, &lazyInitProvider{
+		fetcher: &EcsFetcher{httpRT: httpRT},
+	})
+
+}
+
+func (m *Metadata) EnableKubernetes(nodeClient corev1.NodeInterface) {
+	nodeName := os.Getenv(KUBE_NODE_NAME_ENV)
+	if nodeName == "" {
+		logrus.Warnf("%s environment variable is not set, skipping Kubernetes metadata", KUBE_NODE_NAME_ENV)
+		return
+	}
+	m.providers = append(m.providers, &lazyInitProvider{
+		fetcher: &KubernetesMetadataFetcher{
+			client:   nodeClient,
+			nodeName: nodeName,
+		},
+	})
+}
+
+func (m *Metadata) Get(key MetadataKey) (string, error) {
+	errors := []error{}
+	for _, p := range m.providers {
+		v, err := p.Get(key)
+		if err == nil {
+			return v, nil
+		}
+		if err == ErrUnknownMetadataKey {
+			continue
+		}
+		errors = append(errors, err)
+	}
+	if len(errors) == 0 {
+		return "", ErrUnknownMetadataKey
+	}
+	return "", utilerrors.NewAggregate(errors)
+}
+
+func MustGet(m MetadataProvider, key MetadataKey) string {
+	value, err := m.Get(key)
+	if err != nil {
+		panic(err)
+	}
+	return value
+}

pkg/cloud/metadata/metadata_test.go
@@ -1,0 +1,84 @@
+package metadata
+
+import (
+	"context"
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/jarcoal/httpmock"
+	"github.com/stretchr/testify/assert"
+	fake "k8s.io/client-go/kubernetes/fake"
+)
+
+func TestEcsUnreachable(t *testing.T) {
+	t.Parallel()
+	trans := httpmock.NewMockTransport()
+	trans.RegisterResponder("GET", ECSIdentityEndpoint,
+		httpmock.NewErrorResponder(errors.New("what ever")).Delay(11*time.Second))
+	// Should print a suggestion about disabling ECS metadata
+	m := NewMetadata()
+	m.EnableEcs(trans)
+	_, err := m.Get(RegionID)
+	assert.True(t, errors.Is(err, context.DeadlineExceeded))
+}
+
+func TestEcsDisableByEnv(t *testing.T) {
+	t.Setenv(DISABLE_ECS_ENV, "true")
+	m := NewMetadata()
+	c1 := len(m.providers)
+	m.EnableEcs(httpmock.NewMockTransport())
+	assert.Equal(t, c1, len(m.providers))
+}
+
+func TestCreateK8s(t *testing.T) {
+	m := NewMetadata()
+
+	node := testNode.DeepCopy()
+	node.Labels = map[string]string{
+		"topology.kubernetes.io/region": "cn-beijing",
+	}
+	client := fake.NewSimpleClientset(node).CoreV1().Nodes()
+	t.Run("no node name", func(t *testing.T) {
+		m.EnableKubernetes(client)
+		_, err := m.Get(RegionID)
+		assert.Equal(t, ErrUnknownMetadataKey, err)
+	})
+
+	t.Run("ok", func(t *testing.T) {
+		t.Setenv(KUBE_NODE_NAME_ENV, testNode.Name)
+		m.EnableKubernetes(client)
+		assert.Equal(t, "cn-beijing", MustGet(m, RegionID))
+	})
+}
+
+func TestGetFromEnv(t *testing.T) {
+	t.Setenv("REGION_ID", "cn-hangzhou")
+	m := NewMetadata()
+	assert.Equal(t, "cn-hangzhou", MustGet(m, RegionID))
+}
+
+func fakeMetadata(t *testing.T) *Metadata {
+	trans := httpmock.NewMockTransport()
+	trans.RegisterResponder("GET", ECSIdentityEndpoint, httpmock.NewStringResponder(200, testIdDoc))
+
+	m := NewMetadata()
+	m.EnableEcs(trans)
+	return m
+}
+
+func TestCreateEcs(t *testing.T) {
+	t.Parallel()
+	m := fakeMetadata(t)
+	region, err := m.Get(RegionID)
+	assert.NoError(t, err)
+	assert.Equal(t, "cn-beijing", region)
+}
+
+func TestGetUnknownKey(t *testing.T) {
+	t.Parallel()
+	m := fakeMetadata(t)
+
+	_, err := m.Get(99999)
+	assert.Equal(t, ErrUnknownMetadataKey, err)
+}

go.mod
@@ -15,6 +15,7 @@ require (
 	github.com/gogo/protobuf v1.3.2
 	github.com/golang/mock v1.6.0
 	github.com/golang/protobuf v1.5.2
+	github.com/jarcoal/httpmock v1.3.1
 	github.com/kubernetes-csi/drivers v1.0.2
 	github.com/kubernetes-csi/external-snapshotter/client/v4 v4.2.0
 	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible

go.sum
@@ -280,6 +280,8 @@ github.com/imdario/mergo v0.3.5/go.mod h1:2EnlNZ0deacrJVfApfmtdGgDfMuh/nq6Ok1EcJ
 github.com/imdario/mergo v0.3.7 h1:Y+UAYTZ7gDEuOfhxKWy+dvb5dRQ6rJjFSdX2HZY1/gI=
 github.com/imdario/mergo v0.3.7/go.mod h1:2EnlNZ0deacrJVfApfmtdGgDfMuh/nq6Ok1EcJh5FfA=
 github.com/inconshreveable/mousetrap v1.0.0/go.mod h1:PxqpIevigyE2G7u3NXJIT2ANytuPF1OarO4DADm73n8=
+github.com/jarcoal/httpmock v1.3.1 h1:iUx3whfZWVf3jT01hQTO/Eo5sAYtB2/rqaUuOtpInww=
+github.com/jarcoal/httpmock v1.3.1/go.mod h1:3yb8rc4BI7TCBhFY8ng0gjuLKJNquuDNiPaZjnENuYg=
 github.com/jmespath/go-jmespath v0.0.0-20180206201540-c2b33e8439af h1:pmfjZENx5imkbgOkpRUYLnmbU7UEFbjtDA2hxJ1ichM=
 github.com/jmespath/go-jmespath v0.0.0-20180206201540-c2b33e8439af/go.mod h1:Nht3zPeWKUH0NzdCt2Blrr5ys8VGpn0CEB0cQHVjt7k=
 github.com/jonboulle/clockwork v0.1.0 h1:VKV+ZcuP6l3yW9doeqz6ziZGgcynBVQO+obU0+0hcPo=
@@ -330,6 +332,7 @@ github.com/mattn/go-isatty v0.0.4/go.mod h1:M+lRXTBqGeGNdLjl/ufCoiOlB5xdOkqRJdNx
 github.com/mattn/go-runewidth v0.0.2/go.mod h1:LwmH8dsx7+W8Uxz3IHJYH5QSwggIsqBzpuz5H//U1FU=
 github.com/matttproud/golang_protobuf_extensions v1.0.1 h1:4hp9jkHxhMHkqkrB3Ix0jegS5sx/RkqARlsWZ6pIwiU=
 github.com/matttproud/golang_protobuf_extensions v1.0.1/go.mod h1:D8He9yQNgCq6Z5Ld7szi9bcBfOoFv/3dc6xSMkL2PC0=
+github.com/maxatome/go-testdeep v1.12.0 h1:Ql7Go8Tg0C1D/uMMX59LAoYK7LffeJQ6X2T04nTH68g=
 github.com/mitchellh/go-homedir v1.1.0/go.mod h1:SfyaCUpYCn1Vlf4IUYiD9fPX4A5wJrkLzIz1N1q0pr0=
 github.com/mitchellh/mapstructure v1.1.2/go.mod h1:FVVH3fgwuzCH5S8UJGiWEs2h04kUh9fWfEaFds41c1Y=
 github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd h1:TRLaZ9cD/w8PVh93nsPXa1VrQ6jlwL5oN8l14QlcNfg=

main.go
@@ -29,6 +29,7 @@ import (
 	"time"
 
 	"github.com/kubernetes-sigs/alibaba-cloud-csi-driver/pkg/agent"
+	"github.com/kubernetes-sigs/alibaba-cloud-csi-driver/pkg/cloud/metadata"
 	"github.com/kubernetes-sigs/alibaba-cloud-csi-driver/pkg/cpfs"
 	"github.com/kubernetes-sigs/alibaba-cloud-csi-driver/pkg/dbfs"
 	"github.com/kubernetes-sigs/alibaba-cloud-csi-driver/pkg/disk"
@@ -40,11 +41,14 @@ import (
 	"github.com/kubernetes-sigs/alibaba-cloud-csi-driver/pkg/metric"
 	"github.com/kubernetes-sigs/alibaba-cloud-csi-driver/pkg/nas"
 	"github.com/kubernetes-sigs/alibaba-cloud-csi-driver/pkg/om"
+	"github.com/kubernetes-sigs/alibaba-cloud-csi-driver/pkg/options"
 	_ "github.com/kubernetes-sigs/alibaba-cloud-csi-driver/pkg/options"
 	"github.com/kubernetes-sigs/alibaba-cloud-csi-driver/pkg/oss"
 	"github.com/kubernetes-sigs/alibaba-cloud-csi-driver/pkg/pov"
 	"github.com/kubernetes-sigs/alibaba-cloud-csi-driver/pkg/utils"
 	"github.com/prometheus/common/version"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 func init() {
@@ -169,6 +173,22 @@ func main() {
 
 	// Storage devops
 	go om.StorageOM()
+
+	// initialize node metadata
+	meta := metadata.NewMetadata()
+	meta.EnableEcs(http.DefaultTransport)
+
+	cfg, err := clientcmd.BuildConfigFromFlags(options.MasterURL, options.Kubeconfig)
+	if err != nil {
+		csilog.Log.Warnf("newGlobalConfig: build kubeconfig failed: %v", err)
+	} else {
+		kubeClient, err := kubernetes.NewForConfig(cfg)
+		if err != nil {
+			csilog.Log.Warnf("Error building kubernetes clientset: %v", err)
+		} else {
+			meta.EnableKubernetes(kubeClient.CoreV1().Nodes())
+		}
+	}
 
 	for i, driverName := range driverNames {
 		if !strings.Contains(driverName, TypePluginSuffix) && driverName != ExtenderAgent {

pkg/cloud/metadata/ecs.go
@@ -1,0 +1,112 @@
+package metadata
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/sirupsen/logrus"
+	"k8s.io/apimachinery/pkg/util/wait"
+)
+
+const (
+	ECSMetadataEndpoint = "http://100.100.100.200/latest/"
+	ECSIdentityEndpoint = ECSMetadataEndpoint + "dynamic/instance-identity/document"
+)
+
+type InstanceIdentityDocument struct {
+	ZoneID       string `json:"zone-id"`
+	RegionID     string `json:"region-id"`
+	InstanceID   string `json:"instance-id"`
+	InstanceType string `json:"instance-type"`
+	SerialNumber string `json:"serial-number"`
+}
+
+type ECSMetadata struct {
+	idDoc InstanceIdentityDocument
+}
+
+func NewECSMetadata(httpRT http.RoundTripper) (*ECSMetadata, error) {
+	m := &ECSMetadata{}
+
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+
+	httpClient := http.Client{
+		Transport: httpRT,
+	}
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, ECSIdentityEndpoint, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var lastErr error
+	err = wait.PollImmediateUntil(1*time.Second, func() (bool, error) {
+		if lastErr != nil {
+			logrus.Warnf("retrying ECS metadata: %v", lastErr)
+		}
+		resp, err := httpClient.Do(req)
+		if err != nil {
+			lastErr = err
+			return false, nil
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode != http.StatusOK {
+			lastErr = fmt.Errorf("unexpected status code %d", resp.StatusCode)
+			if resp.StatusCode >= 500 {
+				return false, nil // retry on server errors
+			} else {
+				return false, lastErr
+			}
+		}
+		if err := json.NewDecoder(resp.Body).Decode(&m.idDoc); err != nil {
+			return false, err
+		}
+		if m.idDoc.RegionID == "" {
+			return false, fmt.Errorf("missing region-id in ECS instance identity document, possibly not running on ECS")
+		}
+		return true, nil
+	}, ctx.Done())
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch ECS instance identity document: %w", lastErr)
+	}
+	return m, nil
+}
+
+func (m *ECSMetadata) Get(key MetadataKey) (string, error) {
+	switch key {
+	case RegionID:
+		return m.idDoc.RegionID, nil
+	case ZoneID:
+		return m.idDoc.ZoneID, nil
+	case InstanceID:
+		return m.idDoc.InstanceID, nil
+	case InstanceType:
+		return m.idDoc.InstanceType, nil
+	default:
+		return "", ErrUnknownMetadataKey
+	}
+}
+
+type EcsFetcher struct {
+	httpRT http.RoundTripper
+}
+
+func (f *EcsFetcher) FetchFor(key MetadataKey) (MetadataProvider, error) {
+	switch key {
+	case RegionID, ZoneID, InstanceID, InstanceType:
+	default:
+		return nil, ErrUnknownMetadataKey
+	}
+
+	ecs, err := NewECSMetadata(f.httpRT)
+	if errors.Is(err, context.DeadlineExceeded) {
+		logrus.Warnf("Hint: ECS metadata is only available when running on Alibaba Cloud ECS. "+
+			"Set %s environment variable to disable ECS metadata.", DISABLE_ECS_ENV)
+	}
+	return ecs, err
+}

pkg/cloud/metadata/ecs_test.go
@@ -1,0 +1,101 @@
+package metadata
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/jarcoal/httpmock"
+	"github.com/stretchr/testify/assert"
+)
+
+const testIdDoc = `{"zone-id":"cn-beijing-k","serial-number":"79773bd1-daef-4b82-af25-2008d4f938b4","instance-id":"i-2zec1slzwdzrwmvlr4w2","region-id":"cn-beijing","private-ipv4":"172.27.88.36","owner-account-id":"1857888888888866","mac":"00:16:3e:2c:f3:7f","image-id":"aliyun_3_x64_20G_alibase_20230727.vhd","instance-type":"ecs.g7.xlarge"}`
+
+var badCases = []struct {
+	Responder httpmock.Responder
+	Name      string
+	Retry     bool
+}{
+	{
+		Responder: httpmock.NewErrorResponder(errors.New("unknown error")),
+		Name:      "unknown error",
+		Retry:     true,
+	},
+	{
+		Responder: httpmock.NewStringResponder(500, "Server Error"),
+		Name:      "500",
+		Retry:     true,
+	},
+	{
+		Responder: httpmock.NewStringResponder(404, "Not Found"),
+		Name:      "404",
+		Retry:     false,
+	},
+	{
+		Responder: httpmock.NewStringResponder(200, "Not JSON"),
+		Name:      "invalid json",
+		Retry:     false,
+	},
+	{
+		Responder: httpmock.NewStringResponder(200, `{"foo":"bar"}`),
+		Name:      "missing fields",
+		Retry:     false,
+	},
+}
+
+func TestRetry(t *testing.T) {
+	t.Parallel()
+	for _, badCase := range badCases {
+		badCase := badCase
+		t.Run(badCase.Name, func(t *testing.T) {
+			t.Parallel()
+			trans := httpmock.NewMockTransport()
+			trans.RegisterResponder("GET", ECSIdentityEndpoint,
+				badCase.Responder.Then(httpmock.NewStringResponder(200, testIdDoc)))
+
+			_, err := NewECSMetadata(trans)
+			if badCase.Retry {
+				assert.NoError(t, err)
+			} else {
+				assert.Error(t, err)
+			}
+		})
+	}
+}
+
+func TestFailure(t *testing.T) {
+	t.Parallel()
+	for _, badCase := range badCases {
+		badCase := badCase
+		t.Run(badCase.Name, func(t *testing.T) {
+			t.Parallel()
+			trans := httpmock.NewMockTransport()
+			trans.RegisterResponder("GET", ECSIdentityEndpoint, badCase.Responder)
+
+			_, err := NewECSMetadata(trans)
+			assert.Error(t, err)
+		})
+	}
+}
+
+func TestGetEcs(t *testing.T) {
+	t.Parallel()
+	trans := httpmock.NewMockTransport()
+	trans.RegisterResponder("GET", ECSIdentityEndpoint, httpmock.NewStringResponder(200, testIdDoc))
+	m, err := NewECSMetadata(trans)
+	assert.NoError(t, err)
+
+	expected := map[MetadataKey]string{
+		RegionID:     "cn-beijing",
+		ZoneID:       "cn-beijing-k",
+		InstanceID:   "i-2zec1slzwdzrwmvlr4w2",
+		InstanceType: "ecs.g7.xlarge",
+	}
+	for k, v := range expected {
+		value, err := m.Get(k)
+		assert.NoError(t, err)
+		assert.Equal(t, v, value)
+	}
+
+	_, err = m.Get(99999)
+	assert.Equal(t, ErrUnknownMetadataKey, err)
+}

pkg/cloud/metadata/env.go
@@ -1,0 +1,18 @@
+package metadata
+
+import "os"
+
+type ENVMetadata struct{}
+
+var MetadataEnv = map[MetadataKey]string{
+	RegionID: "REGION_ID",
+}
+
+func (m *ENVMetadata) Get(key MetadataKey) (string, error) {
+	if v, ok := MetadataEnv[key]; ok {
+		if value, ok := os.LookupEnv(v); ok {
+			return value, nil
+		}
+	}
+	return "", ErrUnknownMetadataKey
+}

commit 358fe9ce2abb425169fe9505eb3224da7c8bea5a

metadata: fix data race
also add some test to reproduce the race

Fixes: 218de93c29738db94340d6bad324fc4af5f9abf5

pkg/cloud/metadata/metadata.go
@@ -62,26 +62,24 @@ type lazyInitProvider struct {
 }
 
 func (p *lazyInitProvider) Get(key MetadataKey) (string, error) {
+	p.initMu.Lock()
 	if p.provider == nil && p.err == nil {
-		p.initMu.Lock()
-		if p.provider == nil && p.err == nil {
-			provider, err := p.fetcher.FetchFor(key)
-			if err == ErrUnknownMetadataKey {
-				p.initMu.Unlock()
-				return "", err
-			}
-			if err != nil {
-				err = fmt.Errorf("%T failed: %w", p.fetcher, err)
-				// print a warning if we failed to get a value,
-				// because the error is hide if other providers succeed
-				logrus.Warn(err)
-			}
-			p.fetcher = nil
-			p.provider = provider
-			p.err = err
+		provider, err := p.fetcher.FetchFor(key)
+		if err == ErrUnknownMetadataKey {
+			p.initMu.Unlock()
+			return "", err
 		}
-		p.initMu.Unlock()
+		if err != nil {
+			err = fmt.Errorf("%T failed: %w", p.fetcher, err)
+			// print a warning if we failed to get a value,
+			// because the error is hide if other providers succeed
+			logrus.Warn(err)
+		}
+		p.fetcher = nil
+		p.provider = provider
+		p.err = err
 	}
+	p.initMu.Unlock()
 	if p.err != nil {
 		return "", p.err
 	}

pkg/cloud/metadata/metadata_test.go
@@ -3,6 +3,7 @@ package metadata
 import (
 	"context"
 	"errors"
+	"sync"
 	"testing"
 	"time"
 
@@ -163,4 +164,55 @@ func TestGetUnknownKey(t *testing.T) {
 
 	_, err := m.Get(99999)
 	assert.Equal(t, ErrUnknownMetadataKey, err)
+}
+
+type noopFetcher struct{}
+
+func (f *noopFetcher) FetchFor(MetadataKey) (MetadataProvider, error) {
+	time.Sleep(10 * time.Millisecond)
+	return &fakeProvider{
+		values: map[MetadataKey]string{
+			RegionID: "cn-beijing",
+		},
+	}, nil
+}
+
+// Run this with "-race"
+func TestParallelLazyInit(t *testing.T) {
+	t.Parallel()
+	m := lazyInitProvider{fetcher: &noopFetcher{}}
+
+	wg := sync.WaitGroup{}
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			region, err := m.Get(RegionID)
+			assert.NoError(t, err)
+			assert.Equal(t, "cn-beijing", region)
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+}
+
+// Run this with "-race"
+func TestParallelImmutableProvider(t *testing.T) {
+	t.Parallel()
+	m := newImmutableProvider(&fakeProvider{
+		values: map[MetadataKey]string{
+			RegionID: "cn-beijing",
+		},
+	}, "fake")
+
+	wg := sync.WaitGroup{}
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			region, err := m.Get(RegionID)
+			assert.NoError(t, err)
+			assert.Equal(t, "cn-beijing", region)
+			wg.Done()
+		}()
+	}
+	wg.Wait()
 }