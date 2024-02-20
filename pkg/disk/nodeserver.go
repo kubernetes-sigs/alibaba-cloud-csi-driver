kubernetes-sigs/alibaba-cloud-csi-driver

commit af971c56126ccc766220cfec286c31cf50ae38e4

do not use shell to write cgroupfs
in the process of remove shell usage

pkg/utils/io.go
@@ -119,13 +119,7 @@ func SetVolumeIOLimit(devicePath string, req *csi.NodePublishVolumeRequest) erro
 func writeIoLimit(majMinNum, podBlkIOPath, ioFile string, ioLimit int) error {
 	targetPath := filepath.Join(podBlkIOPath, ioFile)
 	content := majMinNum + " " + strconv.Itoa(ioLimit)
-	cmd := fmt.Sprintf("%s sh -c 'echo %s > %s'", NsenterCmd, content, targetPath)
-	_, err := exec.Command("sh", "-c", cmd).CombinedOutput()
-	if err != nil {
-		log.Errorf("WriteBps: Write file command(%s) error %v", cmd, err)
-		return err
-	}
-	return nil
+	return WriteTrunc(targetPath, content)
 }
 
 func getBpsLimt(bpsLimt string) (int, error) {

commit 7785921816af081ee5a225bd1ceb5d8e94afcd2b

mount cgroupfs into plugin for IO limit
Fixes: af971c56126ccc766220cfec286c31cf50ae38e4

deploy/ecs/csi-plugin.yaml
@@ -164,6 +164,8 @@ spec:
               name: efc-metrics-dir
             - mountPath: /host/run/csi-tool
               name: run-csi
+            - mountPath: /host/sys/fs/cgroup/blkio/kubepods.slice
+              name: cgroup-blkio
             - mountPath: /etc/csi-plugin/config
               name: csi-plugin-cm
             - name: host-mnt
@@ -193,6 +195,10 @@ spec:
         - name: kubelet-dir
           hostPath:
             path: /var/lib/kubelet
+            type: Directory
+        - name: cgroup-blkio
+          hostPath:
+            path: /sys/fs/cgroup/blkio/kubepods.slice
             type: Directory
         - name: host-dev
           hostPath:

deploy/nonecs/csi-plugin.yaml
@@ -148,6 +148,8 @@ spec:
               name: efc-metrics-dir
             - mountPath: /host/run/csi-tool
               name: run-csi
+            - mountPath: /host/sys/fs/cgroup/blkio/kubepods.slice
+              name: cgroup-blkio
             - mountPath: /etc/csi-plugin/config
               name: csi-plugin-cm
             - name: host-mnt
@@ -177,6 +179,10 @@ spec:
         - name: kubelet-dir
           hostPath:
             path: /var/lib/kubelet
+            type: Directory
+        - name: cgroup-blkio
+          hostPath:
+            path: /sys/fs/cgroup/blkio/kubepods.slice
             type: Directory
         - name: host-dev
           hostPath:

pkg/disk/nodeserver.go
@@ -33,6 +33,7 @@ import (
 	"github.com/kubernetes-sigs/alibaba-cloud-csi-driver/pkg/cloud/metadata"
 	"github.com/kubernetes-sigs/alibaba-cloud-csi-driver/pkg/log"
 	"github.com/kubernetes-sigs/alibaba-cloud-csi-driver/pkg/utils"
+	"golang.org/x/sys/unix"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	v1 "k8s.io/api/core/v1"
@@ -642,7 +643,7 @@ func (ns *nodeServer) NodeStageVolume(ctx context.Context, req *csi.NodeStageVol
 				// Note this cannot prevent user from access other device through e.g. /sys/block/vda/subsystem/vdb
 				return nil, status.Errorf(codes.Aborted, "NodeStageVolume: invalid relative path in sysConfig: %s", key)
 			}
-			err := utils.WriteTrunc(fileName, value)
+			err := utils.WriteTrunc(unix.AT_FDCWD, fileName, value)
 			if err != nil {
 				return nil, status.Errorf(codes.Internal,
 					"NodeStageVolume: Volume Block System Config failed, failed to write %s to %s: %v", value, fileName, err)

pkg/utils/io.go
@@ -3,15 +3,18 @@ package utils
 import (
 	"errors"
 	"fmt"
+	"io/fs"
 	"os"
-	"path/filepath"
 	"strconv"
 	"strings"
 	"syscall"
 
 	"github.com/container-storage-interface/spec/lib/go/csi"
 	log "github.com/sirupsen/logrus"
+	"golang.org/x/sys/unix"
 )
+
+const PodBlkIOCgroupPath = "/host/sys/fs/cgroup/blkio/kubepods.slice"
 
 // SetVolumeIOLimit config io limit for device
 // readIOPS: 1000
@@ -71,43 +74,48 @@ func SetVolumeIOLimit(devicePath string, req *csi.NodePublishVolumeRequest) erro
 	}
 	// /sys/fs/cgroup/blkio/kubepods.slice/kubepods-besteffort.slice/kubepods-besteffort-podaadcc749_6776_4933_990d_d50f260f5d46.slice/blkio.throttle.write_bps_device
 	podUID = strings.ReplaceAll(podUID, "-", "_")
-	podBlkIOPath := ""
+	podBlkIOPathFd := -1
 	for _, p := range []string{
-		fmt.Sprintf("/sys/fs/cgroup/blkio/kubepods.slice/kubepods-besteffort.slice/kubepods-besteffort-pod%s.slice", podUID),
-		fmt.Sprintf("/sys/fs/cgroup/blkio/kubepods.slice/kubepods-burstable.slice/kubepods-burstable-pod%s.slice", podUID),
-		fmt.Sprintf("/sys/fs/cgroup/blkio/kubepods.slice/kubepods-pod%s.slice", podUID),
+		fmt.Sprintf("%s/kubepods-besteffort.slice/kubepods-besteffort-pod%s.slice", PodBlkIOCgroupPath, podUID),
+		fmt.Sprintf("%s/kubepods-burstable.slice/kubepods-burstable-pod%s.slice", PodBlkIOCgroupPath, podUID),
+		fmt.Sprintf("%s/kubepods-pod%s.slice", PodBlkIOCgroupPath, podUID),
 	} {
-		if IsHostFileExist(p) {
-			podBlkIOPath = p
+		fd, err := unix.Open(p, unix.O_PATH, 0)
+		if err == nil {
+			podBlkIOPathFd = fd
+			defer unix.Close(podBlkIOPathFd)
 			break
 		}
+		if !errors.Is(err, fs.ErrNotExist) {
+			return fmt.Errorf("Volume(%s) Cannot open pod blkio path: %w", req.VolumeId, err)
+		}
 	}
-	if podBlkIOPath == "" {
+	if podBlkIOPathFd == -1 {
 		log.Errorf("Volume(%s), pod blkio/cgroup path not found", req.VolumeId)
 		return errors.New("pod blkio/cgroup path not found")
 	}
 
 	// io limit set to blkio limit files
 	if readIOPSInt != 0 {
-		err := writeIoLimit(majMinNum, podBlkIOPath, "blkio.throttle.read_iops_device", readIOPSInt)
+		err := writeIoLimit(majMinNum, podBlkIOPathFd, "blkio.throttle.read_iops_device", readIOPSInt)
 		if err != nil {
 			return err
 		}
 	}
 	if writeIOPSInt != 0 {
-		err := writeIoLimit(majMinNum, podBlkIOPath, "blkio.throttle.write_iops_device", writeIOPSInt)
+		err := writeIoLimit(majMinNum, podBlkIOPathFd, "blkio.throttle.write_iops_device", writeIOPSInt)
 		if err != nil {
 			return err
 		}
 	}
 	if readBPSInt != 0 {
-		err := writeIoLimit(majMinNum, podBlkIOPath, "blkio.throttle.read_bps_device", readBPSInt)
+		err := writeIoLimit(majMinNum, podBlkIOPathFd, "blkio.throttle.read_bps_device", readBPSInt)
 		if err != nil {
 			return err
 		}
 	}
 	if writeBPSInt != 0 {
-		err := writeIoLimit(majMinNum, podBlkIOPath, "blkio.throttle.write_bps_device", writeBPSInt)
+		err := writeIoLimit(majMinNum, podBlkIOPathFd, "blkio.throttle.write_bps_device", writeBPSInt)
 		if err != nil {
 			return err
 		}
@@ -116,10 +124,9 @@ func SetVolumeIOLimit(devicePath string, req *csi.NodePublishVolumeRequest) erro
 	return nil
 }
 
-func writeIoLimit(majMinNum, podBlkIOPath, ioFile string, ioLimit int) error {
-	targetPath := filepath.Join(podBlkIOPath, ioFile)
+func writeIoLimit(majMinNum string, podBlkIOPathFd int, ioFile string, ioLimit int) error {
 	content := majMinNum + " " + strconv.Itoa(ioLimit)
-	return WriteTrunc(targetPath, content)
+	return WriteTrunc(podBlkIOPathFd, ioFile, content)
 }
 
 func getBpsLimt(bpsLimt string) (int, error) {

pkg/utils/util.go
@@ -1119,15 +1119,15 @@ func GetNvmeDeviceByVolumeID(volumeID string) (device string, err error) {
 }
 
 // Differs from os.WriteFile in that it does not or create file before writing.
-func WriteTrunc(filePath string, value string) error {
-	f, err := os.OpenFile(filePath, os.O_WRONLY|os.O_TRUNC, 0644)
+// Intended to write Linux virtual files: sysfs, cgroupfs, etc.
+func WriteTrunc(dirFd int, filePath string, value string) error {
+	fd, err := unix.Openat(dirFd, filePath, os.O_WRONLY|os.O_TRUNC, 0644)
 	if err != nil {
 		return err
 	}
-	defer f.Close()
 
-	_, err = f.WriteString(value)
-	if err1 := f.Close(); err1 != nil && err == nil {
+	_, err = unix.Write(fd, []byte(value))
+	if err1 := unix.Close(fd); err1 != nil && err == nil {
 		err = err1
 	}
 	return err