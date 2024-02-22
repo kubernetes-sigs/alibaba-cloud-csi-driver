008c8b86012a4b8a8f152a3dcb0dfb5585ce7222
AlbeeSo <suyashi1321@163.com>
2024-02-22 11:28:41 +0800

    fix test

--- pkg/cloud/metadata/ecs_test.go
+++ pkg/cloud/metadata/ecs_test.go
@@ -89,7 +89,7 @@ func TestGetEcs(t *testing.T) {
 		ZoneID:       "cn-beijing-k",
 		InstanceID:   "i-2zec1slzwdzrwmvlr4w2",
 		InstanceType: "ecs.g7.xlarge",
-		AliUID:       "112233445566",
+		AliUID:       "1857888888888866",
 	}
 	for k, v := range expected {
 		value, err := m.Get(k)
