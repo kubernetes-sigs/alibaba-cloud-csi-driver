02aea3d808c25f9c1a4bdbdba880e6ea7bd0cc8a
mowangdk <geyingqi0701@gmail.com>
2024-02-23 11:34:16 +0800

    feature: support specific http header

--- pkg/disk/cloud.go
+++ pkg/disk/cloud.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"os"
 	"regexp"
 	"strings"
 	"time"
@@ -929,6 +930,10 @@ func request(createDiskRequest *ecs.CreateDiskRequest, ecsClient *ecs.Client) (r
 				return false, "", fmt.Errorf("request: to request %s type disk you needs at least %dGB size which the provided size %dGB does not meet the needs, please resize the size up.", cata, minCap, rValue)
 			}
 		}
+	}
+	// for private cloud
+	if ascmContext := os.Getenv("X-ACSPROXY-ASCM-CONTEXT"); ascmContext != "" {
+		createDiskRequest.GetHeaders()["x-acsproxy-ascm-context"] = ascmContext
 	}
 	log.Infof("request: request content: %++v", *createDiskRequest)
 	volumeRes, err := ecsClient.CreateDisk(createDiskRequest)
