kubernetes-sigs/alibaba-cloud-csi-driver

commit 377c9e9bce468c996426da502fc1e157a13ac5f7

cleanup: remove filesystem size check

pkg/disk/nodeserver.go
@@ -1002,11 +1002,6 @@ func (ns *nodeServer) NodeExpandVolume(ctx context.Context, req *csi.NodeExpandV
 	log.Infof(
 		"NodeExpandVolume:: filesystem resize context device capacity: %v, before resize fs capacity: %v resize fs capacity: %v, requestGB: %v. file system lose percent: %v",
 		deviceCapacity, beforeResizeDiskCapacity, diskCapacity, requestGB, GlobalConfigVar.FilesystemLosePercent)
-	if diskCapacity >= requestGB*GlobalConfigVar.FilesystemLosePercent {
-		// delete autoSnapshot
-		log.Infof("NodeExpandVolume:: resizefs successful volumeId: %s, devicePath: %s, volumePath: %s", diskID, devicePath, volumePath)
-		return &csi.NodeExpandVolumeResponse{}, nil
-	}
 	return nil, status.Errorf(codes.Internal, "requestGB: %v, diskCapacity: %v not in range", requestGB, diskCapacity)
 }
 

commit 535d6b01121e2f43e8d273b848b06f8269ce784b

disk: fix NodeExpandVolume always return error
Fixes: 377c9e9bce468c996426da502fc1e157a13ac5f7

pkg/disk/nodeserver.go
@@ -1016,7 +1016,7 @@ func (ns *nodeServer) NodeExpandVolume(ctx context.Context, req *csi.NodeExpandV
 	log.Infof(
 		"NodeExpandVolume:: filesystem resize context device capacity: %v, before resize fs capacity: %v resize fs capacity: %v, requestGB: %v. file system lose percent: %v",
 		deviceCapacity, beforeResizeDiskCapacity, diskCapacity, requestGB, GlobalConfigVar.FilesystemLosePercent)
-	return nil, status.Errorf(codes.Internal, "requestGB: %v, diskCapacity: %v not in range", requestGB, diskCapacity)
+	return &csi.NodeExpandVolumeResponse{}, nil
 }
 
 // NodeGetVolumeStats used for csi metrics