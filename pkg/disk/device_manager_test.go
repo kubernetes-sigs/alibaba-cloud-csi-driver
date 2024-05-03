kubernetes-sigs/alibaba-cloud-csi-driver

commit 9a71d43bb92d866497e6da11ea8281bd940d5b27

refactor device manager
Return the disk/by-id path if found, it is safer if disk is detached when we are processing.
The path is auto removed with the device, and cannot be reused by another disk.

Avoid filepath.EvalSymlinks(). This function is complex. It resolves ALL symlinks in the path, not just the link pointed by the path.
We instead verify the link by stat this path to see it is really a block special file.

Extract every global dependency into instanced field for testablity.

Add unit test for every function.

pkg/disk/device_manager.go
@@ -6,12 +6,38 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"syscall"
 
 	log "github.com/sirupsen/logrus"
 	utilerrors "k8s.io/apimachinery/pkg/util/errors"
 )
 
+type DevTmpFS interface {
+	DevFor(path string) (int32, int32, error)
+}
+
+type realDevTmpFS struct {
+}
+
+// DevFor returns the major and minor numbers for the device.
+func (d realDevTmpFS) DevFor(path string) (int32, int32, error) {
+	info, err := os.Stat(path)
+	if err != nil {
+		return 0, 0, err
+	}
+	stat, ok := info.Sys().(*syscall.Stat_t)
+	if !ok {
+		return 0, 0, errors.New("unsupported stat type")
+	}
+	if stat.Mode&syscall.S_IFMT != syscall.S_IFBLK {
+		return 0, 0, errors.New("not a block device")
+	}
+	return int32(stat.Rdev / 256), int32(stat.Rdev % 256), nil
+}
+
 type DeviceManager struct {
+	DevTmpFS DevTmpFS
+
 	// The path to the directory containing the device files.
 	// This is usually /dev.
 	DevicePath string
@@ -19,25 +45,35 @@ type DeviceManager struct {
 	// The path to the directory mounted as sysfs.
 	// This is usually /sys.
 	SysfsPath string
+
+	// Disable read serial from sysfs
+	DisableSerial bool
+
+	// Support sole alreadly formatted disk partition
+	EnableDiskPartition bool
 }
 
 var DefaultDeviceManager = &DeviceManager{
+	DevTmpFS:   realDevTmpFS{},
 	DevicePath: "/dev",
 	SysfsPath:  "/sys",
 }
 
+var ErrNotFound = errors.New("device not found")
+
+// returns the block device name (e.g. vda) or error
 func (m *DeviceManager) getDeviceBySerial(serial string) (string, error) {
-	sysBlock := filepath.Join(m.SysfsPath, "block")
+	sysBlock := m.SysfsPath + "/block"
 	allBlocks, err := os.ReadDir(sysBlock)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("read dir %q failed: %w", sysBlock, err)
 	}
 	for _, block := range allBlocks {
-		serialPath := filepath.Join(sysBlock, block.Name(), "serial")
+		serialPath := fmt.Sprintf("%s/%s/serial", sysBlock, block.Name())
 		// NVMe block device is a namespace (e.g. nvme0n1). It does not have a serial file.
 		// Instead, we need to read the serial from the device (nvme0).
 		if strings.HasPrefix(block.Name(), "nvme") {
-			serialPath = filepath.Join(sysBlock, block.Name(), "device", "serial")
+			serialPath = fmt.Sprintf("%s/%s/device/serial", sysBlock, block.Name())
 		}
 		body, err := os.ReadFile(serialPath)
 		if err != nil {
@@ -45,30 +81,46 @@ func (m *DeviceManager) getDeviceBySerial(serial string) (string, error) {
 			continue
 		}
 		if strings.TrimSpace(string(body)) == serial {
-			return filepath.Join(m.DevicePath, block.Name()), nil
+			return block.Name(), nil
 		}
 	}
-	return "", errors.New("not found")
+	return "", ErrNotFound
+}
+
+func (m *DeviceManager) deviceName(devicePath string) (string, error) {
+	major, minor, err := m.DevTmpFS.DevFor(devicePath)
+	if err != nil {
+		return "", err
+	}
+	sysfsPath := fmt.Sprintf("%s/dev/block/%d:%d", m.SysfsPath, major, minor)
+	link, err := os.Readlink(sysfsPath)
+	if err != nil {
+		return "", err
+	}
+	return filepath.Base(link), nil
 }
 
 // We only support static volume with exactly one partition, and is manually formatted.
-// Return the partition block device if it is OK to use.
+// Return the root or partition block device path if it is OK to use.
 func (m *DeviceManager) adaptDevicePartition(rootDevicePath string) (string, error) {
-	devName := filepath.Base(rootDevicePath)
-	partitionPattern := filepath.Join(m.SysfsPath, "block", devName, devName+"*", "partition")
-	// Get all device path relate to root device
+	devName, err := m.deviceName(rootDevicePath)
+	if err != nil {
+		return "", fmt.Errorf("get device name for %s failed: %w", rootDevicePath, err)
+	}
+	partitionPattern := fmt.Sprintf("%s/block/%s/%s*/partition", m.SysfsPath, devName, devName)
 	globDevices, err := filepath.Glob(partitionPattern)
 	if err != nil {
 		return "", fmt.Errorf("get partition list by glob for %s failed: %w", partitionPattern, err)
 	}
+
 	if len(globDevices) == 0 {
 		return rootDevicePath, nil
 	}
 	if len(globDevices) > 1 {
-		return "", fmt.Errorf("%d partitions found for %s", len(globDevices), rootDevicePath)
+		return "", fmt.Errorf("%d partitions found for %s", len(globDevices), devName)
 	}
 
-	// partition found, check if it is formatted
+	// exactly one partition found, check if it is formatted
 	partitionDevicePath := filepath.Join(m.DevicePath, filepath.Base(filepath.Dir(globDevices[0])))
 	if err := checkRootAndSubDeviceFS(rootDevicePath, partitionDevicePath); err != nil {
 		return "", err
@@ -76,34 +128,67 @@ func (m *DeviceManager) adaptDevicePartition(rootDevicePath string) (string, err
 	return partitionDevicePath, nil
 }
 
-func (m *DeviceManager) getDeviceByLink(linkPath string) (string, error) {
-	resolved, err := filepath.EvalSymlinks(linkPath)
-	if err != nil {
-		return "", err
-	}
-	if !strings.HasPrefix(resolved, m.DevicePath) {
-		return "", fmt.Errorf("resolved to unexpected path: %q", resolved)
-	}
-	return resolved, nil
-}
-
 func (m *DeviceManager) GetDeviceByVolumeID(volumeID string) (string, error) {
-	device, err := m.GetRootBlockByVolumeID(volumeID)
+	path, err := m.GetRootBlockByVolumeID(volumeID)
 	if err != nil {
 		return "", err
 	}
-	if !GlobalConfigVar.DiskPartitionEnable {
-		return device, nil
+	if !m.EnableDiskPartition {
+		return path, nil
 	}
 
-	partition, err := m.adaptDevicePartition(device)
+	partition, err := m.adaptDevicePartition(path)
 	if err != nil {
-		return "", fmt.Errorf("volume %s resolved to device %s, but adapt partition failed: %w", volumeID, device, err)
+		return "", fmt.Errorf("volume %s resolved to device %s, but adapt partition failed: %w", volumeID, path, err)
 	}
 	return partition, nil
 }
 
-var idPrefixes = []string{"virtio-", "nvme-Alibaba_Cloud_Elastic_Block_Storage_"}
+var idPrefixes = [...]string{"virtio-", "nvme-Alibaba_Cloud_Elastic_Block_Storage_"}
+
+func (m *DeviceManager) getDeviceByLink(idSuffix string) (string, error) {
+	byIDPath := m.DevicePath + "/disk/by-id"
+
+	var errs []error
+	for _, p := range idPrefixes {
+		volumeLinkPath := filepath.Join(byIDPath, p+idSuffix)
+		major, minor, err := m.DevTmpFS.DevFor(volumeLinkPath)
+		if err == nil {
+			log.Infof("GetDevice: device link %q: %d:%d", volumeLinkPath, major, minor)
+			return volumeLinkPath, nil
+		}
+		errw := fmt.Errorf("get by link %q failed: %w", volumeLinkPath, err)
+		if !errors.Is(err, os.ErrNotExist) {
+			return "", errw
+		}
+		errs = append(errs, errw)
+	}
+	return "", utilerrors.NewAggregate(errs)
+}
+
+func (m *DeviceManager) getDeviceByScanLinks(idSuffix string) (string, error) {
+	byIDPath := m.DevicePath + "/disk/by-id"
+
+	files, err := os.ReadDir(byIDPath)
+	if err != nil {
+		return "", fmt.Errorf("read dir %q failed: %w", byIDPath, err)
+	}
+
+	errs := []error{}
+	for _, f := range files {
+		if !strings.Contains(f.Name(), idSuffix) {
+			continue
+		}
+		volumeLinkPath := filepath.Join(byIDPath, f.Name())
+		major, minor, err := m.DevTmpFS.DevFor(volumeLinkPath)
+		if err == nil {
+			log.Infof("GetDevice: scanned device link %q: %d:%d", volumeLinkPath, major, minor)
+			return volumeLinkPath, nil
+		}
+		errs = append(errs, fmt.Errorf("get by scanned link %q failed: %w", volumeLinkPath, err))
+	}
+	return "", utilerrors.NewAggregate(errs)
+}
 
 // GetRootBlockByVolumeID first try to find the device by device link like:
 // /dev/disk/by-id/virtio-wz9cu3ctp6aj1iagco4h -> ../../vdc
@@ -111,51 +196,33 @@ var idPrefixes = []string{"virtio-", "nvme-Alibaba_Cloud_Elastic_Block_Storage_"
 func (m *DeviceManager) GetRootBlockByVolumeID(volumeID string) (string, error) {
 	errs := []error{}
 
-	byIDPath := filepath.Join(m.DevicePath, "disk", "by-id")
 	idSuffix := strings.TrimPrefix(volumeID, "d-")
-	fileFound := false
-	for _, p := range idPrefixes {
-		volumeLinkPath := byIDPath + p + idSuffix
-		device, err := m.getDeviceByLink(volumeLinkPath)
-		if err == nil {
-			log.Infof("GetDevice: device link %q to %q", volumeLinkPath, device)
-			return device, nil
-		}
-		if !errors.Is(err, os.ErrNotExist) {
-			fileFound = true
-		}
-		errs = append(errs, fmt.Errorf("get by link %q failed: %w", volumeLinkPath, err))
+	linkPath, err := m.getDeviceByLink(idSuffix)
+	if err == nil {
+		return linkPath, nil
 	}
+	errs = append(errs, err)
 
-	// fallback to scan all links
-	if !fileFound {
-		files, err := os.ReadDir(byIDPath)
+	// fallback to scan all links if no known link found
+	if errors.Is(err, os.ErrNotExist) {
+		linkPath, err = m.getDeviceByScanLinks(idSuffix)
+		if linkPath != "" {
+			return linkPath, nil
+		}
 		if err != nil {
-			errs = append(errs, fmt.Errorf("read dir %q failed: %w", byIDPath, err))
-		} else {
-			for _, f := range files {
-				if strings.Contains(f.Name(), idSuffix) {
-					volumeLinkPath := filepath.Join(byIDPath, f.Name())
-					device, err := m.getDeviceByLink(volumeLinkPath)
-					if err == nil {
-						log.Infof("GetDevice: scanned device link %q to %s", volumeLinkPath, device)
-						return device, nil
-					}
-					errs = append(errs, fmt.Errorf("get by scanned link %q failed: %w", volumeLinkPath, err))
-				}
-			}
+			errs = append(errs, err)
 		}
 	}
 
 	// this is danger in Bdf mode
-	if !IsVFNode() {
-		device, err := m.getDeviceBySerial(idSuffix)
+	if !m.DisableSerial {
+		name, err := m.getDeviceBySerial(idSuffix)
 		if err == nil {
-			log.Infof("GetDevice: Use the serial to find device, volumeID: %s, device: %s", volumeID, device)
-			return device, nil
+			log.Infof("GetDevice: Use the serial to find device, volumeID: %s, device: %s", volumeID, name)
+			return filepath.Join(m.DevicePath, name), nil
 		}
 		errs = append(errs, fmt.Errorf("find by serial: %w", err))
 	}
 
-	return "", utilerrors.NewAggregate(errs)
+	return "", utilerrors.Flatten(utilerrors.NewAggregate(errs))
 }

pkg/disk/nodeserver.go
@@ -948,7 +948,7 @@ func (ns *nodeServer) NodeExpandVolume(ctx context.Context, req *csi.NodeExpandV
 	}
 
 	log.Infof("NodeExpandVolume:: volumeId: %s, devicePath: %s, volumePath: %s", diskID, devicePath, volumePath)
-	if GlobalConfigVar.DiskPartitionEnable && !IsDeviceNvme(devicePath) && isDevicePartition(devicePath) {
+	if DefaultDeviceManager.EnableDiskPartition && !IsDeviceNvme(devicePath) && isDevicePartition(devicePath) {
 		rootPath, index, err := getDeviceRootAndIndex(devicePath)
 		if err != nil {
 			log.Errorf("NodeExpandVolume:: GetDeviceRootAndIndex: %s with error: %s", diskID, err.Error())

pkg/disk/device_manager_test.go
@@ -1,0 +1,348 @@
+package disk
+
+import (
+	"compress/gzip"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type fakeDevTmpFS struct {
+	DevicePath string
+	Devs       []fakeDev
+}
+
+type fakeDev struct {
+	Major int32
+	Minor int32
+	Path  string
+	Err   error
+}
+
+var (
+	virtIODev = fakeDev{
+		Major: 253,
+		Minor: 16,
+		Path:  "vdb",
+	}
+	virtIOLink = fakeDev{
+		Major: 253,
+		Minor: 16,
+		Path:  "disk/by-id/virtio-mydiskserial",
+	}
+	nvmeDev = fakeDev{
+		Major: 259,
+		Minor: 2,
+		Path:  "nvme1n1",
+	}
+	nvmeLink = fakeDev{
+		Major: 259,
+		Minor: 2,
+		Path:  "disk/by-id/nvme-Alibaba_Cloud_Elastic_Block_Storage_mydiskserial",
+	}
+	otherDev = fakeDev{
+		Major: 123,
+		Minor: 45,
+		Path:  "other0",
+	}
+	otherLink = fakeDev{
+		Major: 123,
+		Minor: 45,
+		Path:  "disk/by-id/other-mydiskserial",
+	}
+	badLink = fakeDev{
+		Path: "disk/by-id/virtio-mydiskserial",
+		Err:  errors.New("fake bad device"),
+	}
+)
+
+func (d *fakeDevTmpFS) DevFor(path string) (int32, int32, error) {
+	relPath, err := filepath.Rel(d.DevicePath, path)
+	if err != nil {
+		panic(err)
+	}
+	for _, dev := range d.Devs {
+		if dev.Path == relPath {
+			if dev.Err != nil {
+				return 0, 0, dev.Err
+			}
+			return dev.Major, dev.Minor, nil
+		}
+	}
+	return 0, 0, os.ErrNotExist
+}
+
+func testingManager(t *testing.T) *DeviceManager {
+	dir := t.TempDir()
+	err := os.MkdirAll(filepath.Join(dir, "sys/block"), 0o755)
+	assert.NoError(t, err)
+	err = os.MkdirAll(filepath.Join(dir, "sys/dev/block"), 0o755)
+	assert.NoError(t, err)
+
+	devPath := filepath.Join(dir, "dev")
+	err = os.MkdirAll(devPath, 0o755)
+	assert.NoError(t, err)
+	return &DeviceManager{
+		DevTmpFS:   &fakeDevTmpFS{DevicePath: devPath},
+		DevicePath: devPath,
+		SysfsPath:  filepath.Join(dir, "sys"),
+	}
+}
+
+func TestGetDeviceBySerialNotFound(t *testing.T) {
+	m := testingManager(t)
+	_, err := m.getDeviceBySerial("mydiskserial")
+	assert.Equal(t, err, ErrNotFound)
+
+	// ignore block that has no serial
+	err = os.MkdirAll(filepath.Join(m.SysfsPath, "block/loop0"), 0o755)
+	assert.NoError(t, err)
+
+	_, err = m.getDeviceBySerial("mydiskserial")
+	assert.Equal(t, err, ErrNotFound)
+}
+
+func setupVirtIOBlockDevice(t *testing.T, sysfsPath string) string {
+	sysfsDevicePath := filepath.Join(sysfsPath, "devices/pci0000:00/0000:00:0a.0/virtio7/block/vdb")
+	err := os.MkdirAll(sysfsDevicePath, 0o755)
+	assert.NoError(t, err)
+	err = os.WriteFile(filepath.Join(sysfsDevicePath, "serial"), []byte("mydiskserial\n"), 0o644)
+	assert.NoError(t, err)
+	err = os.Symlink("../devices/pci0000:00/0000:00:0a.0/virtio7/block/vdb", filepath.Join(sysfsPath, "block/vdb"))
+	assert.NoError(t, err)
+	err = os.Symlink("../../devices/pci0000:00/0000:00:0a.0/virtio7/block/vdb", filepath.Join(sysfsPath, "dev/block/253:16"))
+	assert.NoError(t, err)
+
+	return sysfsDevicePath
+}
+
+func setupNVMeBlockDevice(t *testing.T, sysfsPath string) string {
+	sysfsDevicePath := filepath.Join(sysfsPath, "devices/pci0000:00/0000:00:07.0/nvme/nvme1/nvme1n1")
+	err := os.MkdirAll(sysfsDevicePath, 0o755)
+	assert.NoError(t, err)
+	err = os.Symlink("../../nvme1", filepath.Join(sysfsDevicePath, "device"))
+	assert.NoError(t, err)
+	err = os.WriteFile(filepath.Join(sysfsPath, "devices/pci0000:00/0000:00:07.0/nvme/nvme1/serial"), []byte("mydiskserial\n"), 0o644)
+	assert.NoError(t, err)
+	err = os.Symlink("../devices/pci0000:00/0000:00:07.0/nvme/nvme1/nvme1n1", filepath.Join(sysfsPath, "block/nvme1n1"))
+	assert.NoError(t, err)
+	err = os.Symlink("../../devices/pci0000:00/0000:00:07.0/nvme/nvme1/nvme1n1", filepath.Join(sysfsPath, "dev/block/259:2"))
+	assert.NoError(t, err)
+
+	return sysfsDevicePath
+}
+
+func TestGetDeviceBySerialVirtIO(t *testing.T) {
+	m := testingManager(t)
+	// Create a fake virtio block device.
+	setupVirtIOBlockDevice(t, m.SysfsPath)
+
+	deviceName, err := m.getDeviceBySerial("mydiskserial")
+	assert.NoError(t, err)
+	assert.Equal(t, "vdb", deviceName)
+}
+
+func TestGetDeviceBySerialNvme(t *testing.T) {
+	m := testingManager(t)
+	// Create a fake NVMe block device.
+	setupNVMeBlockDevice(t, m.SysfsPath)
+
+	deviceName, err := m.getDeviceBySerial("mydiskserial")
+	assert.NoError(t, err)
+	assert.Equal(t, "nvme1n1", deviceName)
+}
+
+func extractGzip(t *testing.T, src, dst string) {
+	f, err := os.Open(src)
+	assert.NoError(t, err)
+	defer f.Close()
+
+	gz, err := gzip.NewReader(f)
+	assert.NoError(t, err)
+
+	f, err = os.Create(dst)
+	assert.NoError(t, err)
+	defer f.Close()
+
+	_, err = io.Copy(f, gz)
+	assert.NoError(t, err)
+}
+
+// returns the single formatted partition
+func TestAdaptDevicePartitionPositive(t *testing.T) {
+	m := testingManager(t)
+	// Create a fake NVMe block device.
+	sysfsDev := setupNVMeBlockDevice(t, m.SysfsPath)
+	err := os.Mkdir(filepath.Join(sysfsDev, "nvme1n1p27"), 0o755)
+	assert.NoError(t, err)
+	err = os.WriteFile(filepath.Join(sysfsDev, "nvme1n1p27/partition"), []byte("27\n"), 0o644)
+	assert.NoError(t, err)
+	m.DevTmpFS.(*fakeDevTmpFS).Devs = []fakeDev{nvmeDev}
+
+	extractGzip(t, "testdata/parted_disk.img.gz", filepath.Join(m.DevicePath, "nvme1n1"))
+	extractGzip(t, "testdata/ext4_disk.img.gz", filepath.Join(m.DevicePath, "nvme1n1p27"))
+
+	devicePath, err := m.adaptDevicePartition(filepath.Join(m.DevicePath, "nvme1n1"))
+	assert.NoError(t, err)
+	assert.Equal(t, filepath.Join(m.DevicePath, "nvme1n1p27"), devicePath)
+}
+
+// returns root device path if no partition found
+func TestAdaptDevicePartitionNoPartition(t *testing.T) {
+	fmt.Print(os.Getenv("PATH"))
+	m := testingManager(t)
+	// Create a fake NVMe block device.
+	setupNVMeBlockDevice(t, m.SysfsPath)
+	m.DevTmpFS.(*fakeDevTmpFS).Devs = []fakeDev{nvmeDev}
+
+	devicePath, err := m.adaptDevicePartition(filepath.Join(m.DevicePath, "nvme1n1"))
+	assert.NoError(t, err)
+	assert.Equal(t, filepath.Join(m.DevicePath, "nvme1n1"), devicePath)
+}
+
+// returns error if more than one partition found
+func TestAdaptDevicePartitionMultiplePartitions(t *testing.T) {
+	m := testingManager(t)
+	// Create a fake NVMe block device.
+	sysfsDev := setupNVMeBlockDevice(t, m.SysfsPath)
+	err := os.Mkdir(filepath.Join(sysfsDev, "nvme1n1p27"), 0o755)
+	assert.NoError(t, err)
+	err = os.WriteFile(filepath.Join(sysfsDev, "nvme1n1p27/partition"), []byte("27\n"), 0o644)
+	assert.NoError(t, err)
+	err = os.Mkdir(filepath.Join(sysfsDev, "nvme1n1p28"), 0o755)
+	assert.NoError(t, err)
+	err = os.WriteFile(filepath.Join(sysfsDev, "nvme1n1p28/partition"), []byte("28\n"), 0o644)
+	assert.NoError(t, err)
+	m.DevTmpFS.(*fakeDevTmpFS).Devs = []fakeDev{nvmeDev}
+
+	_, err = m.adaptDevicePartition(filepath.Join(m.DevicePath, "nvme1n1"))
+	assert.Error(t, err)
+}
+
+func TestGetRootBlockByVolumeID_Link(t *testing.T) {
+	tc := []struct {
+		name       string
+		init       func(t *testing.T, m *DeviceManager)
+		devicePath string
+		err        error
+	}{
+		{
+			name: "virtio",
+			init: func(t *testing.T, m *DeviceManager) {
+				setupVirtIOBlockDevice(t, m.SysfsPath)
+				m.DevTmpFS.(*fakeDevTmpFS).Devs = []fakeDev{virtIODev, virtIOLink}
+			},
+			devicePath: "disk/by-id/virtio-mydiskserial",
+		},
+		{
+			name: "nvme",
+			init: func(t *testing.T, m *DeviceManager) {
+				setupNVMeBlockDevice(t, m.SysfsPath)
+				m.DevTmpFS.(*fakeDevTmpFS).Devs = []fakeDev{nvmeDev, nvmeLink}
+			},
+			devicePath: "disk/by-id/nvme-Alibaba_Cloud_Elastic_Block_Storage_mydiskserial",
+		},
+		{
+			name: "other",
+			init: func(t *testing.T, m *DeviceManager) {
+				err := os.Symlink("../../devices/other/other0", filepath.Join(m.SysfsPath, "dev/block/123:45"))
+				assert.NoError(t, err)
+				m.DevTmpFS.(*fakeDevTmpFS).Devs = []fakeDev{otherDev, otherLink}
+				// We need this because we read the disk/by-id directory to find the device.
+				err = os.MkdirAll(filepath.Join(m.DevicePath, "disk/by-id"), 0o755)
+				assert.NoError(t, err)
+				err = os.Symlink("../../other0", filepath.Join(m.DevicePath, "disk/by-id/other-mydiskserial"))
+				assert.NoError(t, err)
+				err = os.Symlink("../../other1", filepath.Join(m.DevicePath, "disk/by-id/not-a-ecs-disk"))
+				assert.NoError(t, err)
+			},
+			devicePath: "disk/by-id/other-mydiskserial",
+		},
+		{
+			name: "bad",
+			init: func(t *testing.T, m *DeviceManager) {
+				m.DevTmpFS.(*fakeDevTmpFS).Devs = []fakeDev{badLink}
+			},
+			err: badLink.Err,
+		},
+	}
+	for _, c := range tc {
+		t.Run(c.name, func(t *testing.T) {
+			m := testingManager(t)
+			m.DisableSerial = true
+
+			c.init(t, m)
+
+			devicePath, err := m.GetRootBlockByVolumeID("mydiskserial")
+			if c.err != nil {
+				assert.True(t, errors.Is(err, c.err), err)
+				assert.False(t, errors.Is(err, os.ErrNotExist), err) // bad link error should suppress os.ErrNotExist
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, filepath.Join(m.DevicePath, c.devicePath), devicePath)
+			}
+		})
+	}
+}
+
+func TestGetRootBlockByVolumeID_Link_NotFound(t *testing.T) {
+	m := testingManager(t)
+	m.DisableSerial = true
+	setupNVMeBlockDevice(t, m.SysfsPath)
+
+	_, err := m.GetRootBlockByVolumeID("mydiskserial")
+	if assert.Error(t, err) {
+		assert.True(t, errors.Is(err, os.ErrNotExist))
+	}
+}
+
+func TestGetRootBlockByVolumeID_Link_Bad(t *testing.T) {
+	m := testingManager(t)
+	m.DisableSerial = true
+	m.DevTmpFS.(*fakeDevTmpFS).Devs = []fakeDev{badLink}
+
+	_, err := m.GetRootBlockByVolumeID("mydiskserial")
+	if assert.Error(t, err) {
+		assert.True(t, errors.Is(err, badLink.Err))
+		assert.False(t, errors.Is(err, os.ErrNotExist)) // bad link error should suppress os.ErrNotExist
+	}
+}
+
+// Maybe udev is not functional. We should still be able to get the device by reading sysfs.
+func TestGetRootBlockByVolumeID_Serial(t *testing.T) {
+	m := testingManager(t)
+	setupNVMeBlockDevice(t, m.SysfsPath)
+
+	devicePath, err := m.GetRootBlockByVolumeID("mydiskserial")
+	assert.NoError(t, err)
+	assert.Equal(t, filepath.Join(m.DevicePath, "nvme1n1"), devicePath)
+}
+
+func TestGetRootBlockByVolumeID_NotFound(t *testing.T) {
+	m := testingManager(t)
+
+	_, err := m.GetRootBlockByVolumeID("mydiskserial")
+	if assert.Error(t, err) {
+		assert.True(t, errors.Is(err, os.ErrNotExist))
+	}
+}
+
+func TestGetDeviceByVolumeID_Positive(t *testing.T) {
+	for _, partition := range []bool{true, false} {
+		t.Run(fmt.Sprintf("partition=%v", partition), func(t *testing.T) {
+			m := testingManager(t)
+			m.EnableDiskPartition = partition
+			setupVirtIOBlockDevice(t, m.SysfsPath)
+			m.DevTmpFS.(*fakeDevTmpFS).Devs = []fakeDev{virtIODev, virtIOLink}
+
+			devicePath, err := m.GetDeviceByVolumeID("mydiskserial")
+			assert.NoError(t, err)
+			assert.Equal(t, filepath.Join(m.DevicePath, "disk/by-id/virtio-mydiskserial"), devicePath)
+		})
+	}
+}

pkg/disk/disk.go
@@ -73,7 +73,6 @@ type GlobalConfig struct {
 	DiskBdfEnable         bool
 	ClientSet             *kubernetes.Clientset
 	ClusterID             string
-	DiskPartitionEnable   bool
 	ControllerService     bool
 	BdfHealthCheck        bool
 	DiskMultiTenantEnable bool
@@ -245,7 +244,6 @@ func GlobalConfigSet(m metadata.MetadataProvider) *restclient.Config {
 		ClientSet:             kubeClient,
 		SnapClient:            snapClient,
 		ClusterID:             clustID,
-		DiskPartitionEnable:   csiCfg.GetBool("disk-partition-enable", "DISK_PARTITION_ENABLE", true),
 		ControllerService:     controllerServerType,
 		BdfHealthCheck:        csiCfg.GetBool("bdf-health-check", "BDF_HEALTH_CHECK", true),
 		DiskMultiTenantEnable: csiCfg.GetBool("disk-multi-tenant-enable", "DISK_MULTI_TENANT_ENABLE", false),
@@ -262,6 +260,8 @@ func GlobalConfigSet(m metadata.MetadataProvider) *restclient.Config {
 	} else {
 		log.Infof("AD-Controller is disabled, CSI Disk Plugin running in kubelet mode.")
 	}
+	DefaultDeviceManager.EnableDiskPartition = csiCfg.GetBool("disk-partition-enable", "DISK_PARTITION_ENABLE", true)
+	DefaultDeviceManager.DisableSerial = IsVFNode()
 	log.Infof("Starting with GlobalConfigVar: ADControllerEnable(%t), DiskTagEnable(%t), DiskBdfEnable(%t), MetricEnable(%t), RunTimeClass(%s), DetachDisabled(%t), DetachBeforeDelete(%t), ClusterID(%s)",
 		GlobalConfigVar.ADControllerEnable,
 		GlobalConfigVar.DiskTagEnable,

commit f361cc13a76ddc188f78c4f9b4ef983a8cca138e

disk: fix sysConfig
do not assume the device path ends with the device name.

Fixes: 9a71d43bb92d866497e6da11ea8281bd940d5b27

pkg/disk/device_manager.go
@@ -8,7 +8,9 @@ import (
 	"strings"
 	"syscall"
 
+	"github.com/kubernetes-sigs/alibaba-cloud-csi-driver/pkg/utils"
 	log "github.com/sirupsen/logrus"
+	"golang.org/x/sys/unix"
 	utilerrors "k8s.io/apimachinery/pkg/util/errors"
 )
 
@@ -87,13 +89,16 @@ func (m *DeviceManager) getDeviceBySerial(serial string) (string, error) {
 	return "", ErrNotFound
 }
 
+func (m *DeviceManager) sysfsDir(major, minor int32) string {
+	return fmt.Sprintf("%s/dev/block/%d:%d", m.SysfsPath, major, minor)
+}
+
 func (m *DeviceManager) deviceName(devicePath string) (string, error) {
 	major, minor, err := m.DevTmpFS.DevFor(devicePath)
 	if err != nil {
 		return "", err
 	}
-	sysfsPath := fmt.Sprintf("%s/dev/block/%d:%d", m.SysfsPath, major, minor)
-	link, err := os.Readlink(sysfsPath)
+	link, err := os.Readlink(m.sysfsDir(major, minor))
 	if err != nil {
 		return "", err
 	}
@@ -193,6 +198,9 @@ func (m *DeviceManager) getDeviceByScanLinks(idSuffix string) (string, error) {
 // GetRootBlockByVolumeID first try to find the device by device link like:
 // /dev/disk/by-id/virtio-wz9cu3ctp6aj1iagco4h -> ../../vdc
 // If that fails, query the kernel by sysfs to find the device by serial.
+//
+// Returns the path to a block special file.
+// Caller should not assume any other special property of the returned path.
 func (m *DeviceManager) GetRootBlockByVolumeID(volumeID string) (string, error) {
 	errs := []error{}
 
@@ -235,7 +243,7 @@ func (m *DeviceManager) GetDeviceRootAndPartitionIndex(devicePath string) (root 
 	if err != nil {
 		return "", "", err
 	}
-	devSysfsPath := fmt.Sprintf("%s/dev/block/%d:%d", m.SysfsPath, major, minor)
+	devSysfsPath := m.sysfsDir(major, minor)
 	idx, err := os.ReadFile(devSysfsPath + "/partition")
 	if err == nil {
 		link, err := os.Readlink(devSysfsPath) // /sys/dev/block/253:3 -> ../../devices/pci0000:00/0000:00:05.0/virtio2/block/vda/vda3
@@ -249,4 +257,24 @@ func (m *DeviceManager) GetDeviceRootAndPartitionIndex(devicePath string) (root 
 		return devicePath, "", nil
 	}
 	return "", "", fmt.Errorf("read partition from sysfs %q failed: %w", devSysfsPath+"/partition", err)
+}
+
+func (m *DeviceManager) WriteSysfs(devicePath, name, value string) error {
+	major, minor, err := m.DevTmpFS.DevFor(devicePath)
+	if err != nil {
+		return fmt.Errorf("failed to stat %s: %w", devicePath, err)
+	}
+	base := m.sysfsDir(major, minor) + "/"
+	fileName := filepath.Clean(base + name)
+	if !strings.HasPrefix(fileName, base) {
+		// Note this cannot prevent user from accessing other devices through e.g. /sys/block/vda/subsystem/vdb
+		// But we cannot restrict symlink either because names like `bdi/read_ahead_kb` may be vaild, in which `bdi` is a symlink.
+		// Just reject obvious attacks like '../../../root/.ssh/id_rsa'.
+		return fmt.Errorf("invalid relative path in sysConfig: %s", name)
+	}
+	err = utils.WriteTrunc(unix.AT_FDCWD, fileName, value)
+	if err != nil {
+		return fmt.Errorf("failed to write %s to %s: %w", value, fileName, err)
+	}
+	return nil
 }

pkg/disk/nodeserver.go
@@ -35,7 +35,6 @@ import (
 	"github.com/kubernetes-sigs/alibaba-cloud-csi-driver/pkg/cloud/metadata"
 	"github.com/kubernetes-sigs/alibaba-cloud-csi-driver/pkg/utils"
 	log "github.com/sirupsen/logrus"
-	"golang.org/x/sys/unix"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	v1 "k8s.io/api/core/v1"
@@ -646,16 +645,9 @@ func (ns *nodeServer) NodeStageVolume(ctx context.Context, req *csi.NodeStageVol
 				log.Errorf("NodeStageVolume: Volume Block System Config with format error: %s", configStr)
 				return nil, status.Error(codes.Aborted, "NodeStageVolume: Volume Block System Config with format error "+configStr)
 			}
-			base := fmt.Sprintf("/sys/block/%s/", filepath.Base(device))
-			fileName := filepath.Clean(base + key)
-			if !strings.HasPrefix(fileName, base) {
-				// Note this cannot prevent user from access other device through e.g. /sys/block/vda/subsystem/vdb
-				return nil, status.Errorf(codes.Aborted, "NodeStageVolume: invalid relative path in sysConfig: %s", key)
-			}
-			err := utils.WriteTrunc(unix.AT_FDCWD, fileName, value)
+			err := DefaultDeviceManager.WriteSysfs(device, key, value)
 			if err != nil {
-				return nil, status.Errorf(codes.Internal,
-					"NodeStageVolume: Volume Block System Config failed, failed to write %s to %s: %v", value, fileName, err)
+				return nil, status.Errorf(codes.Aborted, "NodeStageVolume: set sysConfig %s=%s failed: %v", key, value, err)
 			}
 			log.Infof("NodeStageVolume: set sysConfig %s=%s", key, value)
 		}

pkg/disk/device_manager_test.go
@@ -451,3 +451,32 @@ func TestGetDeviceRootAndPartitionIndex_Partition(t *testing.T) {
 		})
 	}
 }
+
+func TestWriteSysfs(t *testing.T) {
+	m := testingManager(t)
+	setupNVMeBlockDevice(t, m.SysfsPath)
+	m.DevTmpFS.(*fakeDevTmpFS).Devs = []fakeDev{nvmeDev}
+
+	p := filepath.Join(m.SysfsPath, "devices/pci0000:00/0000:00:07.0/nvme/nvme1/nvme1n1/some/config")
+	err := os.MkdirAll(filepath.Dir(p), 0o755)
+	assert.NoError(t, err)
+	err = os.WriteFile(p, []byte("anything"), 0o644)
+	assert.NoError(t, err)
+
+	dev := filepath.Join(m.DevicePath, "nvme1n1")
+	err = m.WriteSysfs(dev, "some/config", "config-value")
+	assert.NoError(t, err)
+
+	err = m.WriteSysfs("/dev/no-such-device", "some/config", "config-value")
+	assert.True(t, errors.Is(err, os.ErrNotExist), err)
+
+	b, err := os.ReadFile(p)
+	assert.NoError(t, err)
+	assert.Equal(t, "config-value", string(b))
+
+	err = m.WriteSysfs(dev, "some/not-exist-config", "config-value")
+	assert.True(t, errors.Is(err, os.ErrNotExist), err)
+
+	err = m.WriteSysfs(dev, "../invaild/config", "config-value")
+	assert.Error(t, err)
+}