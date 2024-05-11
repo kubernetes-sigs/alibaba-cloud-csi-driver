d7f7b1b0d012b0e61baaf1993b98737186a38896
胡玮文 <huweiwen.hww@alibaba-inc.com>
2024-05-11 14:57:50 +0800

    log: add benchmark
    klog is more efficient than logrus. But the redirection causes about 3x overhead. Mostly caused by stack unwind, the call stack between logrus and its hook is too deep.
    
    goos: linux
    goarch: amd64
    pkg: github.com/kubernetes-sigs/alibaba-cloud-csi-driver/pkg/log
    cpu: Intel(R) Xeon(R) Platinum
    BenchmarkLogrus-4                       	  497246	      2421 ns/op
    BenchmarkLogrusStructured-4             	  338302	      3603 ns/op
    BenchmarkKlog-4                         	  661293	      1652 ns/op
    BenchmarkKlogStructured-4               	  387448	      2936 ns/op
    BenchmarkLogrusRedirected-4             	  110848	     10740 ns/op
    BenchmarkLogrusStructuredRedirected-4   	  120429	     10234 ns/op
    PASS

---
+++ pkg/log/log_test.go
@@ -0,0 +1,64 @@
+package log
+
+import (
+	"io"
+	"testing"
+
+	"github.com/sirupsen/logrus"
+	"k8s.io/klog/v2"
+	klogtest "k8s.io/klog/v2/test"
+)
+
+func BenchmarkLogrus(b *testing.B) {
+	logrus.SetOutput(io.Discard)
+	for i := 0; i < b.N; i++ {
+		logrus.Infof("This is a log message %d", i)
+	}
+}
+
+func BenchmarkLogrusStructured(b *testing.B) {
+	logrus.SetOutput(io.Discard)
+	for i := 0; i < b.N; i++ {
+		logrus.WithField("index", i).Info("This is a structured log message")
+	}
+}
+
+func BenchmarkKlog(b *testing.B) {
+	klogtest.InitKlog()
+	klog.SetOutput(io.Discard)
+	for i := 0; i < b.N; i++ {
+		klog.Infof("This is a log message %d", i)
+	}
+}
+
+func BenchmarkKlogStructured(b *testing.B) {
+	klogtest.InitKlog()
+	klog.SetOutput(io.Discard)
+	logger := klog.Background()
+	for i := 0; i < b.N; i++ {
+		logger.Info("This is a structured log message", "index", i)
+	}
+}
+
+func setupRedirect(tb testing.TB) {
+	klogtest.InitKlog()
+	klog.SetOutput(io.Discard)
+	RedirectLogrusToLogr(logrus.StandardLogger(), klog.Background())
+	tb.Cleanup(func() {
+		logrus.StandardLogger().ReplaceHooks(make(logrus.LevelHooks))
+	})
+}
+
+func BenchmarkLogrusRedirected(b *testing.B) {
+	setupRedirect(b)
+	for i := 0; i < b.N; i++ {
+		logrus.Infof("This is a log message %d", i)
+	}
+}
+
+func BenchmarkLogrusStructuredRedirected(b *testing.B) {
+	setupRedirect(b)
+	for i := 0; i < b.N; i++ {
+		logrus.WithField("index", i).Info("This is a structured log message")
+	}
+}
