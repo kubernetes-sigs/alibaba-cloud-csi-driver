2f1b76e4b2dcdf881ff16c417ba597dcee497645
tydra-wang <jiafu.wjj@alibaba-inc.com>
2024-05-16 16:51:51 +0800

    feat(pov): mount readonly tmpfs in nodepublish

--- pkg/pov/node.go
+++ pkg/pov/node.go
@@ -2,25 +2,27 @@ package pov
 
 import (
 	"context"
+	"os"
+	"time"
+
 	"github.com/container-storage-interface/spec/lib/go/csi"
 	"github.com/kubernetes-sigs/alibaba-cloud-csi-driver/pkg/pov/internal"
-)
-
-var (
-	// nodeCaps represents the capability of node service.
-	nodeCaps = []csi.NodeServiceCapability_RPC_Type{
-		csi.NodeServiceCapability_RPC_STAGE_UNSTAGE_VOLUME,
-		csi.NodeServiceCapability_RPC_EXPAND_VOLUME,
-		// csi.NodeServiceCapability_RPC_GET_VOLUME_STATS,
-	}
+	log "github.com/sirupsen/logrus"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	mountutils "k8s.io/mount-utils"
 )
 
 type nodeService struct {
 	inFlight *internal.InFlight
+	mounter  mountutils.Interface
 }
 
 func newNodeService() nodeService {
-	return nodeService{}
+	return nodeService{
+		inFlight: internal.NewInFlight(),
+		mounter:  mountutils.New(""),
+	}
 }
 
 func (d *nodeService) NodeStageVolume(ctx context.Context, req *csi.NodeStageVolumeRequest) (*csi.NodeStageVolumeResponse, error) {
@@ -28,25 +30,66 @@ func (d *nodeService) NodeStageVolume(ctx context.Context, req *csi.NodeStageVol
 }
 
 func (d *nodeService) NodeUnstageVolume(ctx context.Context, req *csi.NodeUnstageVolumeRequest) (*csi.NodeUnstageVolumeResponse, error) {
-
 	return &csi.NodeUnstageVolumeResponse{}, nil
 }
 
 func (d *nodeService) NodeExpandVolume(ctx context.Context, req *csi.NodeExpandVolumeRequest) (*csi.NodeExpandVolumeResponse, error) {
-
 	return &csi.NodeExpandVolumeResponse{}, nil
 }
 
 func (d *nodeService) NodePublishVolume(ctx context.Context, req *csi.NodePublishVolumeRequest) (*csi.NodePublishVolumeResponse, error) {
+	if !d.inFlight.Insert(req.TargetPath) {
+		return nil, status.Errorf(codes.Aborted, "There is already an operation for %s", req.TargetPath)
+	}
+	defer d.inFlight.Delete(req.TargetPath)
+
+	notMnt, err := d.mounter.IsLikelyNotMountPoint(req.TargetPath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			if err := os.MkdirAll(req.TargetPath, os.ModePerm); err != nil {
+				return nil, status.Error(codes.Internal, err.Error())
+			}
+			notMnt = true
+		} else {
+			return nil, status.Error(codes.Internal, err.Error())
+		}
+	}
+	if !notMnt {
+		log.Infof("NodePublishVolume: %s already mounted", req.TargetPath)
+		return &csi.NodePublishVolumeResponse{}, nil
+	}
+
+	err = d.mounter.Mount("tmpfs", req.TargetPath, "tmpfs", []string{"ro"})
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "mount readonly tmpfs: %v", err)
+	}
+
+	log.WithFields(log.Fields{"volumeId": req.VolumeId, "targetPath": req.TargetPath}).Info("NodePublishVolume succeeded")
 	return &csi.NodePublishVolumeResponse{}, nil
 }
 
 func (d *nodeService) NodeUnpublishVolume(ctx context.Context, req *csi.NodeUnpublishVolumeRequest) (*csi.NodeUnpublishVolumeResponse, error) {
+	if !d.inFlight.Insert(req.TargetPath) {
+		return nil, status.Errorf(codes.Aborted, "There is already an operation for %s", req.TargetPath)
+	}
+	defer d.inFlight.Delete(req.TargetPath)
+
+	var err error
+	forceUnmounter, ok := d.mounter.(mountutils.MounterForceUnmounter)
+	if ok {
+		err = mountutils.CleanupMountWithForce(req.TargetPath, forceUnmounter, false, time.Minute)
+	} else {
+		err = mountutils.CleanupMountPoint(req.TargetPath, d.mounter, false)
+	}
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "umount %s: %v", req.TargetPath, err)
+	}
+
+	log.WithFields(log.Fields{"volumeId": req.VolumeId, "targetPath": req.TargetPath}).Info("NodeUnpublishVolume succeeded")
 	return &csi.NodeUnpublishVolumeResponse{}, nil
 }
 
 func (d *nodeService) NodeGetInfo(ctx context.Context, req *csi.NodeGetInfoRequest) (*csi.NodeGetInfoResponse, error) {
-
 	segments := map[string]string{
 		TopologyKey: GlobalConfigVar.regionID,
 	}
@@ -64,18 +107,7 @@ func (d *nodeService) getVolumesLimit() int64 {
 }
 
 func (d *nodeService) NodeGetCapabilities(ctx context.Context, req *csi.NodeGetCapabilitiesRequest) (*csi.NodeGetCapabilitiesResponse, error) {
-	var caps []*csi.NodeServiceCapability
-	for _, cap := range nodeCaps {
-		c := &csi.NodeServiceCapability{
-			Type: &csi.NodeServiceCapability_Rpc{
-				Rpc: &csi.NodeServiceCapability_RPC{
-					Type: cap,
-				},
-			},
-		}
-		caps = append(caps, c)
-	}
-	return &csi.NodeGetCapabilitiesResponse{Capabilities: caps}, nil
+	return &csi.NodeGetCapabilitiesResponse{}, nil
 }
 
 func (d *nodeService) NodeGetVolumeStats(ctx context.Context, req *csi.NodeGetVolumeStatsRequest) (*csi.NodeGetVolumeStatsResponse, error) {
