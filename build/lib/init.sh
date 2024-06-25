kubernetes-sigs/alibaba-cloud-csi-driver

commit c67535f358679194c34cac29793ab23fafb0c273

build: install alinas-utils on Ubuntu

build/lib/init.sh
@@ -13,23 +13,22 @@ mkdir -p /var/log/alicloud/
 mkdir -p /host/etc/kubernetes/volumes/disk/uuid
 
 HOST_CMD="/usr/bin/nsenter --mount=/proc/1/ns/mnt"
+HOST_ROOT="/proc/1/root"
 
 host_os="centos"
-${HOST_CMD} ls /etc/os-release
-os_release_exist=$?
 
-if [[ "$os_release_exist" = "0" ]]; then
-    osID=`${HOST_CMD} cat /etc/os-release | grep "ID=" | grep -v "VERSION_ID"`
-    if [[ `echo ${osID} | grep "alinux" | wc -l` != "0" ]]; then
-        host_os="alinux$(eval $($HOST_CMD cat /etc/os-release); echo ${VERSION_ID} | cut -d'.' -f1)"
-    fi
-		if [[ `echo ${osID} | grep "lifsea" | wc -l` != "0" ]]; then
-        host_os="lifsea"
-    fi
-    if [[ `echo ${osID} | grep "anolis" | wc -l` != "0" ]]; then
-        osVersion=`${HOST_CMD} cat /etc/os-release | grep "VERSION_ID=" | grep "^VERSION_ID=\"8"`
-        if [[ "${osVersion}" ]]; then
-            host_os="anolis8"
+OS_RELEASE=/etc/os-release
+if ! [ -f $HOST_ROOT$OS_RELEASE ]; then
+    OS_RELEASE=/usr/lib/os-release
+fi
+if [ -f $HOST_ROOT$OS_RELEASE ]; then
+    OS_RELEASE_ID=$(grep -oP '(?<=^ID=).+' $HOST_ROOT$OS_RELEASE | tr -d '"')
+    if [ -n "$OS_RELEASE_ID" ]; then
+        host_os="$OS_RELEASE_ID"
+
+        if [[ $OS_RELEASE_ID = alinux ]]; then
+            OS_RELEASE_VERSION_ID=$(grep -oP '(?<=^VERSION_ID=).+' $HOST_ROOT$OS_RELEASE | tr -d '"')
+            host_os="alinux$(echo $OS_RELEASE_VERSION_ID | cut -d'.' -f1)"
         fi
     fi
 fi
@@ -212,7 +211,18 @@ if [ "$DISABLE_CSIPLUGIN_CONNECTOR" != "true" ] && ([ "$run_oss" = "true" ] || [
     done
 fi
 
-echo "Start checking if the rpm package needs to be installed"
+echo "Start checking if the host system packages needs to be installed"
+
+if $HOST_CMD yum --version; then
+    echo "yum is available"
+    PKG_CMD="yum install -y"
+    HAS_YUM=true
+elif $HOST_CMD apt-get --version; then
+    echo "apt-get is available"
+    PKG_CMD="apt-get install -y"
+    HAS_APT=true
+fi
+
 if ([ "$DISK_BDF_ENABLE" = "true" ] && [ "$run_disk" = "true" ]) || [ "$run_pov" = "true" ]; then
     isbdf="false"
     for i in $(${HOST_CMD} lspci -D | grep "storage controller" | grep "1ded\|Alibaba" | awk '{print $1}' |  sed -n '/0$/p'); 
@@ -263,12 +273,20 @@ if [ "$ARCH" = "x86_64" ] && [ "$run_nas" = "true" ]; then
     if [ $install_utils = "true" ]; then
         # cpfs-nas nas-rich-client common rpm
         echo "installing aliyun-alinas-utils"
-        cp /root/aliyun-alinas-utils-1.1-8.al7.noarch.rpm /host/etc/csi-tool/
-        ${HOST_CMD} yum install -y /etc/csi-tool/aliyun-alinas-utils-1.1-8.al7.noarch.rpm
-
-        if ! ${HOST_CMD} rpm -q aliyun-alinas-utils | grep -E "^aliyun-alinas-utils-1.1-8" >/dev/null; then
-            echo "failed to install aliyun-alinas-utils"
-            exit 1
+        if [ "$HAS_YUM" = "true" ]; then
+            PKG=aliyun-alinas-utils-1.1-8.al7.noarch.rpm
+        elif [ "$HAS_APT" = "true" ]; then
+            PKG=aliyun-alinas-utils-1.1-8.deb
+        else
+            echo "WARN: no package manager found, skip install aliyun-alinas-utils"
+            install_utils="false"
+        fi
+        if [ $install_utils = "true" ]; then
+            cp /root/$PKG /host/etc/csi-tool/
+            ${HOST_CMD} $PKG_CMD /etc/csi-tool/$PKG || {
+                echo "failed to install aliyun-alinas-utils"
+                exit 1
+            }
         fi
     fi
 
@@ -290,4 +308,4 @@ if [ "$ARCH" = "x86_64" ] && [ "$run_nas" = "true" ]; then
 fi
 
 # place it here to remove leftover from previous version
-rm -rf /host/etc/csi-tool/*.rpm
+rm -rf /host/etc/csi-tool/*.{rpm,deb}

build/multi/Dockerfile.multi
@@ -17,6 +17,16 @@ RUN --mount=type=bind,target=. \
     go build -o /out/csiplugin-connector ./build/lib/csiplugin-connector.go
 
 
+FROM --platform=$BUILDPLATFORM registry.eu-west-1.aliyuncs.com/acs/alinux:3-update as download
+ARG TARGETARCH
+WORKDIR /downloads
+RUN CURL="curl --remote-time --remote-name --fail" && \
+    if [[ $TARGETARCH == "amd64" ]]; then \
+        $CURL -H "If-Match: D7AE0A7E48FBB6CF7BDB33C770B5EE0C" https://aliyun-alinas-eac.oss-cn-beijing.aliyuncs.com/alinas-efc-1.2-3.x86_64.rpm && \
+        $CURL -H "If-Match: 9E3E94108EE24C522319B14EF74BDCFC" https://aliyun-encryption.oss-cn-beijing.aliyuncs.com/aliyun-alinas-utils-1.1-8.al7.noarch.rpm && \
+        $CURL -H "If-Match: 259C07CF1C83C71DD78B705BC2E3FAE2" https://aliyun-encryption.oss-cn-beijing.aliyuncs.com/aliyun-alinas-utils-1.1-8.deb; \
+    fi
+
 FROM registry.eu-west-1.aliyuncs.com/acs/alinux:3-update as base
 LABEL maintainers="Alibaba Cloud Authors" description="Alibaba Cloud CSI Plugin"
 
@@ -46,16 +56,13 @@ FROM base as init
 
 ARG TARGETARCH
 WORKDIR /root
-RUN if [[ $TARGETARCH == "amd64" ]]; then \
-        curl -O https://aliyun-alinas-eac.oss-cn-beijing.aliyuncs.com/alinas-efc-1.2-3.x86_64.rpm && \
-        curl -O https://aliyun-encryption.oss-cn-beijing.aliyuncs.com/aliyun-alinas-utils-1.1-8.al7.noarch.rpm; \
-    fi
 RUN --mount=type=bind,source=build/lib,target=/csi-lib \
     cp /csi-lib/init.sh /init.sh && \
     cp /csi-lib/freezefs.sh /freezefs.sh && \
     mkdir /csi && \
     cp /csi-lib/csiplugin-connector.service /csi/csiplugin-connector.service
 COPY --link --from=build /out/csiplugin-connector /csi/csiplugin-connector
+COPY --link --from=download /downloads /root
 ENTRYPOINT ["/init.sh"]
 
 FROM base

commit 9b7158f0d4819da0831ffdd4eb4651c37e052b50

init: fix restart and downgrade for yum
yum install is not idempotent, so we need to check if the package is already installed.
yum also fails if the already installed version is newer, so we need to downgrade.
dnf do all of these automatically.

Fixes: c67535f358679194c34cac29793ab23fafb0c273

build/lib/init.sh
@@ -213,15 +213,42 @@ fi
 
 echo "Start checking if the host system packages needs to be installed"
 
-if $HOST_CMD yum --version; then
+if $HOST_CMD dnf --version; then
+    echo "dnf is available"
+    PKG_CMD=(dnf install -y)
+    HAS_DNF=true
+    HAS_YUM=true
+elif $HOST_CMD yum --version; then
     echo "yum is available"
-    PKG_CMD="yum install -y"
+    # yum install is not idempotent, so we need to check if the package is already installed.
+    # yum also fails if the already installed version is newer, so we need to downgrade.
+    # fail if both install and downgrade fail
+    PKG_CMD=(sh -c 'if rpm --query $(rpm --query --package $1); then
+        echo $1 already installed;
+    else
+        yum install -y $1 || yum downgrade -y $1;
+    fi' install_pkg)
     HAS_YUM=true
 elif $HOST_CMD apt-get --version; then
     echo "apt-get is available"
-    PKG_CMD="apt-get install -y"
+    PKG_CMD=(apt-get install -y --allow-downgrades)
     HAS_APT=true
 fi
+
+# idempotent package installation
+# downgrade if the already installed version is newer than the one we want to install
+function install_package() {
+    cp /root/$1 /host/etc/csi-tool/
+    if [ -z $PKG_CMD ]; then
+        echo "no package manager found"
+        return 1
+    fi
+    ${HOST_CMD} "${PKG_CMD[@]}" /etc/csi-tool/$1 || {
+        echo "failed to install $1"
+        return 1
+    }
+    rm -f /host/etc/csi-tool/$1
+}
 
 if ([ "$DISK_BDF_ENABLE" = "true" ] && [ "$run_disk" = "true" ]) || [ "$run_pov" = "true" ]; then
     isbdf="false"
@@ -278,15 +305,11 @@ if [ "$ARCH" = "x86_64" ] && [ "$run_nas" = "true" ]; then
         elif [ "$HAS_APT" = "true" ]; then
             PKG=aliyun-alinas-utils-1.1-8.deb
         else
-            echo "WARN: no package manager found, skip install aliyun-alinas-utils"
+            echo "WARN: no supported package manager found, skip install aliyun-alinas-utils"
             install_utils="false"
         fi
         if [ $install_utils = "true" ]; then
-            cp /root/$PKG /host/etc/csi-tool/
-            ${HOST_CMD} $PKG_CMD /etc/csi-tool/$PKG || {
-                echo "failed to install aliyun-alinas-utils"
-                exit 1
-            }
+            install_package $PKG || exit $?
         fi
     fi
 