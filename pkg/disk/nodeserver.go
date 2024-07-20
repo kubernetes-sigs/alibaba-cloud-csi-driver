bcf65a4b7730a8bca0bdead7a86e6952916d9e25
胡玮文 <huweiwen.hww@alibaba-inc.com>
2024-07-20 15:05:16 +0800

    disk: add fromController arg to attachDisk

--- pkg/disk/nodeserver.go
+++ pkg/disk/nodeserver.go
@@ -583,7 +583,7 @@ func (ns *nodeServer) NodeStageVolume(ctx context.Context, req *csi.NodeStageVol
 			}
 		}
 	} else {
-		device, err = attachDisk(ctx, req.VolumeContext[TenantUserUID], req.GetVolumeId(), ns.nodeID, isSharedDisk)
+		device, err = attachDisk(ctx, req.VolumeContext[TenantUserUID], req.GetVolumeId(), ns.nodeID, isSharedDisk, false)
 		if err != nil {
 			fullErrorMessage := utils.FindSuggestionByErrorMessage(err.Error(), utils.DiskAttachDetach)
 			log.Errorf("NodeStageVolume: Attach volume: %s with error: %s", req.VolumeId, fullErrorMessage)
