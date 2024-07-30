d589624819cbd09634c7d6f5cef768f204875145
胡玮文 <huweiwen.hww@alibaba-inc.com>
2024-07-30 14:06:33 +0800

    introduce GenericIdentityServer
    to replace DefaultIdentityServer in csicommon, which is deprecated.

---
+++ pkg/common/identityserver.go
@@ -0,0 +1,37 @@
+package common
+
+import (
+	"context"
+
+	"github.com/container-storage-interface/spec/lib/go/csi"
+	"github.com/kubernetes-sigs/alibaba-cloud-csi-driver/pkg/version"
+)
+
+type GenericIdentityServer struct {
+	Name string
+}
+
+func (*GenericIdentityServer) GetPluginCapabilities(ctx context.Context, req *csi.GetPluginCapabilitiesRequest) (*csi.GetPluginCapabilitiesResponse, error) {
+	return &csi.GetPluginCapabilitiesResponse{
+		Capabilities: []*csi.PluginCapability{
+			{
+				Type: &csi.PluginCapability_Service_{
+					Service: &csi.PluginCapability_Service{
+						Type: csi.PluginCapability_Service_CONTROLLER_SERVICE,
+					},
+				},
+			},
+		},
+	}, nil
+}
+
+func (s *GenericIdentityServer) GetPluginInfo(context.Context, *csi.GetPluginInfoRequest) (*csi.GetPluginInfoResponse, error) {
+	return &csi.GetPluginInfoResponse{
+		Name:          s.Name,
+		VendorVersion: version.VERSION,
+	}, nil
+}
+
+func (*GenericIdentityServer) Probe(ctx context.Context, req *csi.ProbeRequest) (*csi.ProbeResponse, error) {
+	return &csi.ProbeResponse{}, nil
+}
