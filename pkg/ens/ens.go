688f226bfffb520bb7d764ed7f7efbf247490100
胡玮文 <huweiwen.hww@alibaba-inc.com>
2024-07-30 14:13:27 +0800

    ens: DefaultIdentityServer -> GenericIdentityServer

--- pkg/ens/ens.go
+++ pkg/ens/ens.go
@@ -7,7 +7,6 @@ import (
 	"sync"
 
 	"github.com/container-storage-interface/spec/lib/go/csi"
-	csicommon "github.com/kubernetes-csi/drivers/pkg/csi-common"
 	"github.com/kubernetes-sigs/alibaba-cloud-csi-driver/pkg/common"
 	"github.com/kubernetes-sigs/alibaba-cloud-csi-driver/pkg/options"
 	"github.com/kubernetes-sigs/alibaba-cloud-csi-driver/pkg/utils"
@@ -50,7 +49,6 @@ var (
 )
 
 type ENS struct {
-	driver           *csicommon.CSIDriver
 	endpoint         string
 	idServer         csi.IdentityServer
 	nodeServer       csi.NodeServer
@@ -67,10 +65,7 @@ func NewDriver(nodeID, endpoint string) *ENS {
 
 	NewGlobalConfig()
 
-	csiDriver := csicommon.NewCSIDriver(driverName, version.VERSION, GlobalConfigVar.InstanceID)
-	tmpENS.driver = csiDriver
-
-	tmpENS.idServer = NewIdentityServer(tmpENS.driver)
+	tmpENS.idServer = NewIdentityServer()
 	if GlobalConfigVar.ControllerService {
 		tmpENS.controllerServer = NewControllerServer()
 	} else {
