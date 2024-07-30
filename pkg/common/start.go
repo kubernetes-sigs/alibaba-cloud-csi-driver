8d808729dbd62c7ed2434f65393d1cc1e713d618
胡玮文 <huweiwen.hww@alibaba-inc.com>
2024-07-30 14:38:58 +0800

    copy over code to RunCSIServer and remove csi-common
    Because glog is also implicitly removed, we should add klog.InitFlags to keep compatible with the "-v" and related flags.

--- pkg/common/start.go
+++ pkg/common/start.go
@@ -1,16 +1,77 @@
 package common
 
 import (
+	"context"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+
 	"github.com/container-storage-interface/spec/lib/go/csi"
-	csicommon "github.com/kubernetes-csi/drivers/pkg/csi-common"
+	"github.com/kubernetes-csi/csi-lib-utils/protosanitizer"
+	log "github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
 )
+
+func logGRPC(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	log.Infof("GRPC call: %s", info.FullMethod)
+	log.Debugf("GRPC request: %s", protosanitizer.StripSecrets(req))
+	resp, err := handler(ctx, req)
+	if err != nil {
+		log.Errorf("GRPC error: %v", err)
+	} else {
+		log.Debugf("GRPC response: %s", protosanitizer.StripSecrets(resp))
+	}
+	return resp, err
+}
+
+func ParseEndpoint(ep string) (string, string, error) {
+	if strings.HasPrefix(strings.ToLower(ep), "unix://") || strings.HasPrefix(strings.ToLower(ep), "tcp://") {
+		s := strings.SplitN(ep, "://", 2)
+		if s[1] != "" {
+			return s[0], s[1], nil
+		}
+	}
+	return "", "", fmt.Errorf("Invalid endpoint: %v", ep)
+}
 
 func RunCSIServer(endpoint string, ids csi.IdentityServer, cs csi.ControllerServer, ns csi.NodeServer) {
 	ns = WrapNodeServerWithValidator(ns)
 	cs = WrapControllerServerWithValidator(cs)
 
-	// start grpc server
-	s := csicommon.NewNonBlockingGRPCServer()
-	s.Start(endpoint, ids, cs, ns)
-	s.Wait()
+	proto, addr, err := ParseEndpoint(endpoint)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	if proto == "unix" {
+		addr = "/" + addr
+		if err := os.Remove(addr); err != nil && !os.IsNotExist(err) {
+			log.Fatalf("Failed to remove %s, error: %s", addr, err.Error())
+		}
+	}
+
+	listener, err := net.Listen(proto, addr)
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	opts := []grpc.ServerOption{
+		grpc.UnaryInterceptor(logGRPC),
+	}
+	server := grpc.NewServer(opts...)
+
+	if ids != nil {
+		csi.RegisterIdentityServer(server, ids)
+	}
+	if cs != nil {
+		csi.RegisterControllerServer(server, cs)
+	}
+	if ns != nil {
+		csi.RegisterNodeServer(server, ns)
+	}
+
+	log.Infof("Listening for connections on address: %#v", listener.Addr())
+
+	server.Serve(listener)
 }
