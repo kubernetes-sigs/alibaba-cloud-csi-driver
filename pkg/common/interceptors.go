515ad5d43b9a1925d6f7eadffb5a8dbc07a40c29
胡玮文 <huweiwen.hww@alibaba-inc.com>
2024-11-18 02:01:14 +0800

    common: update instrumentGRPC to consider context error
    align with GRPC itself

--- pkg/common/interceptors.go
+++ pkg/common/interceptors.go
@@ -39,10 +39,18 @@ func instrumentGRPC(driverType string) grpc.UnaryServerInterceptor {
 }
 
 func recordExecTime(time time.Duration, method, driverType string, err error) {
+	// copied from google.golang.org/grpc/server.go
+	appStatus, ok := status.FromError(err)
+	if !ok {
+		// Convert non-status application error to a status error with code
+		// Unknown, but handle context errors specifically.
+		appStatus = status.FromContextError(err)
+	}
+
 	labels := prometheus.Labels{
 		metric.CsiGrpcExecTimeLabelMethod: method,
 		metric.CsiGrpcExecTimeLabelType:   driverType,
-		metric.CsiGrpcExecTimeLabelCode:   status.Code(err).String(),
+		metric.CsiGrpcExecTimeLabelCode:   appStatus.Code().String(),
 	}
 	metric.CsiGrpcExecTimeCollector.ExecCountMetric.With(labels).Inc()
 	metric.CsiGrpcExecTimeCollector.ExecTimeTotalMetric.With(labels).Add(time.Seconds())
