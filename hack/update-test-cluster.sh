c93da959fdaabe0f252a0969d19b10d6c63f297a
胡玮文 <huweiwen.hww@alibaba-inc.com>
2024-12-07 05:14:37 +0800

    test: bump containerd version
    also include a script to automate this in the future

---
+++ hack/update-test-cluster.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+read -r k8s containerd alinux3 alinux3arm containeros < \
+    <(aliyun cs GET /api/v1/metadata/versions --Region 'cn-beijing' --ClusterType ManagedKubernetes | \
+        jq -r '.[0] | [
+            (.version | tojson),
+            (.runtimes.[] | select(.name=="containerd") | .version | tojson),
+            (.images.[] | select(.image_type=="AliyunLinux3") | .image_id),
+            (.images.[] | select(.image_type=="AliyunLinux3Arm64") | .image_id),
+            (.images.[] | select(.image_type=="ContainerOS") | .image_id)
+        ] | @tsv')
+
+echo "     cluster: $k8s"
+echo "  containerd: $containerd"
+echo "    ALinux 3: $alinux3"
+echo "ALinux 3 ARM: $alinux3arm"
+echo " ContainerOS: $containeros"
+
+sed -i '' "s/kubernetes_version:.*/kubernetes_version: $k8s,/;
+           s/runtime_version:.*/runtime_version: $containerd,/;" \
+    test/cluster/cluster-template.jsonnet
+
+sed -i '' "s/\${OS_IMAGE_ALINUX3.*}/\${OS_IMAGE_ALINUX3:-$alinux3}/;
+           s/\${OS_IMAGE_ALINUX3_ARM64.*}/\${OS_IMAGE_ALINUX3_ARM64:-$alinux3arm}/;
+           s/\${OS_IMAGE_CONTAINEROS3.*}/\${OS_IMAGE_CONTAINEROS3:-$containeros}/;" \
+    test/cluster/cluster_setup.sh
