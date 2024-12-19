f104d733301c9b89915d4c7f6970c83930eba680
胡玮文 <huweiwen.hww@alibaba-inc.com>
2024-12-19 21:08:58 +0800

    support for json format log output

--- main.go
+++ main.go
@@ -48,6 +48,9 @@ import (
 	"github.com/sirupsen/logrus"
 	"golang.org/x/sys/unix"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/component-base/logs"
+	logsapi "k8s.io/component-base/logs/api/v1"
+	_ "k8s.io/component-base/logs/json/register"
 	"k8s.io/klog/v2"
 )
 
@@ -87,15 +90,31 @@ var (
 	rootDir = flag.String("rootdir", "/var/lib/kubelet/csi-plugins", "Kubernetes root directory")
 )
 
-func main() {
+func setupFlags() {
 	csilog.RedirectLogrusToLogr(logrus.StandardLogger(), klog.Background())
 
-	utils.AddKlogFlags(flag.CommandLine)
 	utils.AddGoFlags(flag.CommandLine)
 
-	flag.Var(features.FunctionalMutableFeatureGate, "feature-gates", "A set of key=value pairs that describe feature gates for alpha/experimental features. "+
-		"Options are:\n"+strings.Join(features.FunctionalMutableFeatureGate.KnownFeatures(), "\n"))
+	fg := features.FunctionalMutableFeatureGate
+	err := logsapi.AddFeatureGates(fg)
+	if err != nil {
+		panic(err) // just panic since the log infra is not initialized yet.
+	}
+	flag.Var(fg, "feature-gates", "A set of key=value pairs that describe feature gates for alpha/experimental features. "+
+		"Options are:\n"+strings.Join(fg.KnownFeatures(), "\n"))
+
+	c := logsapi.NewLoggingConfiguration()
+	logsapi.AddFlags(c, flag.CommandLine)
+	logs.InitLogs()
 	flag.Parse()
+	if err := logsapi.ValidateAndApply(c, fg); err != nil {
+		klog.ErrorS(err, "LoggingConfiguration is invalid")
+		klog.FlushAndExit(klog.ExitFlushTimeout, 1)
+	}
+}
+
+func main() {
+	setupFlags()
 	serviceType := utils.GetServiceType(*runAsController, *runControllerService, *runNodeService)
 
 	// enable pprof analyse
