b1e0a55265543304f0e7a31ef5cc1735591f34da
胡玮文 <huweiwen.hww@alibaba-inc.com>
2024-12-21 23:34:33 +0800

    Read configmap from volume mount
    Avoid RBAC permission to configmap and save some APIServer call.
    The permission to get ack-cluster-profile is moved to a separate role, not assuming we are installed in kube-system.

--- pkg/disk/disk.go
+++ pkg/disk/disk.go
@@ -17,7 +17,6 @@ limitations under the License.
 package disk
 
 import (
-	"context"
 	"os"
 	"strconv"
 	"strings"
@@ -30,7 +29,6 @@ import (
 	"github.com/kubernetes-sigs/alibaba-cloud-csi-driver/pkg/options"
 	"github.com/kubernetes-sigs/alibaba-cloud-csi-driver/pkg/utils"
 	"github.com/kubernetes-sigs/alibaba-cloud-csi-driver/pkg/version"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
@@ -140,8 +138,6 @@ func (disk *DISK) Run() {
 
 // GlobalConfigSet set Global Config
 func GlobalConfigSet(m metadata.MetadataProvider) {
-	configMapName := "csi-plugin"
-
 	// Global Configs Set
 	cfg, err := clientcmd.BuildConfigFromFlags(options.MasterURL, options.Kubeconfig)
 	if err != nil {
@@ -170,13 +166,7 @@ func GlobalConfigSet(m metadata.MetadataProvider) {
 		klog.Fatalf("Error building kubernetes clientset: %s", err.Error())
 	}
 
-	csiCfg := utils.Config{}
-	configMap, err := kubeClient.CoreV1().ConfigMaps("kube-system").Get(context.Background(), configMapName, metav1.GetOptions{})
-	if err != nil {
-		klog.Infof("Not found configmap named as csi-plugin under kube-system, with: %v", err)
-	} else {
-		csiCfg.ConfigMap = configMap.Data
-	}
+	csiCfg := utils.DefaultConfig()
 
 	// Env variables
 	avmfe := os.Getenv("ADDON_VM_FATAL_EVENTS")
