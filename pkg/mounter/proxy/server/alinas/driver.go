483425091b13458cbeba9dc3f222d9c1c4aa1c7b
tydra-wang <jiafu.wjj@alibaba-inc.com>
2025-03-17 11:41:42 +0800

    feat(mount-proxy): support cnfs-alinas-damon mode
    fix(mount-proxy/alinas): set pgid for watchdog
    
    bump efc version

--- pkg/mounter/proxy/server/alinas/driver.go
+++ pkg/mounter/proxy/server/alinas/driver.go
@@ -2,7 +2,12 @@ package alinas
 
 import (
 	"context"
+	"io"
+	"io/fs"
+	"os"
 	"os/exec"
+	"path/filepath"
+	"syscall"
 	"time"
 
 	"github.com/kubernetes-sigs/alibaba-cloud-csi-driver/pkg/mounter/proxy"
@@ -45,6 +50,7 @@ func (h *Driver) Mount(ctx context.Context, req *proxy.MountRequest) error {
 func (h *Driver) Init() {
 	go runCommandForever("aliyun-alinas-mount-watchdog")
 	go runCommandForever("aliyun-cpfs-mount-watchdog")
+	setupDefaultConfigs()
 }
 
 func (h *Driver) Terminate() {}
@@ -53,9 +59,73 @@ func runCommandForever(command string, args ...string) {
 	wait.Forever(func() {
 		klog.InfoS("Starting", "command", command)
 		cmd := exec.Command(command, args...)
+		cmd.SysProcAttr = &syscall.SysProcAttr{
+			Setpgid: true,
+		}
 		err := cmd.Run()
 		if err != nil {
 			klog.ErrorS(err, "Exited", "command", command)
 		}
 	}, time.Second)
+}
+
+const (
+	defaultConfigDir = "/etc/aliyun-defaults"
+	configDir        = "/etc/aliyun"
+)
+
+func setupDefaultConfigs() {
+	for _, name := range []string{"cpfs", "alinas"} {
+		srcDir := filepath.Join(defaultConfigDir, name)
+		dstDir := filepath.Join(configDir, name)
+		if err := filepath.WalkDir(srcDir, func(path string, d fs.DirEntry, err error) error {
+			if err != nil {
+				return err
+			}
+
+			if d.IsDir() {
+				return nil
+			}
+
+			relPath, err := filepath.Rel(srcDir, path)
+			if err != nil {
+				return err
+			}
+
+			dstPath := filepath.Join(dstDir, relPath)
+
+			if _, err := os.Stat(dstPath); err == nil {
+				// File already exists, skip
+				return nil
+			} else if !os.IsNotExist(err) {
+				return err
+			}
+
+			klog.InfoS("Copying default config file", "path", dstPath)
+			return copyFile(path, dstPath)
+		}); err != nil {
+			panic(err)
+		}
+	}
+}
+
+func copyFile(src, dst string) error {
+	srcFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
+
+	dstFile, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer dstFile.Close()
+
+	_, err = io.Copy(dstFile, srcFile)
+	if err != nil {
+		return err
+	}
+
+	return dstFile.Sync()
 }
