kubernetes-sigs/alibaba-cloud-csi-driver

commit 5defa961b54a444feef153e87cf6706e4f4a81b9

unify node/controller service enabling
Every driver supports enable/disable node or controller service, with unified interface.

deprecate SERVICE_TYPE env
deprecate -run-as-controller flags
introduce -run-controller-service and -run-node-service,
allowing controller and node service to be enabled/disabled independently.

main.go
@@ -77,20 +77,16 @@ const (
 )
 
 var (
-	endpoint        = flag.String("endpoint", "unix://tmp/csi.sock", "CSI endpoint")
-	nodeID          = flag.String("nodeid", "", "node id")
-	runAsController = flag.Bool("run-as-controller", false, "Only run as controller service")
-	driver          = flag.String("driver", TypePluginDISK, "CSI Driver")
+	endpoint             = flag.String("endpoint", "unix://tmp/csi.sock", "CSI endpoint")
+	nodeID               = flag.String("nodeid", "", "node id")
+	runAsController      = flag.Bool("run-as-controller", false, "Only run as controller service (deprecated)")
+	runControllerService = flag.Bool("run-controller-service", true, "activate CSI controller service")
+	runNodeService       = flag.Bool("run-node-service", true, "activate CSI node service")
+	driver               = flag.String("driver", TypePluginDISK, "CSI Driver")
 	// Deprecated: rootDir is instead by KUBELET_ROOT_DIR env.
 	rootDir = flag.String("rootdir", "/var/lib/kubelet/csi-plugins", "Kubernetes root directory")
 )
 
-type globalMetricConfig struct {
-	enableMetric bool
-	serviceType  string
-}
-
-// Nas CSI Plugin
 func main() {
 	csilog.RedirectLogrusToLogr(logrus.StandardLogger(), klog.Background())
 
@@ -98,17 +94,8 @@ func main() {
 		"Options are:\n"+strings.Join(features.FunctionalMutableFeatureGate.KnownFeatures(), "\n"))
 	klog.InitFlags(nil)
 	flag.Parse()
+	serviceType := utils.GetServiceType(*runAsController, *runControllerService, *runNodeService)
 
-	serviceType := os.Getenv(utils.ServiceType)
-
-	if len(serviceType) == 0 || serviceType == "" {
-		serviceType = utils.PluginService
-	}
-
-	// When serviceType is neither plugin nor provisioner, the program will exits.
-	if serviceType != utils.PluginService && serviceType != utils.ProvisionerService {
-		klog.Fatalf("Service type is unknown:%s", serviceType)
-	}
 	// enable pprof analyse
 	pprofPort := os.Getenv("PPROF_PORT")
 	if pprofPort != "" {
@@ -184,13 +171,13 @@ func main() {
 		case TypePluginOSS:
 			go func(endPoint string) {
 				defer wg.Done()
-				driver := oss.NewDriver(endPoint, meta, *runAsController)
+				driver := oss.NewDriver(endPoint, meta, serviceType)
 				driver.Run()
 			}(endPointName)
 		case TypePluginDISK:
 			go func(endPoint string) {
 				defer wg.Done()
-				driver := disk.NewDriver(meta, endPoint, *runAsController)
+				driver := disk.NewDriver(meta, endPoint, serviceType)
 				driver.Run()
 			}(endPointName)
 
@@ -199,7 +186,7 @@ func main() {
 		case TypePluginENS:
 			go func(endpoint string) {
 				defer wg.Done()
-				driver := ens.NewDriver(*nodeID, endpoint)
+				driver := ens.NewDriver(*nodeID, endpoint, serviceType)
 				driver.Run()
 			}(endPointName)
 		// TODO: remove entire rund-csi protocol 1.0
@@ -212,7 +199,7 @@ func main() {
 		case TypePluginPOV:
 			go func(endPoint string) {
 				defer wg.Done()
-				driver := pov.NewDriver(meta, endPoint, *runAsController)
+				driver := pov.NewDriver(meta, endPoint, serviceType)
 				driver.Run()
 			}(endPointName)
 		default:
@@ -230,34 +217,25 @@ func main() {
 
 	servicePort := os.Getenv("SERVICE_PORT")
 
-	if len(servicePort) == 0 || servicePort == "" {
-		switch serviceType {
-		case utils.PluginService:
-			servicePort = PluginServicePort
-		case utils.ProvisionerService:
+	if servicePort == "" {
+		servicePort = PluginServicePort
+		if serviceType&utils.Controller != 0 {
 			servicePort = ProvisionerServicePort
-		default:
 		}
 	}
 
-	metricConfig := &globalMetricConfig{
-		true,
-		"plugin",
-	}
-
-	enableMetric := os.Getenv("ENABLE_METRIC")
 	version.SetPrometheusVersion()
-	if enableMetric == "false" {
-		metricConfig.enableMetric = false
+	enableMetric := true
+	if os.Getenv("ENABLE_METRIC") == "false" {
+		enableMetric = false
 	}
-	metricConfig.serviceType = serviceType
 
 	klog.Info("CSI is running status.")
 	csiMux := http.NewServeMux()
 	csiMux.HandleFunc("/healthz", healthHandler)
 	klog.Infof("Metric listening on address: /healthz")
-	if metricConfig.enableMetric {
-		metricHandler := metric.NewMetricHandler(metricConfig.serviceType, driverNames)
+	if enableMetric && serviceType&utils.Node != 0 {
+		metricHandler := metric.NewMetricHandler(driverNames)
 		csiMux.Handle("/metrics", metricHandler)
 		klog.Infof("Metric listening on address: /metrics")
 	}

pkg/disk/disk.go
@@ -68,7 +68,6 @@ type GlobalConfig struct {
 	DiskBdfEnable         bool
 	ClientSet             *kubernetes.Clientset
 	ClusterID             string
-	ControllerService     bool
 	BdfHealthCheck        bool
 	DiskMultiTenantEnable bool
 	CheckBDFHotPlugin     bool
@@ -93,13 +92,20 @@ func initDriver() {
 }
 
 // NewDriver create the identity/node/controller server and disk driver
-func NewDriver(m metadata.MetadataProvider, endpoint string, runAsController bool) *DISK {
+func NewDriver(m metadata.MetadataProvider, endpoint string, serviceType utils.ServiceType) *DISK {
 	initDriver()
 	tmpdisk := &DISK{}
 	tmpdisk.endpoint = endpoint
 
 	// Config Global vars
 	GlobalConfigSet(m)
+
+	if serviceType&utils.Node != 0 {
+		GlobalConfigVar.NodeID = metadata.MustGet(m, metadata.InstanceID)
+		DefaultDeviceManager.DisableSerial = IsVFNode()
+	} else {
+		GlobalConfigVar.NodeID = "not-retrieved" // make csi-common happy
+	}
 
 	// Init ECS Client
 	accessControl := utils.GetAccessControl()
@@ -113,9 +119,10 @@ func NewDriver(m metadata.MetadataProvider, endpoint string, runAsController boo
 
 	// Create GRPC servers
 	tmpdisk.idServer = NewIdentityServer()
-	tmpdisk.controllerServer = NewControllerServer()
-
-	if !runAsController {
+	if serviceType&utils.Controller != 0 {
+		tmpdisk.controllerServer = NewControllerServer()
+	}
+	if serviceType&utils.Node != 0 {
 		tmpdisk.nodeServer = NewNodeServer(m)
 	}
 
@@ -181,19 +188,9 @@ func GlobalConfigSet(m metadata.MetadataProvider) {
 
 	nodeName := os.Getenv(kubeNodeName)
 
-	controllerServerType := false
-	nodeID := ""
-	if os.Getenv(utils.ServiceType) == utils.ProvisionerService {
-		controllerServerType = true
-		nodeID = "controller" // make csi-common happy
-	} else {
-		nodeID = metadata.MustGet(m, metadata.InstanceID)
-	}
-
 	// Global Config Set
 	GlobalConfigVar = GlobalConfig{
 		Region: metadata.MustGet(m, metadata.RegionID),
-		NodeID: nodeID,
 		ADControllerEnable: features.FunctionalMutableFeatureGate.Enabled(features.DiskADController) ||
 			csiCfg.GetBool("disk-adcontroller-enable", "DISK_AD_CONTROLLER", false),
 		DiskTagEnable:         csiCfg.GetBool("disk-tag-by-plugin", "DISK_TAGED_BY_PLUGIN", false),
@@ -205,7 +202,6 @@ func GlobalConfigSet(m metadata.MetadataProvider) {
 		ClientSet:             kubeClient,
 		SnapClient:            snapClient,
 		ClusterID:             os.Getenv("CLUSTER_ID"),
-		ControllerService:     controllerServerType,
 		BdfHealthCheck:        csiCfg.GetBool("bdf-health-check", "BDF_HEALTH_CHECK", true),
 		DiskMultiTenantEnable: csiCfg.GetBool("disk-multi-tenant-enable", "DISK_MULTI_TENANT_ENABLE", false),
 		NodeMultiZoneEnable:   csiCfg.GetBool("node-multi-zone-enable", "NODE_MULTI_ZONE_ENABLE", false),
@@ -222,7 +218,6 @@ func GlobalConfigSet(m metadata.MetadataProvider) {
 		klog.Infof("AD-Controller is disabled, CSI Disk Plugin running in kubelet mode.")
 	}
 	DefaultDeviceManager.EnableDiskPartition = csiCfg.GetBool("disk-partition-enable", "DISK_PARTITION_ENABLE", true)
-	DefaultDeviceManager.DisableSerial = !controllerServerType && IsVFNode()
 	klog.Infof("Starting with GlobalConfigVar: ADControllerEnable(%t), DiskTagEnable(%t), DiskBdfEnable(%t), MetricEnable(%t), DetachDisabled(%t), DetachBeforeDelete(%t), ClusterID(%s)",
 		GlobalConfigVar.ADControllerEnable,
 		GlobalConfigVar.DiskTagEnable,
@@ -233,7 +228,7 @@ func GlobalConfigSet(m metadata.MetadataProvider) {
 		GlobalConfigVar.ClusterID,
 	)
 
-	if controllerServerType {
+	if GlobalConfigVar.ADControllerEnable {
 		pDetach := features.FunctionalMutableFeatureGate.Enabled(features.DiskParallelDetach)
 		pAttach := features.FunctionalMutableFeatureGate.Enabled(features.DiskParallelAttach)
 		GlobalConfigVar.AttachDetachSlots = NewSlots(!pDetach, !pAttach)
@@ -244,6 +239,7 @@ func GlobalConfigSet(m metadata.MetadataProvider) {
 			klog.Infof("Disk parallel detach enabled")
 		}
 	} else {
+		// if ADController is not enabled, we need serial attach to recognize old disk
 		GlobalConfigVar.AttachDetachSlots = NewSlots(true, true)
 	}
 }

pkg/disk/nodeserver.go
@@ -182,7 +182,7 @@ func NewNodeServer(m metadata.MetadataProvider) csi.NodeServer {
 		go checkVfhpOnlineReconcile()
 	}
 
-	if !GlobalConfigVar.ControllerService && IsVFNode() && GlobalConfigVar.BdfHealthCheck {
+	if IsVFNode() && GlobalConfigVar.BdfHealthCheck {
 		go BdfHealthCheck()
 	}
 

pkg/ens/ens.go
@@ -57,7 +57,7 @@ type ENS struct {
 
 func initDriver() {}
 
-func NewDriver(nodeID, endpoint string) *ENS {
+func NewDriver(nodeID, endpoint string, serviceType utils.ServiceType) *ENS {
 
 	initDriver()
 	tmpENS := &ENS{}
@@ -66,9 +66,10 @@ func NewDriver(nodeID, endpoint string) *ENS {
 	NewGlobalConfig()
 
 	tmpENS.idServer = NewIdentityServer()
-	if GlobalConfigVar.ControllerService {
+	if serviceType&utils.Controller != 0 {
 		tmpENS.controllerServer = NewControllerServer()
-	} else {
+	}
+	if serviceType&utils.Node != 0 {
 		tmpENS.nodeServer = NewNodeServer()
 	}
 	return tmpENS
@@ -142,11 +143,6 @@ func NewGlobalConfig() {
 		detachBeforeAttach = true
 	}
 
-	controllerServerType := false
-	if os.Getenv(utils.ServiceType) == utils.ProvisionerService {
-		controllerServerType = true
-	}
-
 	GlobalConfigVar = GlobalConfig{
 		KClient:                      kubeClient,
 		InstanceID:                   instanceID,
@@ -155,7 +151,6 @@ func NewGlobalConfig() {
 		RegionID:                     regionID,
 		EnableAttachDetachController: attachDetachController,
 		DetachBeforeAttach:           detachBeforeAttach,
-		ControllerService:            controllerServerType,
 	}
 }
 
@@ -164,7 +159,6 @@ type GlobalConfig struct {
 	InstanceID         string
 	ClusterID          string
 	DetachBeforeAttach bool
-	ControllerService  bool
 
 	EnableDiskPartition          string
 	EnableAttachDetachController string

pkg/metric/collector.go
@@ -45,34 +45,32 @@ type CSICollector struct {
 }
 
 // newCSICollector method returns the CSICollector object
-func newCSICollector(metricType string, driverNames []string) error {
+func newCSICollector(driverNames []string) error {
 	if csiCollectorInstance != nil {
 		return nil
 	}
 	collectors := make(map[string]Collector)
-	if metricType == pluginService {
-		enabledDrivers := map[string]struct{}{}
-		for _, d := range driverNames {
-			enabledDrivers[d] = struct{}{}
-		}
-		for _, reg := range registry {
-			enabled := len(reg.RelatedDrivers) == 0
-			for _, d := range reg.RelatedDrivers {
-				if _, ok := enabledDrivers[d]; ok {
-					enabled = true
-					break
-				}
-			}
-			if enabled {
-				collector, err := reg.Factory()
-				if err != nil {
-					return err
-				}
-				collectors[reg.Name] = collector
-			}
-		}
-
+	enabledDrivers := map[string]struct{}{}
+	for _, d := range driverNames {
+		enabledDrivers[d] = struct{}{}
 	}
+	for _, reg := range registry {
+		enabled := len(reg.RelatedDrivers) == 0
+		for _, d := range reg.RelatedDrivers {
+			if _, ok := enabledDrivers[d]; ok {
+				enabled = true
+				break
+			}
+		}
+		if enabled {
+			collector, err := reg.Factory()
+			if err != nil {
+				return err
+			}
+			collectors[reg.Name] = collector
+		}
+	}
+
 	csiCollectorInstance = &CSICollector{Collectors: collectors}
 
 	return nil

pkg/metric/metric.go
@@ -39,9 +39,9 @@ func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 // NewMetricHandler method returns a promHttp object
-func NewMetricHandler(serviceType string, driverNames []string) *Handler {
+func NewMetricHandler(driverNames []string) *Handler {
 	//csi collector singleton
-	err := newCSICollector(serviceType, driverNames)
+	err := newCSICollector(driverNames)
 	if err != nil {
 		klog.Errorf("Couldn't create collector: %s", err)
 	}

pkg/nas/nas.go
@@ -37,14 +37,14 @@ type NAS struct {
 	nodeServer       *nodeServer
 }
 
-func NewDriver(meta *metadata.Metadata, endpoint, serviceType string) *NAS {
+func NewDriver(meta *metadata.Metadata, endpoint string, serviceType utils.ServiceType) *NAS {
 	klog.Infof("Driver: %v version: %v", driverName, version.VERSION)
 
 	var d NAS
 	d.endpoint = endpoint
 	d.identityServer = newIdentityServer()
 
-	if serviceType == utils.ProvisionerService {
+	if serviceType&utils.Controller != 0 {
 		config, err := internal.GetControllerConfig(meta)
 		if err != nil {
 			klog.Fatalf("Get nas controller config: %v", err)
@@ -54,14 +54,14 @@ func NewDriver(meta *metadata.Metadata, endpoint, serviceType string) *NAS {
 			klog.Fatalf("Failed to init nas controller server: %v", err)
 		}
 		d.controllerServer = cs
-	} else {
+	}
+	if serviceType&utils.Node != 0 {
 		config, err := internal.GetNodeConfig()
 		if err != nil {
 			klog.Fatalf("Get nas node config: %v", err)
 		}
 		d.nodeServer = newNodeServer(config)
 	}
-
 	return &d
 }
 

pkg/nas/nas_test.go
@@ -2,12 +2,13 @@ package nas
 
 import (
 	"fmt"
+	"testing"
+
 	"github.com/jarcoal/httpmock"
 	"github.com/kubernetes-sigs/alibaba-cloud-csi-driver/pkg/cloud/metadata"
 	"github.com/kubernetes-sigs/alibaba-cloud-csi-driver/pkg/options"
 	"github.com/kubernetes-sigs/alibaba-cloud-csi-driver/pkg/utils"
 	"github.com/stretchr/testify/assert"
-	"testing"
 )
 
 const (
@@ -54,7 +55,7 @@ func TestNewDriverProvisioner(t *testing.T) {
 	prepareNasTestFakeK8sContext()
 	prepareFakeRegionEnvVar(t)
 
-	driver := NewDriver(metadata.NewMetadata(), "", utils.ProvisionerService)
+	driver := NewDriver(metadata.NewMetadata(), "", utils.Controller)
 	assert.NotNil(t, driver)
 }
 
@@ -83,7 +84,7 @@ func TestNewDriverPlugin(t *testing.T) {
 	prepareNasTestFakeK8sContext()
 	prepareNodeConfigEnvVars(t)
 
-	driver := NewDriver(metadata.NewMetadata(), "", utils.PluginService)
+	driver := NewDriver(metadata.NewMetadata(), "", utils.Node)
 	assert.NotNil(t, driver)
 }
 

commit aa3578cc10062ed9b24553513207e96b3300717f

re-enable metric for controller
this seems to be an error while resolving conflict

Fixes: 5defa96

main.go
@@ -254,7 +254,7 @@ func main() {
 	csiMux := http.NewServeMux()
 	csiMux.HandleFunc("/healthz", healthHandler)
 	klog.Infof("Metric listening on address: /healthz")
-	if enableMetric && serviceType&utils.Node != 0 {
+	if enableMetric {
 		metricHandler := metric.NewMetricHandler(driverNames, serviceType)
 		csiMux.Handle("/metrics", metricHandler)
 		klog.Infof("Metric listening on address: /metrics")