3944b205f5c3b8c4fcc6c3dab967d37a28206f7b
胡玮文 <huweiwen.hww@alibaba-inc.com>
2025-04-25 14:46:14 +0800

    add scrape, process, Go metrics

--- pkg/metric/metric.go
+++ pkg/metric/metric.go
@@ -6,10 +6,12 @@ import (
 	"strings"
 	"sync"
 
+	"github.com/go-logr/logr"
 	"github.com/kubernetes-sigs/alibaba-cloud-csi-driver/pkg/utils"
 	"github.com/prometheus/client_golang/prometheus"
 	versioncollector "github.com/prometheus/client_golang/prometheus/collectors/version"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"k8s.io/component-base/metrics/legacyregistry"
 	"k8s.io/klog/v2"
 )
 
@@ -49,18 +51,35 @@ func NewMetricHandler(driverNames []string, serviceType utils.ServiceType) *Hand
 	return newHandler()
 }
 
+type logger struct {
+	logr.Logger
+}
+
+func (l logger) Println(v ...interface{}) {
+	if len(v) == 2 {
+		msg, ok1 := v[0].(string)
+		err, ok2 := v[1].(error)
+		if ok1 && ok2 {
+			l.Logger.Error(err, msg)
+			return
+		}
+	}
+	l.Logger.Error(nil, "failed to gather metrics", "error", v)
+}
+
 func (h *Handler) innerHandler() (http.Handler, error) {
 	r := prometheus.NewRegistry()
-	r.MustRegister(versioncollector.NewCollector("alibaba_cloud_csi_driver"))
-	if err := r.Register(csiCollectorInstance); err != nil {
-		return nil, fmt.Errorf("Couldn't register node collector: %s", err)
-	}
-	handler := promhttp.HandlerFor(
-		r,
+	r.MustRegister(
+		versioncollector.NewCollector("alibaba_cloud_csi_driver"),
+		csiCollectorInstance)
+	handler := promhttp.InstrumentMetricHandler(r, promhttp.HandlerFor(
+		prometheus.Gatherers{r, legacyregistry.DefaultGatherer},
 		promhttp.HandlerOpts{
 			ErrorHandling: promhttp.ContinueOnError,
+			ErrorLog:      logger{klog.Background()},
+			Registry:      r,
 		},
-	)
+	))
 	return handler, nil
 }
 
