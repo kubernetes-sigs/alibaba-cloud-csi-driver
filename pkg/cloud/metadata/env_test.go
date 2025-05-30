bf3fd718f98716e1b4ddb5c71cf798d486c521d2
AlbeeSo <suyashi1321@163.com>
2025-05-30 14:36:50 +0800

    support customized repositry prefix

--- pkg/cloud/metadata/env_test.go
+++ pkg/cloud/metadata/env_test.go
@@ -11,12 +11,14 @@ func TestGetEnv(t *testing.T) {
 	t.Setenv("ALIBABA_CLOUD_ACCOUNT_ID", "112233445566")
 	t.Setenv("CLUSTER_ID", "c12345678")
 	t.Setenv("DEFAULT_REGISTRY", "registry.cn-hangzhou.aliyuncs.com")
+	t.Setenv("DEFAULT_REPOSITORY_PREFIX", "my-repo")
 	m := &ENVMetadata{}
 	expected := map[MetadataKey]string{
-		RegionID:    "cn-hangzhou",
-		AccountID:   "112233445566",
-		ClusterID:   "c12345678",
-		RegistryURL: "registry.cn-hangzhou.aliyuncs.com",
+		RegionID:         "cn-hangzhou",
+		AccountID:        "112233445566",
+		ClusterID:        "c12345678",
+		RegistryURL:      "registry.cn-hangzhou.aliyuncs.com",
+		RepositoryPrefix: "my-repo",
 	}
 	for k, v := range expected {
 		value, err := m.Get(k)
