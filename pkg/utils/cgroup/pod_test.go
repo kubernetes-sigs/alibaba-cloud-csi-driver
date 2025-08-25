43989ce7a46dfcbd95a7cd2f5e4043916a62582d
胡玮文 <huweiwen.hww@alibaba-inc.com>
2025-08-25 11:30:26 +0800

    utils/cgroup: support for cgroupFS driver

--- pkg/utils/cgroup/pod_test.go
+++ pkg/utils/cgroup/pod_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"golang.org/x/sys/unix"
 )
 
@@ -13,6 +14,19 @@ func TestFind(t *testing.T) {
 		KubepodsSlicePath: t.TempDir(),
 	}
 	err := os.MkdirAll(slice.KubepodsSlicePath+"/kubepods-podtest_pod_uid.slice", 0755)
+	assert.NoError(t, err)
+
+	fd, err := slice.FindPodDir("test-pod-uid")
+	assert.NoError(t, err)
+	defer unix.Close(fd)
+}
+
+func TestFindCgroupFS(t *testing.T) {
+	slice := PodCGroupSlice{
+		KubepodsSlicePath: t.TempDir(),
+		CgroupDriver:      CgroupFS,
+	}
+	err := os.MkdirAll(slice.KubepodsSlicePath+"/podtest-pod-uid", 0755)
 	assert.NoError(t, err)
 
 	fd, err := slice.FindPodDir("test-pod-uid")
@@ -43,24 +57,36 @@ func TestDetectNone(t *testing.T) {
 
 func TestDetectV1(t *testing.T) {
 	path := t.TempDir()
-	err := os.MkdirAll(path+"/blkio/kubepods.slice", 0755)
-	assert.NoError(t, err)
+	require.NoError(t, os.MkdirAll(path+"/blkio/kubepods.slice", 0755))
 	mkV1Files(t, path+"/blkio/kubepods.slice")
 
 	slice, io, err := DetectCgroupVersion(path)
 	assert.NoError(t, err)
 	assert.Equal(t, path+"/blkio/kubepods.slice", slice.KubepodsSlicePath)
+	assert.Equal(t, Systemd, slice.CgroupDriver)
+	assert.IsType(t, V1{}, io)
+}
+
+func TestDetectV1CgroupFS(t *testing.T) {
+	path := t.TempDir()
+	require.NoError(t, os.MkdirAll(path+"/blkio/kubepods", 0755))
+	mkV1Files(t, path+"/blkio/kubepods")
+
+	slice, io, err := DetectCgroupVersion(path)
+	assert.NoError(t, err)
+	assert.Equal(t, path+"/blkio/kubepods", slice.KubepodsSlicePath)
+	assert.Equal(t, CgroupFS, slice.CgroupDriver)
 	assert.IsType(t, V1{}, io)
 }
 
 func TestDetectV2(t *testing.T) {
 	path := t.TempDir()
-	err := os.MkdirAll(path+"/kubepods.slice", 0755)
-	assert.NoError(t, err)
+	require.NoError(t, os.MkdirAll(path+"/kubepods.slice", 0755))
 	mkFile(t, path+"/kubepods.slice/io.max")
 
 	slice, io, err := DetectCgroupVersion(path)
 	assert.NoError(t, err)
 	assert.Equal(t, path+"/kubepods.slice", slice.KubepodsSlicePath)
+	assert.Equal(t, Systemd, slice.CgroupDriver)
 	assert.IsType(t, V2{}, io)
 }
