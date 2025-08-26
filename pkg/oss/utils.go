kubernetes-sigs/alibaba-cloud-csi-driver

commit b86808e7f79bf13205ed02268215909c8bd3d5d8

oss: fetch RAM role name with metadata

pkg/oss/utils.go
@@ -57,7 +57,7 @@ const (
 // reqName: for subpath generating, CreateVolumeRequest.GetName
 // onNode: run on nodeserver mode
 func parseOptions(volOptions, secrets map[string]string, volCaps []*csi.VolumeCapability,
-	readOnly bool, region, reqName string, onNode bool) *oss.Options {
+	readOnly bool, reqName string, onNode bool, m metadata.MetadataProvider) *oss.Options {
 
 	if volOptions == nil {
 		volOptions = map[string]string{}
@@ -172,6 +172,7 @@ func parseOptions(volOptions, secrets map[string]string, volCaps []*csi.VolumeCa
 	}
 
 	url := opts.URL
+	region := metadata.MustGet(m, metadata.RegionID)
 	if region != "" && utils.GetNetworkType() == "vpc" {
 		url, _ = setNetworkType(url, region)
 	}
@@ -202,8 +203,12 @@ func parseOptions(volOptions, secrets map[string]string, volCaps []*csi.VolumeCa
 	case oss.AuthTypeSTS:
 		// try to get default ECS worker role from metadata server
 		if opts.RoleName == "" {
-			workerRole, _ := utils.GetMetaData(utils.WorkerRoleResource)
-			opts.RoleName = utils.MetadataURL + utils.WorkerRoleResource + workerRole
+			workerRole, err := m.Get(metadata.RAMRoleName)
+			if err != nil {
+				klog.ErrorS(err, "get worker role name failed")
+			} else {
+				opts.RoleName = utils.MetadataURL + utils.WorkerRoleResource + workerRole
+			}
 		}
 	}
 

pkg/cloud/metadata/ecs_dynamic.go
@@ -1,0 +1,35 @@
+package metadata
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/kubernetes-sigs/alibaba-cloud-csi-driver/pkg/cloud/metadata/imds"
+)
+
+type ECSDynamic struct {
+	client imds.Client
+}
+
+func NewEcsDynamic(httpRT http.RoundTripper) *ECSDynamic {
+	return &ECSDynamic{
+		client: *imds.NewClient(httpRT),
+	}
+}
+
+func (m *ECSDynamic) fetch(path string) (string, error) {
+	data, err := m.client.Fetch(context.TODO(), path)
+	if err != nil {
+		return "", err
+	}
+	return string(data), nil
+}
+
+func (m *ECSDynamic) Get(key MetadataKey) (string, error) {
+	switch key {
+	case RAMRoleName:
+		return m.fetch("meta-data/ram/security-credentials/")
+	default:
+		return "", ErrUnknownMetadataKey
+	}
+}

pkg/cloud/metadata/metadata.go
@@ -25,6 +25,7 @@ const (
 	ClusterID
 	VmocType
 	DataPlaneZoneID
+	RAMRoleName
 )
 
 func (k MetadataKey) String() string {
@@ -45,6 +46,8 @@ func (k MetadataKey) String() string {
 		return "VmocType"
 	case DataPlaneZoneID:
 		return "DataPlaneZoneID"
+	case RAMRoleName:
+		return "RAMRoleName"
 	default:
 		return fmt.Sprintf("MetadataKey(%d)", k)
 	}
@@ -144,7 +147,7 @@ func (m *Metadata) EnableEcs(httpRT http.RoundTripper) {
 	}
 	m.providers = append(m.providers, &lazyInitProvider{
 		fetcher: &EcsFetcher{httpRT: httpRT},
-	})
+	}, NewEcsDynamic(httpRT))
 
 }
 

pkg/cloud/metadata/metadata_test.go
@@ -153,6 +153,7 @@ func fakeMetadata(t *testing.T) *Metadata {
 	trans := httpmock.NewMockTransport()
 	trans.RegisterResponder("PUT", imds.ECSTokenEndpoint, httpmock.NewStringResponder(200, "fake_metadata_token"))
 	trans.RegisterResponder("GET", imds.ECSMetadataEndpoint+ECSIdentityPath, httpmock.NewStringResponder(200, testIdDoc))
+	trans.RegisterResponder("GET", imds.ECSMetadataEndpoint+"meta-data/ram/security-credentials/", httpmock.NewStringResponder(200, "testRoleName"))
 
 	m := NewMetadata()
 	m.EnableEcs(trans)
@@ -165,6 +166,10 @@ func TestCreateEcs(t *testing.T) {
 	region, err := m.Get(RegionID)
 	assert.NoError(t, err)
 	assert.Equal(t, "cn-beijing", region)
+
+	roleName, err := m.Get(RAMRoleName)
+	assert.NoError(t, err)
+	assert.Equal(t, "testRoleName", roleName)
 }
 
 func TestGetUnknownKey(t *testing.T) {

pkg/oss/controllerserver.go
@@ -64,8 +64,7 @@ func (cs *controllerServer) CreateVolume(ctx context.Context, req *csi.CreateVol
 		return nil, status.Errorf(codes.InvalidArgument, "ReclaimPolicy must be Retain. The current reclaimPolicy is %q", reclaimPolicy)
 	}
 
-	region, _ := cs.metadata.Get(metadata.RegionID)
-	ossVol := parseOptions(req.GetParameters(), req.GetSecrets(), req.GetVolumeCapabilities(), false, region, req.GetName(), false)
+	ossVol := parseOptions(req.GetParameters(), req.GetSecrets(), req.GetVolumeCapabilities(), false, req.GetName(), false, cs.metadata)
 	volumeContext := req.GetParameters()
 	if volumeContext == nil {
 		volumeContext = map[string]string{}
@@ -119,9 +118,8 @@ func (cs *controllerServer) ControllerPublishVolume(ctx context.Context, req *cs
 	klog.Infof("ControllerPublishVolume: volume %s on node %s", req.VolumeId, req.NodeId)
 	// parse options
 	nodeName := req.NodeId
-	region, _ := cs.metadata.Get(metadata.RegionID)
 	// ensure fuseType is not empty
-	opts := parseOptions(req.GetVolumeContext(), req.GetSecrets(), []*csi.VolumeCapability{req.GetVolumeCapability()}, req.GetReadonly(), region, "", false)
+	opts := parseOptions(req.GetVolumeContext(), req.GetSecrets(), []*csi.VolumeCapability{req.GetVolumeCapability()}, req.GetReadonly(), "", false, cs.metadata)
 	if err := setCNFSOptions(ctx, cs.cnfsGetter, opts); err != nil {
 		return nil, err
 	}

pkg/oss/nodeserver.go
@@ -109,9 +109,8 @@ func (ns *nodeServer) NodePublishVolume(ctx context.Context, req *csi.NodePublis
 	}
 
 	// parse options
-	region, _ := ns.metadata.Get(metadata.RegionID)
 	// ensure fuseType is not empty
-	opts := parseOptions(req.GetVolumeContext(), req.GetSecrets(), []*csi.VolumeCapability{req.GetVolumeCapability()}, req.GetReadonly(), region, "", true)
+	opts := parseOptions(req.GetVolumeContext(), req.GetSecrets(), []*csi.VolumeCapability{req.GetVolumeCapability()}, req.GetReadonly(), "", true, ns.metadata)
 	if err := setCNFSOptions(ctx, ns.cnfsGetter, opts); err != nil {
 		return nil, err
 	}

pkg/oss/utils_test.go
@@ -29,6 +29,13 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var m = metadata.FakeProvider{
+	Values: map[metadata.MetadataKey]string{
+		metadata.RegionID:    "cn-beijing",
+		metadata.RAMRoleName: "worker-role",
+	},
+}
+
 func Test_parseOptions(t *testing.T) {
 	t.Setenv("ALIBABA_CLOUD_NETWORK_TYPE", "vpc")
 
@@ -37,7 +44,7 @@ func Test_parseOptions(t *testing.T) {
 	testCVReq := csi.CreateVolumeRequest{
 		Parameters: map[string]string{
 			"bucket":   "test-bucket",
-			"url":      "oss-cn-beijing.aliyuncs.com",
+			"url":      "oss-cn-hangzhou.aliyuncs.com",
 			"volumeAs": "subpath",
 		},
 		Secrets: map[string]string{
@@ -54,7 +61,7 @@ func Test_parseOptions(t *testing.T) {
 	}
 	expectedOptions = &oss.Options{
 		Bucket:        "test-bucket",
-		URL:           "https://oss-cn-beijing.aliyuncs.com",
+		URL:           "https://oss-cn-hangzhou.aliyuncs.com",
 		AkID:          "test-akid",
 		AkSecret:      "test-aksecret",
 		FuseType:      "ossfs",
@@ -63,7 +70,7 @@ func Test_parseOptions(t *testing.T) {
 		MetricsTop:    defaultMetricsTop,
 	}
 	gotOptions = parseOptions(testCVReq.GetParameters(),
-		testCVReq.GetSecrets(), testCVReq.GetVolumeCapabilities(), false, "", "volume-id", false)
+		testCVReq.GetSecrets(), testCVReq.GetVolumeCapabilities(), false, "volume-id", false, m)
 	assert.Equal(t, expectedOptions, gotOptions)
 
 	// ControllerPublishVolume
@@ -100,7 +107,7 @@ func Test_parseOptions(t *testing.T) {
 	}
 	gotOptions = parseOptions(testCPVReq.GetVolumeContext(),
 		testCPVReq.GetSecrets(), []*csi.VolumeCapability{testCPVReq.GetVolumeCapability()},
-		testCPVReq.Readonly, "cn-beijing", "", false)
+		testCPVReq.Readonly, "", false, m)
 	assert.Equal(t, expectedOptions, gotOptions)
 
 	// NodePublishVolume
@@ -138,7 +145,7 @@ func Test_parseOptions(t *testing.T) {
 	}
 	gotOptions = parseOptions(testNPReq.GetVolumeContext(),
 		testNPReq.GetSecrets(), []*csi.VolumeCapability{testNPReq.GetVolumeCapability()},
-		testNPReq.Readonly, "cn-beijing", "", true)
+		testNPReq.Readonly, "", true, m)
 	assert.Equal(t, expectedOptions, gotOptions)
 
 	// test authtype
@@ -147,7 +154,7 @@ func Test_parseOptions(t *testing.T) {
 	}
 	t.Setenv("ACCESS_KEY_ID", "test-akid")
 	t.Setenv("ACCESS_KEY_SECRET", "test-aksecret")
-	gotOptions = parseOptions(options, nil, nil, true, "cn-beijing", "", true)
+	gotOptions = parseOptions(options, nil, nil, true, "", true, m)
 	expectedOptions = &oss.Options{
 		AkID:          "test-akid",
 		AkSecret:      "test-aksecret",
@@ -165,7 +172,7 @@ func Test_parseOptions(t *testing.T) {
 		"roleName": "test-rolename",
 		"url":      "oss-cn-beijing.aliyuncs.com",
 	}
-	gotOptions = parseOptions(options, nil, nil, true, "", "", true)
+	gotOptions = parseOptions(options, nil, nil, true, "", true, m)
 	expectedOptions = &oss.Options{
 		AuthType:      oss.AuthTypeSTS,
 		FuseType:      "ossfs",
@@ -174,7 +181,25 @@ func Test_parseOptions(t *testing.T) {
 		MetricsTop:    defaultMetricsTop,
 		ReadOnly:      true,
 		RoleName:      "test-rolename",
-		URL:           "https://oss-cn-beijing.aliyuncs.com",
+		URL:           "http://oss-cn-beijing-internal.aliyuncs.com",
+	}
+	assert.Equal(t, expectedOptions, gotOptions)
+
+	// Auto RoleName
+	options = map[string]string{
+		"authType": "sts",
+		"url":      "oss-cn-beijing.aliyuncs.com",
+	}
+	gotOptions = parseOptions(options, nil, nil, true, "", true, m)
+	expectedOptions = &oss.Options{
+		AuthType:      oss.AuthTypeSTS,
+		FuseType:      "ossfs",
+		Path:          "/",
+		UseSharedPath: true,
+		MetricsTop:    defaultMetricsTop,
+		ReadOnly:      true,
+		RoleName:      "http://100.100.100.200/latest/meta-data/ram/security-credentials/worker-role",
+		URL:           "http://oss-cn-beijing-internal.aliyuncs.com",
 	}
 	assert.Equal(t, expectedOptions, gotOptions)
 }

pkg/utils/util.go
@@ -24,7 +24,6 @@ import (
 	"fmt"
 	"io"
 	"net"
-	"net/http"
 	"os"
 	"os/exec"
 	"path"
@@ -213,55 +212,6 @@ func IsFileExisting(filename string) bool {
 		return true
 	}
 	return !os.IsNotExist(err)
-}
-
-// GetRegionID Get RegionID from Environment Variables or Metadata
-func GetRegionID() (string, error) {
-	var err error
-	regionID := os.Getenv("REGION_ID")
-	if regionID == "" {
-		regionID, err = GetMetaData(RegionIDTag)
-	}
-	return regionID, err
-}
-
-// GetMetaData get metadata from ecs meta-server
-func GetMetaData(resource string) (string, error) {
-	resp, err := http.Get(MetadataURL + resource)
-	if err != nil {
-		return "", err
-	}
-	if resp.StatusCode != 200 {
-		msg := fmt.Sprintf("GetMetaData Response StatusCode %d, Response: %++v", resp.StatusCode, resp)
-		return "", errors.New(msg)
-	}
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-	if strings.Contains(string(body), "Error 500 Internal Server Error") {
-		msg := fmt.Sprintf("GetMetaData Response StatusCode %d, Response: %++v", resp.StatusCode, resp)
-		return "", errors.New(msg)
-	}
-	return string(body), nil
-}
-
-// RetryGetMetaData ...
-func RetryGetMetaData(resource string) string {
-	var response string
-	for i := 0; i < MetadataMaxRetryCount; i++ {
-		response, _ = GetMetaData(resource)
-		if response != "" {
-			break
-		}
-		time.Sleep(1 * time.Second)
-	}
-	if response == "" {
-		klog.Fatalf("RetryGetMetadata: failed to get metadata %s%s after %d retries", MetadataURL, resource, MetadataMaxRetryCount)
-	}
-	klog.Infof("RetryGetMetaData: successful get metadata %v: %v", resource, response)
-	return response
 }
 
 func Gi2Bytes(gb int64) int64 {

commit 9f64aca39fc1afdecb190afe92709f90d6d501d5

oss: tolerate no regionID
Fixes: b86808e7f79bf13205ed02268215909c8bd3d5d8

pkg/oss/utils.go
@@ -173,7 +173,7 @@ func parseOptions(volOptions, secrets map[string]string, volCaps []*csi.VolumeCa
 	}
 
 	url := opts.URL
-	region := metadata.MustGet(m, metadata.RegionID)
+	region, _ := m.Get(metadata.RegionID)
 	if region != "" && utils.GetNetworkType() == "vpc" {
 		url, _ = setNetworkType(url, region)
 	}