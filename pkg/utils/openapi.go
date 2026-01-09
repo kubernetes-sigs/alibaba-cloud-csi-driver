1ca64682fbb4bb1612ec067da6117e70d14d4e4f
胡玮文 <huweiwen.hww@alibaba-inc.com>
2026-01-09 18:39:51 +0800

    utils: ECS SDK v2: use VPC endpoints for cn-hangzhou, etc.

--- pkg/utils/openapi.go
+++ pkg/utils/openapi.go
@@ -4,7 +4,35 @@ import (
 	"os"
 
 	openapi "github.com/alibabacloud-go/darabonba-openapi/v2/client"
+	"k8s.io/utils/ptr"
 )
+
+func getRegionalEndpoint(product, network, regionID string) *string {
+	// See github.com/alibabacloud-go/darabonba-openapi/v2/utils.GetEndpointRules
+	switch network {
+	case "":
+		return nil // use SDK default
+	case "public":
+		network = ""
+	default:
+		network = "-" + network
+	}
+	return ptr.To(product + network + "." + regionID + ".aliyuncs.com")
+
+}
+
+// Notes on cn-hangzhou-finance
+// ECS: the same region as cn-hangzhou, don't have cn-hangzhou-finance
+// EFLO: the same region as cn-hangzhou, don't have cn-hangzhou-finance
+// ACK: the same region as cn-hangzhou, don't have cn-hangzhou-finance
+//      This is the REGION_ID env and in ack-cluster-profile configmap
+// STS: the same region as cn-hangzhou, don't have cn-hangzhou-finance
+// NAS: has both cn-hangzhou and cn-hangzhou-finance, both can be accessed. We use cn-hangzhou
+// OSS: completely different. But we don't care, user specifies endpoint directly
+//      - oss-cn-hzjbp-a-internal.aliyuncs.com
+//      - oss-cn-hzfinance.aliyuncs.com
+// ACR: has both cn-hangzhou and cn-hangzhou-finance, both can be accessed.
+//      We use registry-cn-hangzhou-finance-vpc.ack.aliyuncs.com, determined by ACK deploy platform
 
 func getOpenAPIConfig(regionID string) *openapi.Config {
 	config := &openapi.Config{RegionId: &regionID}
@@ -29,6 +57,12 @@ func GetEcsConfig(regionID string) *openapi.Config {
 	config := getOpenAPIConfig(regionID)
 	if e := os.Getenv("ECS_ENDPOINT"); e != "" {
 		config.Endpoint = &e
+	} else {
+		// ECS default endpoint logic maps cn-hangzhou, etc to public network,
+		// but we want to use VPC if requested.
+		// So lets stick with the SDK V1 logic
+		network := ptr.Deref(config.Network, "")
+		config.Endpoint = getRegionalEndpoint("ecs", network, regionID)
 	}
 	return config
 }
