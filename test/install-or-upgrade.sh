00ffd7e5548b73cf7c4569e72bb0e3272622f5cc
胡玮文 <huweiwen.hww@alibaba-inc.com>
2026-03-06 16:22:11 +0800

    test: upgrade CRDs

--- test/install-or-upgrade.sh
+++ test/install-or-upgrade.sh
@@ -7,14 +7,24 @@ set -e
 UPSTREAM=${UPSTREAM:-"origin/master"}
 CHART=deploy/charts/alibaba-cloud-csi-driver
 
+install() {
+    base=$1
+    shift
+
+    echo installing CRDs  # helm does not upgrade CRDs if already exists
+    kubectl apply -f "$base/$CHART/crds"
+
+    echo installing helm chart
+    helm upgrade --install --namespace kube-system alibaba-cloud-csi-driver "$base/$CHART" "$@"
+}
+
 if ! git diff --exit-code --stat "$UPSTREAM" HEAD -- $CHART; then
     echo "helm chart is changed, installing $UPSTREAM version first"
     git worktree add --detach --no-checkout ../upstream "$UPSTREAM"
-    ( cd ../upstream && git checkout HEAD -- $CHART )
-
-    helm install --namespace kube-system alibaba-cloud-csi-driver ../upstream/$CHART "$@"
+    ( cd ../upstream && git checkout HEAD -- $CHART )  # only checkout chart for install
+    install ../upstream "$@"
     git worktree remove --force ../upstream
 fi
 
 echo installing local version
-helm upgrade --install --namespace kube-system alibaba-cloud-csi-driver ./$CHART "$@"
+install . "$@"
