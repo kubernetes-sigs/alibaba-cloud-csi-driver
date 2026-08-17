fd685800f1dc37600daa5ab096121a954af97be2
AlbeeSo <suyashi1321@163.com>
2026-08-17 11:32:51 +0800

    bump ossfs version to v1.91.12.ack.2, ossfs2 to v2.0.9.ack.2

--- pkg/mounter/fuse_pod_manager/oss/utils.go
+++ pkg/mounter/fuse_pod_manager/oss/utils.go
@@ -18,8 +18,8 @@ import (
 
 var (
 	defaultOssfsImageTag        = "v1.88.4-80d165c-aliyun"
-	defaultOssfsUpdatedImageTag = "v1.91.11.ack.1-f3157f4"
-	defaultOssfs2ImageTag       = "v2.0.8.ack.1-d7bd1d4"
+	defaultOssfsUpdatedImageTag = "v1.91.12.ack.2-5cf3371"
+	defaultOssfs2ImageTag       = "v2.0.9.ack.2-4094fc0"
 )
 
 func SetDefaultImage(fuseType string, m metadata.MetadataProvider, config *fpm.FuseContainerConfig) {
